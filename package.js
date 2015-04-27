68e41d2a84ff6fd00f668a4d714a77e14dc7189a
jbaxleyiii <james@whitefloordesign.com>
2015-04-27 19:54:23 -0400

    spec bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   'name': 'newspring:apollos-core',
-  'version': '0.1.4',
+  'version': '0.1.5',
   'summary': 'The main Apollos namespace and utilities for all apollos packages.',
   'git': 'https://github.com/NewSpring/newspring-core.git'
 });
