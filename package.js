6ed79b4a5a36b6bc34087dd1b3d9461c54483fd0
jbaxleyiii <james@whitefloordesign.com>
2015-05-03 17:37:15 -0400

    added back in queing system to core

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   'name': 'newspring:apollos-core',
-  'version': '0.1.7',
+  'version': '0.1.8',
   'summary': 'The main Apollos namespace and utilities for all apollos packages.',
   'git': 'https://github.com/NewSpring/newspring-core.git'
 });
