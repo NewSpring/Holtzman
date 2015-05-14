54022f225834522e219db130339f5f0cdd140741
Ben Wiley <ben.wiley@newspring.cc>
2015-05-14 17:14:24 -0400

    Refactor regex file and add credit cards

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   'name': 'newspring:apollos-core',
-  'version': '0.1.21',
+  'version': '0.1.22',
   'summary': 'The main Apollos namespace and utilities for all apollos packages.',
   'git': 'https://github.com/NewSpring/newspring-core.git'
 });
