cbfac8abac3730dff516c12da6da4680e67969e9
Ben Wiley <ben.wiley@newspring.cc>
2015-05-24 19:03:11 -0400

    Bump bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   'name': 'newspring:apollos-core',
-  'version': '0.1.23',
+  'version': '0.1.24',
   'summary': 'The main Apollos namespace and utilities for all apollos packages.',
   'git': 'https://github.com/NewSpring/newspring-core.git'
 });
