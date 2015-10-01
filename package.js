f27f1284adb7f7ebd22ffd66c1e9ccb1e9e8194f
ben.wiley <ben.wiley@newspring.cc>
2015-10-01 09:49:27 -0400

    upgrade to meteor 1.2.0.2

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.describe({
 Package.onUse(function(api) {
 
 
-  api.versionsFrom("1.2.0.1");
+  api.versionsFrom("1.2.0.2");
   api.use('fourseven:scss@3.2.0');
   api.use('coffeescript@1.0.9');
   api.use("jquery", "client");
