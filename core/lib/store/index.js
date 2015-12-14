99de6893ea8cbe4eac70cce432ee8480fdb6e872
James Baxley <james.baxley@newspring.cc>
2015-12-14 15:41:02 -0500

    routes as an array not as an object

--- core/lib/store/index.js
+++ core/lib/store/index.js
@@ -1,10 +1,10 @@
 
 
 // stored state for use with other packages
-let routes = {}
+let routes = []
 import Error from "../error"
 
-const storeRoutes = (route) => { routes = {...routes, ...route }; return route }
+const storeRoutes = (route) => { routes = routes.concat(route); return route }
 const getRoutes = () => { return routes }
 
 export default {
