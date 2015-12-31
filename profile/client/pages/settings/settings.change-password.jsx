01afdfeb039a9ad967398000bd64ddbb10e4251b
James Baxley <james.baxley@newspring.cc>
2015-12-31 12:00:07 -0500

    formatting on success of password reset

--- profile/client/pages/settings/settings.change-password.jsx
+++ profile/client/pages/settings/settings.change-password.jsx
@@ -110,7 +110,7 @@ export default class ChangePassword extends Component {
 
     if (this.state.state === "success") {
       return (
-        <div className="one-whole text-center push-double-top@lap-and-up">
+        <div className="one-whole text-center push-double-top soft-double-top@lap-and-up">
           <h4>Your password has been updated!</h4>
         </div>
       )
