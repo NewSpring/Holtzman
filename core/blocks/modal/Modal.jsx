3f94d7e4af9cb06444e92a5fa04dbae3d7601ef1
James Baxley <james.baxley@newspring.cc>
2016-01-14 23:10:28 -0500

    refactor into container and component

---
+++ core/blocks/modal/Modal.jsx
@@ -0,0 +1,105 @@
+import { Component, PropTypes} from "react";
+import {VelocityComponent } from "velocity-react";
+
+export default class SideModal extends Component {
+
+  static propTypes = {
+    childClasses: PropTypes.array,
+    float: PropTypes.bool,
+    classes: PropTypes.array,
+    offset: PropTypes.bool,
+    styles: PropTypes.obj,
+    close: PropTypes.func.isRequired,
+    component: PropTypes.func.isRequired,
+    props: PropTypes.obj.isRequired
+  }
+
+  static defaultProps = {
+    childClasses: [],
+    float: false,
+    classes: [],
+    offset: false,
+    styles: {},
+    props: {}
+  }
+
+  childClasses = () => {
+
+    const { childClasses, float } = this.props
+
+    let classes = [
+      "hard",
+      "one-whole",
+      styles["interior"],
+    ];
+
+    if (childClasses.length) {
+      classes.concat(childClasses);
+    }
+
+    if (float) {
+      classes.push("floating__item")
+    } else {
+      classes = classes.concat([
+        "inline-block",
+        "locked-top"
+      ])
+    }
+
+    return classes.join(" ");
+
+  }
+
+  layoutClasses = () => {
+
+    const { classes, float, offset } = this.props.modal
+
+    let classes = [
+      "hard",
+      "flush",
+      "background--light-primary",
+    ];
+
+    if (classes.length) {
+      classes.concat(classes);
+    } else {
+      classes.push(styles["side-panel"])
+    }
+
+    if (float) {
+      classes.push("floating")
+    }
+
+    if (offset) {
+      classes.push(offset["offset"])
+    }
+
+    return classes.join(" ");
+  }
+
+
+  render () {
+    const { close, component, props } = this.props
+
+    return (
+      <div className="panel overlay--solid-dark" onClick={close}>
+        <VelocityComponent
+          animation={slide}
+          duration={300}
+          runOnMount={true}
+        >
+          <section
+            className={ this.props.theme || this.layoutClasses() }
+            style={ this.props.styles || this.props.style }
+          >
+            <div className={ this.childClasses() }>
+              <component {...props} />
+            </div>
+
+          </section>
+        </VelocityComponent>
+      </div>
+    )
+
+  }
+}
--- core/blocks/modal/index.jsx
+++ core/blocks/modal/index.jsx
@@ -1,10 +1,11 @@
 import { Component, PropTypes} from "react"
 import { connect } from "react-redux"
 
-import {VelocityComponent, VelocityTransitionGroup } from "velocity-react"
+import { VelocityTransitionGroup } from "velocity-react"
 
 import { modal as modalActions, nav as navActions } from "../../store"
 
+import Modal from "./Modal.jsx"
 import offset from "../../blocks/nav/nav.offset.css"
 import styles from "./modal.css"
 
@@ -15,65 +16,7 @@ const map = (state) => ({
 })
 
 @connect(map)
-export default class SideModal extends Component {
-
-  childClasses = () => {
-
-    const { props } = this.props.modal
-
-    let classes = [
-      "hard",
-      "one-whole",
-      styles["interior"]
-    ];
-
-    if (props.childClasses) {
-      classes.concat(props.childClasses);
-    }
-
-    if (props.float) {
-      classes.push("floating__item")
-    } else {
-      classes = classes.concat([
-        "inline-block",
-        "locked-top"
-      ])
-    }
-
-    return classes.join(" ");
-
-  }
-
-  layoutClasses = () => {
-
-    const { props } = this.props.modal
-
-    let classes = [
-      "hard",
-      "flush",
-      "background--light-primary",
-    ];
-
-    if (props.classes.length) {
-      classes.concat(props.classes);
-    } else {
-      classes.push(styles["side-panel"])
-    }
-
-    if (props.float) {
-      classes.push("floating")
-    }
-
-    if (this.props.navigation.visible) {
-      classes.push(offset["offset"])
-    }
-
-    return classes.join(" ");
-  }
-
-  styles = () => {
-    return {}
-  }
+export default class SideModalContainer extends Component {
 
   componentWillReceiveProps(nextProps) {
 
@@ -82,13 +25,9 @@ export default class SideModal extends Component {
     }
 
     if (!nextProps.modal.visible && nextProps.navigation.level === "MODAL" && !this.props.modal.props.keepNav) {
-      // this will need to be expanded...
       this.props.dispatch(navActions.setLevel("TOP"))
     }
 
-    // if (this.props.router.path === nextProps.router.path && nextProps.router.replace) {
-    //   this.props.dispatch(modalActions.hide())
-    // }
   }
 
   componentWillUpdate(nextProps){
@@ -124,54 +63,23 @@ export default class SideModal extends Component {
     let exit = "fadeOut"
     let slide = "transition.slideLeftIn"
 
-    // if (typeof window != "undefined" && window != null) {
-    //   const width = window.innerWidth
-    //   if (width < 768) {
-    //     enter = "transition.slideUpIn"
-    //     exit = "slideDown"
-    //     slide = "slideUp"
-    //   }
-    // }
+    const { visible, content, props } = this.props.modal
+
+    if (!visible || !content) {
+      Modal = null
+    }
 
     return (
-
       <VelocityTransitionGroup
-        enter={{
-          animation: enter, duration: 250
-        }}
-        leave={{
-          animation: exit, duration: 250
-        }}
+        enter={{ animation: enter, duration: 250 }}
+        leave={{ animation: exit, duration: 250 }}
       >
-
-        {() => {
-
-          if (!this.props.modal.visible) {
-            return null
-          }
-
-          return (
-
-            <div className="panel overlay--solid-dark" onClick={this.close}>
-
-              <VelocityComponent
-                animation={slide}
-                duration={500}
-                runOnMount={true}
-              >
-                <section
-                  className={ this.props.theme || this.layoutClasses() }
-                  style={ this.props.styles || this.styles() }
-                >
-                  <div className={this.props.childClasses || this.childClasses()}>
-                    <this.props.modal.content {...this.props.modal.props}/>
-                  </div>
-
-                </section>
-              </VelocityComponent>
-            </div>
-          )
-        }()}
+        <Modal
+          close={this.close}
+          component={content}
+          props={props}
+          {...this.props}
+        />
       </VelocityTransitionGroup>
     )
   }
