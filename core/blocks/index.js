967a64011aecbc2ba36a959fbd85954b1f256b18
James Baxley <james.baxley@newspring.cc>
2016-01-14 23:50:35 -0500

    blocks section refactored

---
+++ core/blocks/index.js
@@ -0,0 +1,14 @@
+
+import Authorized from "./authorzied"
+import OnBoard from "./onBoard"
+import Nav from "./nav"
+import Modal from "./modal"
+import Sections from "./sections"
+
+export default {
+  Authorized,
+  Nav,
+  OnBoard,
+  Modal,
+  Sections
+}
