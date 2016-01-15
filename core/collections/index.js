84f457c67d8085de69f38885fb4c7000a3dc156d
James Baxley <james.baxley@newspring.cc>
2016-01-14 23:53:35 -0500

    rename collections

---
+++ core/collections/index.js
@@ -0,0 +1,12 @@
+
+import Campuses from "./campuses"
+import Likes from "./likes"
+import People from "./people"
+import Sections from "./sections"
+
+export default {
+  Campuses,
+  Likes,
+  People,
+  Sections,
+}
