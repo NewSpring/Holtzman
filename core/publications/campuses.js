1e050ce7653afd5446e1310d48134f4aa7fa1f46
James Baxley <james.baxley@newspring.cc>
2016-01-15 00:29:22 -0500

    add pub methods

---
+++ core/publications/campuses.js
@@ -0,0 +1,38 @@
+/*global REST2DDP */
+
+import { api } from "../../util/rock"
+
+const campuses = () => {
+  if (api._ && api._.baseURL && typeof REST2DDP != "undefined") {
+
+
+    const getCampuses = function (callback) {
+      let query = api.parseEndpoint(`
+        Campuses?
+          $select=
+            Name,
+            ShortCode,
+            Id,
+            LocationId
+      `)
+
+      api.get(query, (err, locations) => {
+        if (err) { callback(err); return }
+
+        callback(null, locations)
+      })
+    }
+
+
+    return REST2DDP.publish("campuses", {
+      collectionName: "campuses",
+      method: getCampuses,
+      jsonPath: "*",
+      pollInterval: 86400000
+    })
+
+  }
+
+}
+
+export default campuses
---
+++ core/publications/likes.js
@@ -0,0 +1,15 @@
+import { Likes } from "../collections"
+
+// must use function because arrow version doesn't like 'this'
+Meteor.publish("likes", function() {
+  return Likes.find(
+    {
+      userId: this.userId
+    },
+    {
+      sort: {
+        dateLiked: -1
+      }
+    }
+  )
+})
