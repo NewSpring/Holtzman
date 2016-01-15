4dbc681796ee82e7740b870fe1235291412059c2
James Baxley <james.baxley@newspring.cc>
2016-01-15 11:14:14 -0500

    move around and refactor util

---
+++ core/util/format/index.js
@@ -0,0 +1,13 @@
+
+
+import { capitalize } from "./strings"
+import { toCurrency } from "./currency"
+import { toDateString } from "./dates"
+import { creditCard } from "./credit-card"
+
+export default {
+  capitalize,
+  toCurrency,
+  toDateString,
+  creditCard,
+}
