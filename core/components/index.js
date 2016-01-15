88f45e681de16b8d5874801f42471bb7fef41b55
James Baxley <james.baxley@newspring.cc>
2016-01-15 00:09:33 -0500

    moved over components

---
+++ core/components/index.js
@@ -0,0 +1,16 @@
+
+import Card from "./cards"
+import Controls from "./controls"
+import Forms from "./forms"
+import Loading from "./loading"
+import Map from "./map"
+import Icons from "./icons"
+
+export default {
+  Card,
+  Controls,
+  Forms,
+  Loading,
+  Map,
+  Icons
+}
