dd68f7f584ad3c9308636b0294d0c29a2241364c
James Baxley <james.baxley@newspring.cc>
2016-01-15 19:19:10 -0500

    update linking of publications

---
+++ give/publications/index.js
@@ -0,0 +1,30 @@
+
+import accounts from "./accounts"
+import transactions from "./transactions"
+import scheduledTransactions from "./scheduledTransactions"
+import paymentDetails from "./paymentDetails"
+
+// observers
+import {
+  transactions as transactionsObserver,
+  schedules as scheduledTransactionsObserver
+} from "../observers"
+
+const publications = {
+  accounts,
+  transactions,
+  scheduledTransactions,
+  paymentDetails,
+  transactionsObserver,
+  scheduledTransactionsObserver
+}
+
+const publish = () => {
+  for (let publication in publications) {
+    publications[publication]()
+  }
+}
+
+export default {
+  publish
+}
