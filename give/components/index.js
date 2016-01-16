b3c8212519580bec4111b1b66ea981eb686e556f
James Baxley <james.baxley@newspring.cc>
2016-01-15 22:53:05 -0500

    added more components

--- give/components/index.js
+++ give/components/index.js
@@ -1,9 +1,10 @@
 
-import AccountType from "./accountType"
+import AccountType from "./AccountType"
 import Icons from "./Icons"
-
+import Status from "./Status"
 
 export default {
   AccountType,
   Icons,
+  Status
 }
