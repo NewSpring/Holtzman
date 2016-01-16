6094e2a94c80ab622fe06fa6d84f6387acd414e0
James Baxley <james.baxley@newspring.cc>
2016-01-15 19:21:15 -0500

    update linking of collections

---
+++ give/collections/index.js
@@ -0,0 +1,13 @@
+
+
+import Accounts from "./accounts"
+import Transactions from "./transactions"
+import ScheduledTransactions from "./scheduledTransactions"
+import PaymentDetails from "./paymentDetails"
+
+export default {
+  Accounts,
+  Transactions,
+  ScheduledTransactions,
+  PaymentDetails
+}
