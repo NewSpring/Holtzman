7f75e2197b4c67ad44b6bdb7e07a9039476ad0e0
James Baxley <james.baxley@newspring.cc>
2016-02-06 14:09:40 -0500

    added correct children support for right side panel

--- core/blocks/split/Right.jsx
+++ core/blocks/split/Right.jsx
@@ -77,6 +77,7 @@ export default class Right extends Component {
   preloader() {
     return (
       <section className={`${this.imageclasses.join(" ")} ${Styles["load-item"]}`}>
+        {this.children}
       </section>
     );
   }
@@ -85,6 +86,7 @@ export default class Right extends Component {
   renderElement() {
     return (
       <section className={this.imageclasses.join(" ")} style={this.style}>
+        {this.children}
       </section>
     );
   }
