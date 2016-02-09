1a6e382148d17fa8984c5c3a63836e642641e1b8
James Baxley <james.baxley@newspring.cc>
2016-02-09 11:25:45 -0500

    moar conflicts

--- give/blocks/AddSchedule/index.jsx
+++ give/blocks/AddSchedule/index.jsx
@@ -158,14 +158,10 @@ export default class CartContainer extends Component {
       }
     ]
 
-<<<<<<< HEAD
     let mappedAccounts = this.props.accounts.filter((x) => {
       return x.description && x.image
       // return true
     }).map((x) => ({
-=======
-    let mappedAccounts = this.props.accounts.map((x) => ({
->>>>>>> 277ad2915467ce53f88e16196d84ca0534c3e61f
       value: x.id,
       label: x.name
     }))
