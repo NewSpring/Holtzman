090bafac4801ac58f3d6c3b7b61f52108012c1a3
John Pinkerton <jpinkerton88@gmail.com>
2016-02-17 16:41:13 -0500

    allow link to be passed through for full right

--- core/blocks/split/Right.jsx
+++ core/blocks/split/Right.jsx
@@ -103,22 +103,23 @@ export default class Right extends Component {
       Wrapper = ImageLoader
     }
 
-    // if (this.props.link) {
-    //   let OldWrapper = Wrapper
-    //   Wrapper = (props) => (
-    //     <Link
-    //       to={this.props.link}
-    //       className={this.props.theme || this.layoutClasses().join(" ")}
-    //     >
-    //       <OldWrapper {...props} imageclasses={[
-    //         "background--fill",
-    //         "locked-ends",
-    //         "locked-sides",
-    //         "hard"
-    //       ]} />
-    //     </Link>
-    //   )
-    // }
+    if (this.props.link) {
+      let OldWrapper = Wrapper
+      Wrapper = (props) => (
+        <Link
+          to={this.props.link}
+          className={this.props.theme || this.layoutClasses().join(" ")}
+        >
+          <OldWrapper {...props} imageclasses={[
+            "background--fill",
+            "locked-ends",
+            "locked-sides",
+            "hard",
+            "floating"
+          ]} />
+        </Link>
+      )
+    }
 
     return (
 
