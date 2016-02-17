a2a531b45ff6534ce667c585149164acee401aa3
James Baxley <james.baxley@newspring.cc>
2016-02-16 23:53:41 -0500

    move nmi out of meteor settings

---
+++ core/util/rock/startup.js
@@ -0,0 +1,72 @@
+
+export default function startup(api) {
+
+  if (Meteor.isServer) {
+
+    let { ROOT_URL } = __meteor_runtime_config__
+    let current = api.get.sync(`DefinedValues?$filter=Value eq '${ROOT_URL}'`)
+
+    if (!current.length) {
+      let DefinedValue = {
+        IsSystem: true,
+        Order: 0,
+        DefinedTypeId: 12,
+        Value: ROOT_URL,
+        Description: `Application at ${ROOT_URL}`,
+        Guid: makeNewGuid()
+      }
+      current = api.post.sync("DefinedValues", DefinedValue)
+      current = [{
+        Id: current
+      }]
+    }
+
+    api._.rockId = current[0].Id
+
+    let site = api.get.sync(`Sites?$filter=Name eq '${ROOT_URL}'`)
+
+    if (!site.length) {
+      let Site = {
+        IsSystem: false,
+        Name: ROOT_URL,
+        Description: `Application at ${ROOT_URL}`,
+        Theme: "Rock",
+        AllowIndexing: false,
+        Guid: makeNewGuid(),
+      }
+
+      site = api.post.sync("Sites", Site)
+      site = [{
+        Id: site
+      }]
+
+    }
+
+    api._.siteId = site[0].Id
+
+    let NMI = api.get.sync("FinancialGateways?$filter=EntityType/Name eq 'Rock.Financial.NMIGateway'&$expand=EntityType")
+
+    if (!NMI.statusText && NMI.length) {
+      // Meteor.settings.nmi = NMI
+      let { Id } = NMI[0]
+      NMI = api.get.sync(`AttributeValues?$filter=EntityId eq ${Id} and Attribute/Key eq 'SecurityKey'&$expand=Attribute&$select=Value`)
+      if (!NMI.statusText && NMI.length) {
+        console.log(NMI[0].Value)
+        Meteor.settings.nmi = NMI[0].Value
+      }
+    }
+
+    console.log(Meteor.settings.nmi)
+
+    if (typeof serverWatch != "undefined") {
+      // If Rock is being watched (aka old states), remove watching
+      if (serverWatch.getKeys().indexOf("ROCK") != -1) {
+        serverWatch.stopWatching("ROCK")
+      }
+
+      // Start watching again
+      serverWatch.watch("ROCK", api._.baseURL, 30 * 1000)
+
+    }
+  }
+}
