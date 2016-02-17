dcaba6c42c519e64ee3474e8a835249c359fd995
James Baxley <james.baxley@newspring.cc>
2016-02-16 21:49:12 -0500

    fix sharing feature

--- core/store/share/saga.js
+++ core/store/share/saga.js
@@ -10,14 +10,20 @@ addSaga(function* share(getStore) {
     const { payload } = yield take("SHARE.SHARE")
 
     let { share } = getStore()
+    let msg = {}
 
-    if (typeof window != "undefined" && window != null && window.socialmessage) {
-      let msg = {}
-
-      for (let key in share.content) {
-        if (share.content[key]) msg[key] = share.conent[key]
+    for (let key in share.content) {
+      if (share.content[key] != null ) {
+        msg[key] = share.content[key]
       }
-      
+
+    }
+    if (
+      typeof window != "undefined" &&
+      window != null &&
+      window.socialmessage &&
+      Object.keys.length(msg)
+    ) {
       window.socialmessage.send(msg)
     }
 
