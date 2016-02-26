c74102947021e3a4a8de6402b61b44e77b4c9956
James Baxley <james.baxley@newspring.cc>
2016-02-26 10:50:39 -0500

    fixed sending all of the sites to rock and allow prompting a user to confirm the cancel of a schedule

--- core/util/rock/startup.js
+++ core/util/rock/startup.js
@@ -5,46 +5,49 @@ export default function startup(api) {
 
   if (Meteor.isServer) {
 
-    let { ROOT_URL } = __meteor_runtime_config__
-    let current = api.get.sync(`DefinedValues?$filter=Value eq '${ROOT_URL}'`)
+    if (process.env.NODE_ENV === "production") {
+      let { ROOT_URL } = __meteor_runtime_config__
+      let current = api.get.sync(`DefinedValues?$filter=Value eq '${ROOT_URL}'`)
 
-    if (!current.length) {
-      let DefinedValue = {
-        IsSystem: true,
-        Order: 0,
-        DefinedTypeId: 12,
-        Value: ROOT_URL,
-        Description: `Application at ${ROOT_URL}`,
-        Guid: makeNewGuid()
-      }
-      current = api.post.sync("DefinedValues", DefinedValue)
-      current = [{
-        Id: current
-      }]
-    }
-
-    api._.rockId = current[0].Id
-
-    let site = api.get.sync(`Sites?$filter=Name eq '${ROOT_URL}'`)
-
-    if (!site.length) {
-      let Site = {
-        IsSystem: false,
-        Name: ROOT_URL,
-        Description: `Application at ${ROOT_URL}`,
-        Theme: "Rock",
-        AllowIndexing: false,
-        Guid: makeNewGuid(),
+      if (!current.length) {
+        let DefinedValue = {
+          IsSystem: true,
+          Order: 0,
+          DefinedTypeId: 12,
+          Value: ROOT_URL,
+          Description: `Application at ${ROOT_URL}`,
+          Guid: makeNewGuid()
+        }
+        current = api.post.sync("DefinedValues", DefinedValue)
+        current = [{
+          Id: current
+        }]
       }
 
-      site = api.post.sync("Sites", Site)
-      site = [{
-        Id: site
-      }]
+      api._.rockId = current[0].Id
+
+      let site = api.get.sync(`Sites?$filter=Name eq '${ROOT_URL}'`)
+
+      if (!site.length) {
+        let Site = {
+          IsSystem: false,
+          Name: ROOT_URL,
+          Description: `Application at ${ROOT_URL}`,
+          Theme: "Rock",
+          AllowIndexing: false,
+          Guid: makeNewGuid(),
+        }
+
+        site = api.post.sync("Sites", Site)
+        site = [{
+          Id: site
+        }]
+
+      }
+
+      api._.siteId = site[0].Id
 
     }
-
-    api._.siteId = site[0].Id
 
 
     // GIVE
