6123bae3324b016c399af82d63afa33ab9808e71
James Baxley <james.baxley@newspring.cc>
2016-02-29 09:47:11 -0500

    remove splitting again

--- community/index.js
+++ community/index.js
@@ -1,58 +1,58 @@
 
-// import Global from "../core/blocks/global"
-// import { Routes } from "./pages"
+import Global from "../core/blocks/global"
+import { Routes } from "./pages"
 import "./store"
-
-// export default {
-//   path: "community",
-//   component: Global,
-//   // indexRoute: { component: Home },
-//   childRoutes: Routes
-// }
-
 
 export default {
   path: "community",
-  getComponent(location, cb) {
-    if (Meteor.isClient) {
-      // Split the code on a different file when on a client
-      require.ensure([], require => {
-        cb(null, require("./../core/blocks/global"))
-      }, "community");
-    } else {
-      // Save the chunk for server-rendering
-      global.__CHUNK_COLLECTOR__.push("community");
-      cb(null, require("./../core/blocks/global"));
-    }
-  },
-
-  // getIndexRoute(location, cb) {
-  //   if (Meteor.isClient) {
-  //     // Split the code on a different file when on a client
-  //     require.ensure([], require => {
-  //       cb(null, {
-  //         component: require("./client/pages/home")
-  //       })
-  //     }, "profile");
-  //   } else {
-  //     // Save the chunk for server-rendering
-  //     global.__CHUNK_COLLECTOR__.push("profile");
-  //     cb(null, {
-  //       component: require("./client/pages/home")
-  //     });
-  //   }
-  // },
-
-  getChildRoutes(location, cb) {
-    if (Meteor.isClient) {
-      // Split the code on a different file when on a client
-      require.ensure([], require => {
-        cb(null, require("./routes"))
-      }, "community");
-    } else {
-      // Save the chunk for server-rendering
-      global.__CHUNK_COLLECTOR__.push("community");
-      cb(null, require("./routes"));
-    }
-  }
+  component: Global,
+  // indexRoute: { component: Home },
+  childRoutes: Routes
 }
+
+
+// export default {
+//   path: "community",
+//   getComponent(location, cb) {
+//     if (Meteor.isClient) {
+//       // Split the code on a different file when on a client
+//       require.ensure([], require => {
+//         cb(null, require("./../core/blocks/global"))
+//       }, "community");
+//     } else {
+//       // Save the chunk for server-rendering
+//       global.__CHUNK_COLLECTOR__.push("community");
+//       cb(null, require("./../core/blocks/global"));
+//     }
+//   },
+//
+//   // getIndexRoute(location, cb) {
+//   //   if (Meteor.isClient) {
+//   //     // Split the code on a different file when on a client
+//   //     require.ensure([], require => {
+//   //       cb(null, {
+//   //         component: require("./client/pages/home")
+//   //       })
+//   //     }, "profile");
+//   //   } else {
+//   //     // Save the chunk for server-rendering
+//   //     global.__CHUNK_COLLECTOR__.push("profile");
+//   //     cb(null, {
+//   //       component: require("./client/pages/home")
+//   //     });
+//   //   }
+//   // },
+//
+//   getChildRoutes(location, cb) {
+//     if (Meteor.isClient) {
+//       // Split the code on a different file when on a client
+//       require.ensure([], require => {
+//         cb(null, require("./routes"))
+//       }, "community");
+//     } else {
+//       // Save the chunk for server-rendering
+//       global.__CHUNK_COLLECTOR__.push("community");
+//       cb(null, require("./routes"));
+//     }
+//   }
+// }
