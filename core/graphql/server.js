1e46d7de9fd4ab9509290a8056d6efd104ed4c1f
James Baxley <james.baxley@newspring.cc>
2016-03-04 16:11:19 -0500

    more logs for heighliner debugging

--- core/graphql/server.js
+++ core/graphql/server.js
@@ -14,7 +14,7 @@ Meteor.methods({
     const payload = { query, variables, operationName };
     const f = new Future();
 
-    console.log(`Making request to ${Meteor.settings.heighliner}`)
+    console.log(`Making request to ${Meteor.settings.heighliner}`, query, variables)
     fetch(Meteor.settings.heighliner, {
         method: "POST",
         headers: {
