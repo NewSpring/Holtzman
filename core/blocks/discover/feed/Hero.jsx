f995aa9ffb963a6c4a711a56e8943d8e52bb9e91
John Pinkerton <jpinkerton88@gmail.com>
2016-03-06 11:43:40 -0500

    add preloader to discover images

--- core/blocks/discover/feed/Hero.jsx
+++ core/blocks/discover/feed/Hero.jsx
@@ -1,5 +1,8 @@
 import { Component, PropTypes } from "react";
 
+import { ImageLoader } from "../../../components/loading"
+import LoadingStyles from "../../../components/loading/FeedItemSkeleton.css"
+
 export default class DiscoverHero extends Component {
 
   static propTypes = {
@@ -13,23 +16,57 @@ export default class DiscoverHero extends Component {
     backgroundImage: `url('${this.props.image}')`
   }
 
+  imageclasses = [
+    "background--fill",
+    "ratio--square",
+    "floating--bottom",
+    "floating--left"
+  ];
+
+  // context from ImageLoader
+  preloader = () => {
+    return (
+      <div
+        className={`${this.imageclasses.join(" ")} ${LoadingStyles["load-item"]}`}
+        >
+        {this.children}
+      </div>
+    );
+  }
+
+  // context from ImageLoader
+  renderElement = () => {
+    return (
+      <div
+        className={this.imageclasses.join(" ")}
+        style={this.backgroundStyles}
+        >
+        {this.children}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <a
           href={this.props.link}
-          className="background--fill ratio--square
-                     floating--bottom floating--left"
-          style={this.backgroundStyles}
         >
-          <div className="floating__item soft-left soft-bottom text-light-primary">
-            {/*<h4 className="flush">{this.props.topicName}</h4>
+        <ImageLoader
+          src={this.props.image}
+          preloader={this.preloader}
+          renderElement={this.renderElement}
+          imageclasses={this.imageclasses}>
 
-              <h7 className="soft-top">
-                {this.props.tags.join(", ")}
-              </h7>
-              */}
-          </div>
+            <div className="floating__item soft-left soft-bottom text-light-primary">
+              {/*<h4 className="flush">{this.props.topicName}</h4>
+
+                <h7 className="soft-top">
+                  {this.props.tags.join(", ")}
+                </h7>
+                */}
+            </div>
+          </ImageLoader>
         </a>
       </div>
     );
