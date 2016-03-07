11b246a54dc5f0771352cde386d8f3d433c79197
James Baxley <james.baxley@newspring.cc>
2016-03-07 17:07:31 -0500

    support removing customer vaults from NMI

--- give/methods/paymentAccounts/server/index.js
+++ give/methods/paymentAccounts/server/index.js
@@ -1,8 +1,19 @@
 /*global Meteor */
 
 import { api } from "../../../../core/util/rock"
-
+import { cancelBilling as gatewayCancelBilling } from "../../give/server/nmi"
 const remove = (id) => {
+
+  let existing = api.get.sync(`FinancialPersonSavedAccounts/${id}`)
+
+  // only remove if this is an NMI transaction and we have a gateway code
+  if (existing.ReferenceNumber && existing.FinancialGatewayId === api._.give.gateway.id) {
+    try {
+      let response = Meteor.wrapAsync(gatewayCancelBilling)(existing.ReferenceNumber)
+    } catch (e) {
+      throw new Meteor.Error(e.message ? e.message : e)
+    }
+  }
 
   let result = api.delete.sync(`FinancialPersonSavedAccounts/${id}`)
 
@@ -12,6 +23,7 @@ const remove = (id) => {
 
   return true
 }
+
 
 Meteor.methods({ "PaymentAccounts.remove": remove })
 
