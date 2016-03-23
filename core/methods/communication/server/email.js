f83a88ddfbc9acab99ddc935c7c9a5db2c9b785e
James Baxley <james.baxley@newspring.cc>
2016-03-23 13:45:08 -0400

    add billing address or home address for person during gift

--- core/methods/communication/server/email.js
+++ core/methods/communication/server/email.js
@@ -81,7 +81,6 @@ Parser.registerFilters({...caseChangedFilter, ...{
       return input.toUTCString();
     } else {
       format = format.replace(/y/gmi, "Y")
-      console.log(Moment(input).format(format))
       return Moment(input).format(format);
     }
 
