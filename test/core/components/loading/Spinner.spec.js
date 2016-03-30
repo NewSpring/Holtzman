babedcb8ab67581207244dfcebfd128fbfaeedce
delianides <drew@delianides.com>
2016-03-30 09:52:25 -0400

    added react component test for Spinner

---
+++ test/core/components/loading/Spinner.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { expect } from "chai";
+import { shallow } from "enzyme";
+import Spinner from "./../../../../core/components/loading/Spinner";
+
+describe("Spinner component", () => {
+
+ it("has the correct classname", () => {
+    const wrapper = shallow(<Spinner />);
+    expect(wrapper.hasClass("loader")).to.equal(true);
+  });
+});
