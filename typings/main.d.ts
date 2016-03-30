6d0222193c4d4628b0933ac74c9751716e3834d7
delianides <drew@delianides.com>
2016-03-30 11:35:07 -0400

    removed dir

--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/enzyme/index.d.ts" />
-/// <reference path="main/ambient/meteor/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
