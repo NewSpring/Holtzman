b42f2a953509ad6185470d2e14cbbc2d6bf30c06
John Pinkerton <jpinkerton88@gmail.com>
2016-04-18 19:57:35 -0400

    explain yo self

--- core/graphql/apollo.js
+++ core/graphql/apollo.js
@@ -1,3 +1,9 @@
+// apollo client imports need to remain in this file
+// apollo client does not like to mingle with other imports
+// apollo client has not told me why
+// apollo client demands exclusivity?
+// apollo client is probably fine and it's webpack
+// webpack is evil
 import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { readQueryFromStore } from "apollo-client/lib/src/data/readFromStore";
 import { addReducer, addMiddleware } from "../store";
