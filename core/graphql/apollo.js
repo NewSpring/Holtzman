8e3d7c6922b80e7bf538721e6eefce8079cc4f67
John Pinkerton <jpinkerton88@gmail.com>
2016-04-18 17:58:48 -0400

    integrate apollo store with existing store

--- core/graphql/apollo.js
+++ core/graphql/apollo.js
@@ -1,10 +1,16 @@
 import ApolloClient, { createNetworkInterface } from "apollo-client";
 import { readQueryFromStore } from "apollo-client/lib/src/data/readFromStore";
+import { addReducer, addMiddleware } from "../store";
 
 const heighliner = createNetworkInterface("http://localhost:8888/");
 const apolloClient = new ApolloClient({
   networkInterface: heighliner,
 });
+
+addReducer({
+  apollo: apolloClient.reducer(),
+});
+addMiddleware(apolloClient.middleware());
 
 export {
   apolloClient,
