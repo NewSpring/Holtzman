30f80f3fb9bf30e50dae3f43573e9a09711aba40
John Pinkerton <jpinkerton88@gmail.com>
2016-04-25 16:56:53 -0400

    token for now

--- core/graphql/server.js
+++ core/graphql/server.js
@@ -14,20 +14,21 @@ Meteor.methods({
     const payload = { query, variables, operationName };
     const f = new Future();
 
-    let token = "basic ";
-    if (this.userId) {
-      const user = Meteor.users.findOne(this.userId);
-      token += new Buffer(`${user._id}:${user.profile.token}`).toString("base64")
-    } else {
-      token += new Buffer(`guest:guest`).toString("base64")
-    }
+    // let token = "basic ";
+    // if (this.userId) {
+    //   const user = Meteor.users.findOne(this.userId);
+    //   token += new Buffer(`${user._id}:${user.profile.token}`).toString("base64")
+    // } else {
+    //   token += new Buffer(`guest:guest`).toString("base64")
+    // }
 
+    console.log(Meteor.settings);
     fetch(Meteor.settings.public.heighliner, {
         method: "POST",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'Authorization': "basic " + new Buffer(`apollos:${Meteor.settings.rock.public.token}`).toString("base64")
+          'Authorization': "basic " + new Buffer(`apollos:${Meteor.settings.rock.token}`).toString("base64")
         },
         body: JSON.stringify(payload)
       })
