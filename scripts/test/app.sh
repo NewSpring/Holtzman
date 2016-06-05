d375734e5e4db4603cbae1c3ecd45fdb5c1a010c
John Pinkerton <jpinkerton88@gmail.com>
2016-06-05 16:17:09 -0400

    split up app test scripts

--- scripts/test/app.sh
+++ scripts/test/app.sh
@@ -7,7 +7,7 @@ set -x
 
 # exit if it's a linux container
 if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
-  echo "Not building app on ${TRAVIS_OS_NAME}"
+  echo "Not testing app on ${TRAVIS_OS_NAME}"
   exit 0
 fi
 
@@ -19,28 +19,6 @@ yecho () {
 
 yecho "### Entering app directory ###"
 cd sites/app
-
-yecho "### Installing Meteor ###"
-curl https://install.meteor.com | /bin/sh
-
-yecho "### Installing NPM deps ###"
-npm install
-
-yecho "### Installing Norma ###"
-npm install -g NewSpring/Norma#forked-cson
-
-yecho "### Downloading things with Norma ###"
-norma build
-
-yecho "### Remove platforms for Gagarin workaround ###"
-meteor remove-platform ios android
-
-yecho "### Preparing Gagarin test build ###"
-npm install -g gagarin
-cp ./.remote/settings/sites/app.newspring.io/alpha.settings.json ./settings.json
-meteor build --directory ./.gagarin/local --server localhost:3000
-cd ./.gagarin/local/bundle/programs/server/ && npm i
-cd $TRAVIS_BUILD_DIR
 
 yecho "### Starting Chromedriver ###"
 ./node_modules/chromedriver/lib/chromedriver/chromedriver --port=9515 &
