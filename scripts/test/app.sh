1516b5a0261b56a77922671979d0efbf3859509f
John Pinkerton <jpinkerton88@gmail.com>
2016-06-05 14:54:04 -0400

    add some color

--- scripts/test/app.sh
+++ scripts/test/app.sh
@@ -5,32 +5,34 @@ if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
   exit 0
 fi
 
-echo "### Entering app directory ###"
+YELLOW=`tput setaf 3`
+
+echo "${YELLOW}### Entering app directory ###"
 cd sites/app
 
-echo "### Installing Meteor ###"
+echo "${YELLOW}### Installing Meteor ###"
 curl https://install.meteor.com | /bin/sh
 
-echo "### Installing NPM deps ###"
+echo "${YELLOW}### Installing NPM deps ###"
 npm install
 
-echo "### Installing Norma ###"
+echo "${YELLOW}### Installing Norma ###"
 npm install -g NewSpring/Norma#forked-cson
 
-echo "### Downloading things with Norma ###"
+echo "${YELLOW}### Downloading things with Norma ###"
 norma build
 
-echo "### Remove platforms for Gagarin workaround ###"
+echo "${YELLOW}### Remove platforms for Gagarin workaround ###"
 meteor remove-platform ios android
 
-echo "### Preparing Gagarin test build ###"
+echo "${YELLOW}### Preparing Gagarin test build ###"
 npm install -g gagarin
 cp ./.remote/settings/sites/app.newspring.io/alpha.settings.json ./settings.json
 meteor build --directory ./.gagarin/local --server localhost:3000
 cd ./.gagarin/local/bundle/programs/server/ && npm i
 
-echo "### Starting Chromedriver ###"
+echo "${YELLOW}### Starting Chromedriver ###"
 ./node_modules/.bin/chromedriver --port=9515 &
 
-echo "### Running App Tests ###"
+echo "${YELLOW}### Running App Tests ###"
 npm test
