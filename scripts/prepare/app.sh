850325ab94ba2efdc6fd32b401c54edcc1c7a2ec
John Pinkerton <jpinkerton88@gmail.com>
2016-06-05 16:17:09 -0400

    split up app test scripts

---
+++ scripts/prepare/app.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# force script to error out at first error
+set -e
+# trace execution of commands
+set -x
+
+# exit if it's a linux container
+if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
+  echo "Not preparing app on ${TRAVIS_OS_NAME}"
+  exit 0
+fi
+
+YELLOW=`tput setaf 3`
+yecho () {
+  echo "${YELLOW}$1"
+}
+
+
+yecho "### Entering app directory ###"
+cd sites/app
+
+yecho "### Installing Meteor ###"
+curl https://install.meteor.com | /bin/sh
+
+yecho "### Installing NPM deps ###"
+npm install
+
+yecho "### Installing Norma ###"
+npm install -g NewSpring/Norma#forked-cson
+
+yecho "### Downloading things with Norma ###"
+norma build
+
+yecho "### Remove platforms for Gagarin workaround ###"
+meteor remove-platform ios android
+
+yecho "### Preparing Gagarin test build ###"
+npm install -g gagarin
+cp ./.remote/settings/sites/app.newspring.io/alpha.settings.json ./settings.json
+meteor build --directory ./.gagarin/local --server localhost:3000
+cd ./.gagarin/local/bundle/programs/server/ && npm i
