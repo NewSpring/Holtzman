c1d8534f0bc4756a28ccfbb1913ef916d07fe2cb
benwiley <ben.wiley@newspring.cc>
2016-06-08 16:58:29 -0400

    Show search form in header when the props are right.  Need to hook up the submit action next

--- apollos/core/components/header/index.jsx
+++ apollos/core/components/header/index.jsx
@@ -2,16 +2,19 @@
 import { Component, PropTypes } from "react"
 import { connect } from "react-redux"
 
-@connect((state) => ({
-  color: state.header.content.color || "#6BAC43",
-  light: state.header.content.light,
-  text: state.header.content.title,
-  visible: state.header.visible,
-  isSearch: state.header.content.isSearch
-}))
+@connect((state) => {
+  return {
+    color: state.header.content.color || "#6BAC43",
+    light: state.header.content.light,
+    text: state.header.content.title,
+    visible: state.header.visible,
+    isSearch: state.header.content.isSearch
+  };
+})
 export default class Header extends Component {
 
   render () {
+    console.log(this.props);
     const lightColor = "text-light-primary";
     const darkColor = "text-dark-primary";
 
@@ -27,7 +30,7 @@ export default class Header extends Component {
       <div
         className="text-center"
         style={{
-          paddingTop: "15px",
+          paddingTop: this.props.isSearch ? "0" : "15px",
           paddingBottom: "15px",
           backgroundColor: this.props.color,
           borderBottom: "1px solid rgba(0,0,0, 0.1)",
@@ -38,13 +41,18 @@ export default class Header extends Component {
       {(() => {
         if (this.props.isSearch) {
           return (
-            <h6 className={`flush hard text-light-primary uppercase one-whole`}
-              style={{
-                fontWeight: 900,
-                letterSpacing: "1px",
-              }}>
-              SEARCH
-            </h6>
+            <form onSubmit={this.props.searchSubmit} className={`hard-ends soft-sides`}>
+              <div className={`input hard-bottom`}>
+                <i className="icon-search locked-left push-half-top text-light-primary"></i>
+                <input
+                  id="search"
+                  type="text"
+                  className="h5 text-light-primary"
+                  autoComplete="off"
+                  style={{ paddingLeft: "30px", borderBottom: "none", marginTop: "7px" }}
+                />
+              </div>
+            </form>
           )
         }
 
