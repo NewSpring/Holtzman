dcbb7a3c0177fcf5df20d99fdf81c9d20b059991
John Pinkerton <jpinkerton88@gmail.com>
2016-06-15 19:27:43 -0400

    remove unecessary sections padding

--- sites/app/modules/app/client/pages/sections/index.jsx
+++ sites/app/modules/app/client/pages/sections/index.jsx
@@ -4,10 +4,8 @@ import Sections from "apollos/core/blocks/sections";
 
 const Template = () => {
   return (
-    <div className="background--light-primary soft-double-bottom">
-      <div className="soft-bottom">
-        <Sections />
-      </div>
+    <div className="background--light-primary">
+      <Sections />
     </div>
   );
 }
