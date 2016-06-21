f1ba882f671185ecd8a029735837b79fe087d1a7
John Pinkerton <jpinkerton88@gmail.com>
2016-06-21 15:19:46 -0400

    don't keep nav for sections and discover in cordova

--- apollos/core/blocks/sections/index.jsx
+++ apollos/core/blocks/sections/index.jsx
@@ -18,10 +18,12 @@ export default class SectionsContainer extends Component {
 
   componentDidMount() {
     this.props.dispatch(navActions.setLevel("TOP"))
-    this.props.dispatch(modal.update({
-      keepNav: true,
-      modalBackground: "light",
-    }));
+    if (!Meteor.isCordova) {
+      this.props.dispatch(modal.update({
+        keepNav: true,
+        modalBackground: "light",
+      }));
+    }
 
     this.lockHeader("SectionsContainer");
     this.headerAction({
