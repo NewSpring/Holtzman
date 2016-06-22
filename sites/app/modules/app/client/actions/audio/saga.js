c31eeb38aa076cb8fe6499e00cc3f873a901e0c1
John Pinkerton <jpinkerton88@gmail.com>
2016-06-22 14:18:11 -0400

    add support for reporting play, pause, and duration

--- sites/app/modules/app/client/actions/audio/saga.js
+++ sites/app/modules/app/client/actions/audio/saga.js
@@ -7,18 +7,30 @@ addSaga(function* setMetaData(getStore) {
   while(true) {
     const payload = yield take("AUDIO.SET_STATE");
 
+    if (typeof NowPlaying === "undefined") return;
+
     const { audio } = getStore();
 
-    if(typeof NowPlaying !== "undefined") {
-      NowPlaying.set({
-        albumTitle: audio.playing.album.title,
-        artist: "NewSpring",
-        title: audio.playing.track.title,
-        elapsedPlaybackTime: 30,
-        playbackDuration: 500,
-        artwork: `https://${audio.playing.album.content.images[0]}`,
-      });
-    }
+    const playing = audio.state === "playing";
+    const paused = audio.state === "paused";
+
+    // convert elapsed time to seconds
+    const [timeMin, timeSec] = audio.time.split(":");
+    const elapsedSeconds = (Number((timeMin * 60)) + Number(timeSec)) || 0;
+
+    // convert duration to seconds
+    const [durMin, durSec] = audio.playing.track.duration.split(":");
+    const durationSeconds = Number((durMin * 60)) + Number(durSec) || 0;
+
+    NowPlaying.set({
+      albumTitle: audio.playing.album.title,
+      artist: "NewSpring", // switch to artist when available
+      title: audio.playing.track.title,
+      artwork: `https://${audio.playing.album.content.images[0]}`,
+      elapsedPlaybackTime: elapsedSeconds,
+      playbackDuration: durationSeconds,
+      playbackRate: playing ? 1 : 0,
+    });
 
   }
 
