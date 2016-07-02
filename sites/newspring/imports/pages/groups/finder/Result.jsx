9845ccb30dee36ce8ca4600581ac3aba3393edf8
James Baxley <james.baxley@newspring.cc>
2016-07-02 00:54:04 -0400

    nearly finished group finder

---
+++ sites/newspring/imports/pages/groups/finder/Result.jsx
@@ -0,0 +1,140 @@
+import { Component, PropTypes} from "react";
+import { connect } from "react-apollo";
+import { withRouter } from "react-router";
+import gql from "apollo-client/gql";
+
+import Split, { Left, Right } from "apollos/dist/core/blocks/split";
+import GoogleMap from "apollos/dist/core/components/map";
+import Layout from "./ResultLayout";
+
+const mapStateToProps = ({ routing }) => {
+  const { location } = routing;
+  const tags = Object.keys(location.query).length && location.query.tags ? location.query.tags : "";
+  const q = Object.keys(location.query).length && location.query.q ? location.query.q : null;
+  return { tags, q, location };
+}
+
+const mapQueriesToProps = ({ ownProps }) => ({
+  data: {
+    query: gql`
+      query GroupFinder($query: String, $tags: [String], $limit: Int, $offset: Int) {
+        groups(query: $query, attributes: $tags, limit: $limit, offset: $offset) {
+          count
+          results {
+            id
+            name
+            entityId
+            type
+            demographic
+            description
+            photo
+            kidFriendly
+            ageRange
+            locations {
+              location {
+                latitude
+                longitude
+              }
+            }
+            tags {
+              id
+              value
+            }
+            members {
+              role
+              person {
+                photo
+                firstName
+                nickName
+                lastName
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      tags: ownProps.tags.split(",").filter(x => x),
+      query: ownProps.q,
+      limit: 10,
+      offset: 0,
+    },
+  },
+});
+const defaultArray = [];
+@withRouter
+@connect({ mapQueriesToProps, mapStateToProps })
+export default class Template extends Component {
+
+  state = {
+    markers: []
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const markers = this.getMarkers(nextProps);
+    this.setState({ markers });
+  }
+
+  getMarkers = (props) => {
+    const { data } = props;
+    const { markers } = this.state;
+    if (!data.groups || !data.groups.results) return markers;
+
+    return markers.concat(data.groups.results)
+      .filter(x => x.locations && x.locations.length && x.locations[0].location)
+      .map(x => ({
+        latitude: x.locations[0].location.latitude,
+        longitude: x.locations[0].location.longitude,
+        id: x.id,
+      }))
+      .filter(x => x.latitude && x.longitude);
+  }
+
+  removeQueryString = (e) => {
+    if (e) e.preventDefault();
+    const { location, router } = this.props;
+
+    if (location && location.query && location.query.q) delete location.query.q;
+    const newPath = router.createPath(location);
+    router.replace(newPath);
+  }
+
+  render () {
+    const { data, location, tags, q } = this.props;
+    let count, groups = defaultArray;
+    if (data.groups && data.groups.count) count = data.groups.count;
+    if (data.groups && data.groups.results) groups = data.groups.results;
+    let isMobile;
+    if (typeof window != "undefined" && window != null ) {
+      isMobile = window.matchMedia("(max-width: 768px)").matches;
+    }
+    return (
+      <div>
+        <Split>
+          {/* Map */}
+          <Right mobile={false} classes={["background--left"]}>
+            {(() => {
+              if (isMobile) return null;
+              return (
+                <GoogleMap
+                  autoCenter={true}
+                  markers={this.state.markers}
+                />
+              )
+            })()}
+          </Right>
+        </Split>
+        <Left scroll={true} classes={["background--light-secondary"]}>
+          <Layout
+            loading={data.loading}
+            groups={groups}
+            count={count}
+            tags={tags.split(",").filter(x => x)}
+            query={q}
+            removeQueryString={this.removeQueryString}
+          />
+        </Left>
+      </div>
+    )
+  }
+}
---
+++ sites/newspring/imports/pages/groups/finder/index.jsx
@@ -0,0 +1,134 @@
+import { Component, PropTypes} from "react"
+import { connect } from "react-apollo"
+import gql from "apollo-client/gql";
+import { withRouter } from "react-router";
+import Split, { Left, Right } from "apollos/dist/core/blocks/split";
+
+import Layout from "./Layout";
+import Result from "./Result"
+const mapQueriesToProps = ({ ownProps }) => ({
+  attributes: {
+    query: gql`
+      query GetGroupAttributes {
+        tags: groupAttributes {
+          id
+          description
+          value
+        }
+      }
+    `,
+  },
+  content: {
+    query: gql`
+      query GetTaggedContent($tagName: String!, $limit: Int, $includeChannels: [String]) {
+        entries: taggedContent(tagName: $tagName, limit: $limit, includeChannels: $includeChannels, cache: false) {
+          entryId: id
+          title
+          meta {
+            summary
+            urlTitle
+          }
+          content {
+            images {
+              fileName
+              fileType
+              fileLabel
+              s3
+              cloudfront
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      tagName: "community",
+      includeChannels: ["articles"],
+      limit: 2,
+    }
+  }
+});
+
+const defaultArray = [];
+const mapStateToProps = (state) => ({ location: state.routing.location })
+@withRouter
+@connect({ mapQueriesToProps, mapStateToProps })
+export default class Template extends Component {
+
+  state = {
+    tags: [],
+    query: null,
+  }
+
+  tagOnClick = (tag) => {
+    let tagList = [...this.state.tags];
+    if (tagList.indexOf(tag) > -1) {
+      // remove the tag from the list string
+      tagList.splice(tagList.indexOf(tag), 1);
+    } else {
+      tagList.push(tag);
+    }
+
+    this.setState({ tags: tagList });
+  }
+
+  inputOnChange = (value) => {
+    this.setState({
+      tags: this.state.tags,
+      query: value
+    });
+  }
+
+  getResults = () => {
+    const { tags, query } = this.state;
+    let { router, location } = this.props;
+
+    if (!location.query) location.query = {};
+
+    if (query) location.query.q = query
+    if (tags.length) {
+      location.query.tags = tags.join(",").toLowerCase()
+    }
+    // reset state
+    this.setState({ tags: [], query: null });
+    router.push(location);
+  }
+
+  submitTags = (e) => {
+    if (e) e.preventDefault();
+    this.getResults();
+  }
+
+  findByQuery = (e) => {
+    if (e) e.preventDefault();
+    this.getResults();
+  }
+
+  render () {
+    const { attributes, location, content } = this.props;
+    if (Object.keys(location.query).length) return <Result />;
+
+    return (
+      <div>
+        <Split>
+          <Right
+            mobile={false}
+            classes={["background--left"]}
+            background="//dg0ddngxdz549.cloudfront.net/images/cached/images/remote/http_s3.amazonaws.com/ns.images/newspring/groups/groups.1x1_1700_1700_90_c1.jpg"
+          />
+        </Split>
+        <Left scroll={true} classes={["background--light-secondary"]}>
+          <Layout
+            canSearchTags={this.state.tags.length || this.state.query}
+            tags={(attributes && attributes.tags) || defaultArray}
+            tagOnClick={this.tagOnClick}
+            submitTags={this.submitTags}
+            findByQuery={this.findByQuery}
+            inputOnChange={this.inputOnChange}
+            content={content.loading ? defaultArray : content.entries}
+          />
+        </Left>
+      </div>
+
+    )
+  }
+}
