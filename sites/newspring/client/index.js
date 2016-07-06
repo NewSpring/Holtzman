d4bc419fb1b2f2ecf5320787d8513e08304a5a4f
John Pinkerton <jpinkerton88@gmail.com>
2016-07-06 15:45:03 -0400

    update music player styles

--- sites/newspring/client/index.js
+++ sites/newspring/client/index.js
@@ -3,5 +3,4 @@ import { GraphQL } from "apollos/dist/core/graphql";
 import { run } from "apollos/dist/core/router/client";
 import { routes, client } from "/imports"
 
-console.log("LETS DO THIS");
 run(routes, client);
