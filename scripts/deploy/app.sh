55e9d5925b641dc86e9f8caf81e38320e1900732
James Baxley <james.baxley@newspring.cc>
2016-07-10 19:14:17 -0400

    try to remove sites?

--- scripts/deploy/app.sh
+++ scripts/deploy/app.sh
@@ -119,7 +119,13 @@ if [ "$DEST" = "web" ]; then NATIVE=false; fi
 
 cd ../../apollos && WEB=$WEB NATIVE=$NATIVE npm run compile && cd ../
 rm -rf sites/$APP/.meteor/local
-cd ./sites/$APP && WEB=$WEB NATIVE=$NATIVE launch build $ROOT_URL $METEOR_SETTINGS_PATH
+cd ./sites/$APP
+if [ "$DEST" = "web" ]; then
+  yecho "### Removing cordova platforms ###"
+  meteor remove-platform android
+  meteor remove-platform ios
+fi
+WEB=$WEB NATIVE=$NATIVE launch build $ROOT_URL $METEOR_SETTINGS_PATH
 
 yecho "### Uploading bundle to S3 ###"
 aws s3 cp .build/$APP s3://ns.ops/apollos/$CURRENT_TAG-$TRAVIS_COMMIT.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
