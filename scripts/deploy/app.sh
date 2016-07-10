554b262113fd1d1ca788520cd43b69f7a7e203db
James Baxley <james.baxley@newspring.cc>
2016-07-10 18:02:50 -0400

    Merged deploys (#771)
    * start working on merged deploys
    
    * Save some time on prep
    
    * correctly hoist vars
    
    * exit when needed
    
    * don't bother building without tests
    
    * remove all doubled iffs
    
    * try alternative check
    
    * turn off prep for now
    
    * formatting and validation stuff
    
    * try to prepare and buid the app
    
    * modify shell script
    
    * try alternative && statement
    
    * lets try this now
    
    * fix a couple minor shell bugs
    
    * cd correctly and try stringified env var
    
    * hard code env vars?
    
    * ready for deploy

--- scripts/deploy/app.sh
+++ scripts/deploy/app.sh
@@ -1,83 +1,106 @@
 #!/usr/bin/env sh
 
-# exit if it's a linux container
-if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
-  echo "Not deploying app on ${TRAVIS_OS_NAME}"
-  exit 0
-fi
-
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "This is a pull request. No deployment will be done."
   exit 0
 fi
 
-# if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-#   echo "Testing on a branch other than master. No deployment will be done."
-#   exit 0
-# fi
-
-if [[ $TRAVIS_TAG == "" ]]; then
+if [ -z "$TRAVIS_TAG" ]; then
   echo "No tags found, no need for a release."
   exit 0
 fi
 
-YELLOW=`tput setaf 3`
+
+YELLOW=$(tput setaf 3)
 yecho () {
-  echo "${YELLOW}$1"
+        echo "${YELLOW}$1"
 }
 
 # force script to error out at first error
 set -e
 
-yecho "### Entering app directory ###"
-cd sites/newspring
+CURRENT_TAG=$(git describe --exact-match --abbrev=0 --tags)
 
-CURRENT_TAG=`git describe --exact-match --abbrev=0 --tags`
+PREVIOUS_TAG=$(git describe HEAD^1 --abbrev=0 --tags)
+GIT_HISTORY=$(git log --no-merges --format="- %s" "$PREVIOUS_TAG"..HEAD)
 
-PREVIOUS_TAG=`git describe HEAD^1 --abbrev=0 --tags`
-GIT_HISTORY=`git log --no-merges --format="- %s" $PREVIOUS_TAG..HEAD`
-
-if [[ $PREVIOUS_TAG == "" ]]; then
-  GIT_HISTORY=`git log --no-merges --format="- %s"`
+if [ -z "$PREVIOUS_TAG" ]; then
+  GIT_HISTORY=$(git log --no-merges --format="- %s")
 fi
 
-APP=$(echo $CURRENT_TAG | cut -d'/' -f1)
-CHANNEL=$(echo $CURRENT_TAG | cut -d'/' -f2)
-
-if [[ $APP != "app" ]]; then
-  echo "Not deploying the app because this release is for $APP"
-  exit 0
-fi
-
-yecho "### Deploying $APP to $CHANNEL ###"
 
 yecho "Current Tag: $CURRENT_TAG"
 yecho "Previous Tag: $PREVIOUS_TAG"
 yecho "Release Notes:
 
-$GIT_HISTORY"
+$GIT_HISTORY
 
-yecho "### Installing jq ###"
-brew install jq
+"
 
-yecho "### Installing Android sdks ###"
-brew install android-sdk
-echo export ANDROID_HOME=/usr/local/opt/android-sdk >> ~/.bashrc
-echo y | android update sdk --no-ui --all --filter tools,platform-tools,build-tools-23.0.3,android-23
-echo export ANDROID_ZIPALIGN=/usr/local/opt/android-sdk/build-tools/23.0.3/zipalign >> ~/.bashrc
+APP=$(echo "$CURRENT_TAG" | cut -d'/' -f1)
+DEST=$(echo "$CURRENT_TAG" | cut -d'/' -f2)
+CHANNEL=$(echo "$CURRENT_TAG" | cut -d'/' -f3)
 
-yecho "### Moving settings and certs ###"
-cp ./.remote/settings/sites/app.newspring.io/apollos.pem ./apollos.pem
-cp ./.remote/settings/sites/app.newspring.io/compose.pem ./compose.pem
-cp ./.remote/settings/sites/app.newspring.io/androidkey ~/.keystore
-cp ./.remote/settings/ssl/bundle.crt .
-cp ./.remote/settings/ssl/private.key .
+# exit if it's a linux container and a native build
+if [ "$TRAVIS_OS_NAME" != "osx" ] && [ "$DEST" = "native" ]; then
+  echo "Not deploying app on $TRAVIS_OS_NAME"
+  exit 0
+fi
+# exit if it's an osx container and a web build
+if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$DEST" != "native" ]; then
+  echo "Not deploying app on $TRAVIS_OS_NAME"
+  exit 0
+fi
 
-yecho "### Updating fastlane ###"
-gem install fastlane --no-ri --no-rdoc
+yecho "### Entering app directory ###"
+cd "sites/$APP"
+
+yecho "### Creating settings for the $CHANNEL of $APP:$DEST"
+URLPREFIX="my"
+if [ "$DEST" = "native" ]; then URLPREFIX="native"; fi
+METEOR_SETTINGS_PATH="$TRAVIS_BUILD_DIR/sites/$APP/.remote/settings/sites/$APP/$CHANNEL.settings.json"
+ROOT_URL="https://$CHANNEL-$URLPREFIX.newspring.cc"
+ECS_TASK_NAME="$DEST" # XXX long term this should be $APP or maybe $APP-$DEST
+ECS_CLUSTER=apollos # XXX move to Guild
+ECS_FAMILY="$DEST"
+ECS_SERVICE="$CHANNEL-$DEST"
+BUNDLE_URL="http://ns.ops.s3.amazonaws.com/apollos/$CURRENT_TAG-$TRAVIS_COMMIT.tar.gz"
+HOST_PORT=8080 # production newspring web
+if [ "$DEST" = "native" ] && [ "$CHANNEL" = "alpha" ]; then HOST_PORT=8062; fi
+if [ "$DEST" = "native" ] && [ "$CHANNEL" = "beta" ]; then HOST_PORT=8072; fi
+if [ "$DEST" = "native" ] && [ "$CHANNEL" = "prod" ]; then HOST_PORT=8082; fi
+if [ "$DEST" = "web" ] && [ "$CHANNEL" = "beta" ]; then HOST_PORT=8070; fi
+ENVDEST=$(echo $DEST | tr 'a-z' 'A-Z')
+
+
+
+yecho "### Deploying $APP:$DEST to $CHANNEL ###"
+if [ "$DEST" = "native" ]; then
+  yecho "### Installing Android sdks ###"
+  brew install android-sdk
+  echo export ANDROID_HOME=/usr/local/opt/android-sdk >> ~/.bashrc
+  echo y | android update sdk --no-ui --all --filter tools,platform-tools,build-tools-23.0.3,android-23
+  echo export ANDROID_ZIPALIGN=/usr/local/opt/android-sdk/build-tools/23.0.3/zipalign >> ~/.bashrc
+
+  yecho "### Moving settings and certs ###"
+  cp ./.remote/settings/sites/$APP/androidkey ~/.keystore
+
+  yecho "### Updating fastlane ###"
+  gem install fastlane --no-ri --no-rdoc
+
+  yecho "### Installing python ###"
+  brew install python
+
+  yecho "### Installing jq ###"
+  brew install jq
+
+fi
+
+yecho "### Installing launch ###"
+npm install -g meteor-launch
+cp ./.remote/settings/sites/$APP/launch.json ./launch.json
 
 yecho "### Installing aws and boto3 ###"
-brew install python
 python --version
 sudo pip install awscli boto3
 
@@ -87,28 +110,86 @@ aws configure set default.aws_access_key_id $AWS_ACCESS_KEY
 aws configure set default.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region us-east-1
 
-yecho "### Installing launch ###"
-npm install -g meteor-launch
-cp ./.remote/settings/sites/app.newspring.io/launch.json ./launch.json
 
-yecho "### Building for linux environment https://${CHANNEL}-app.newspring.io ###"
-launch build "https://${CHANNEL}-app.newspring.io" "$TRAVIS_BUILD_DIR/sites/newspring/.remote/settings/sites/app.newspring.io/${CHANNEL}.settings.json"
+yecho "### Building for linux environment https://$CHANNEL-$URLPREFIX.newspring.cc ###"
+WEB=true
+NATIVE=true
+if [ "$DEST" = "native" ]; then WEB=false; fi
+if [ "$DEST" = "web" ]; then NATIVE=false; fi
+
+cd ../../apollos && WEB=$WEB NATIVE=$NATIVE npm run compile && cd ../
+rm -rf sites/$APP/.meteor/local
+WEB=$WEB NATIVE=$NATIVE launch build $ROOT_URL $METEOR_SETTINGS_PATH
 
 yecho "### Uploading bundle to S3 ###"
 aws s3 cp .build/newspring s3://ns.ops/apollos/$CURRENT_TAG-$TRAVIS_COMMIT.tar.gz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
 yecho "### Updating ECS ###"
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
 
-if [ "${CHANNEL}" == "alpha" ]; then
-  MONGO_URL=$DOCKER_MONGO_URL OPLOG_URL=$DOCKER_OPLOG_URL ROOT_URL="https://${CHANNEL}-app.newspring.io" METEOR_SETTINGS_PATH="$TRAVIS_BUILD_DIR/sites/newspring/.remote/settings/sites/app.newspring.io/${CHANNEL}.settings.json" ECS_TASK_NAME=app ECS_CLUSTER=apollos ECS_FAMILY=app ECS_SERVICE=alpha-app HOST_PORT=8062 BUNDLE_URL="http://ns.ops.s3.amazonaws.com/apollos/$CURRENT_TAG-$TRAVIS_COMMIT.tar.gz" $TRAVIS_BUILD_DIR/scripts/deploy/ecs.sh
+# sets $task_def
+make_task_def() {
+    meteor_settings=$(cat $METEOR_SETTINGS_PATH | $JQ . | sed 's/\"/\\"/g' | tr -d '\n')
+    task_template='[
+      {
+        "name": "'"$ECS_TASK_NAME"'",
+        "memory": 512,
+        "cpu": 512,
+        "essential": true,
+        "image": "meteorhacks/meteord:binbuild",
+        "portMappings": [
+          { "hostPort": '"$HOST_PORT"', "containerPort": 80, "protocol": "tcp" }
+        ],
+        "environment": [
+          { "name": "REBUILD_NPM_MODULES", "value": "1" },
+          { "name": "MONGO_URL", "value": "'"$DOCKER_MONGO_URL"'" },
+          { "name": "DISABLE_WEBSOCKETS", "value": "1" },
+          { "name": "ROOT_URL", "value": "'"$ROOT_URL"'" },
+          { "name": "BUNDLE_URL", "value": "'"$BUNDLE_URL"'" },
+          { "name": "OPLOG_URL", "value": "'"$DOCKER_OPLOG_URL"'" },
+          { "name": "METEOR_SETTINGS", "value": "'"$meteor_settings"'" },
+          { "name": "TZ", "value": "America/New_York" }
+        ]
+      }
+    ]'
+}
 
-  yecho "### Deploying to Hockey ###"
-  launch hockey https://alpha-app.newspring.io $METEOR_SETTINGS_PATH
+register_definition() {
+  if revision=$(aws ecs register-task-definition --container-definitions "$task_template" --family $ECS_FAMILY | $JQ '.taskDefinition.taskDefinitionArn'); then
+    yecho "Revision: $revision"
+  else
+    echo "Failed to register task definition"
+    return 1
+  fi
+}
+
+deploy_cluster() {
+  make_task_def
+  register_definition
+  if [ $(aws ecs update-service --cluster $ECS_CLUSTER --service $ECS_SERVICE --task-definition $revision | \
+               $JQ '.service.taskDefinition') != $revision ]; then
+    echo "Error updating service."
+    return 1
+  fi
+  return 0
+}
+
+deploy_cluster
+
+
+if [ "$DEST" = "web" ]; then
+  yecho "### $APP deployed to $ROOT_URL"
+  exit 0
 fi
 
-if [ "${CHANNEL}" == "beta" ]; then
-  ROOT_URL="https://${CHANNEL}-app.newspring.io" METEOR_SETTINGS_PATH="$TRAVIS_BUILD_DIR/sites/newspring/.remote/settings/sites/app.newspring.io/${CHANNEL}.settings.json" ECS_TASK_NAME=app ECS_CLUSTER=apollos ECS_FAMILY=app ECS_SERVICE=beta-app HOST_PORT=8072 BUNDLE_URL="http://ns.ops.s3.amazonaws.com/apollos/$CURRENT_TAG-$TRAVIS_COMMIT.tar.gz" $TRAVIS_BUILD_DIR/scripts/deploy/ecs.sh
+yecho "### Deploying app bundle ###"
+if [ "$CHANNEL" = "alpha" ]; then
+  yecho "### Deploying to Hockey ###"
+  launch hockey ROOT_URL $METEOR_SETTINGS_PATH
+fi
 
+if [ "$CHANNEL" = "beta" ]; then
   yecho "### Deploying to TestFlight ###"
-  launch testflight https://beta-app.newspring.io $METEOR_SETTINGS_PATH
+  launch testflight ROOT_URL $METEOR_SETTINGS_PATH
 fi
