745aca248fc0f9eacaeff52300aeee8a5baf5659
Sam Claridge <st.claridge@gmail.com>
2016-07-12 16:36:40 -0400

    add animations to css

--- apollos/src/core/components/live/live-css.js
+++ apollos/src/core/components/live/live-css.js
@@ -1,10 +1,32 @@
 import { StyleSheet } from "aphrodite";
 
+const keyframes = {
+  "0%": {
+    backgroundColor: "#6BAC43",
+  },
+  "50%": {
+    backgroundColor: "#1c683e",
+  },
+  "100%": {
+    backgroundColor: "#6BAC43",
+  },
+};
+
+const animation = {
+  animationDuration: "5s",
+  animationIterationCount: "infinite",
+  animationName: keyframes,
+  animationTimingFunction: "cubic-bezier(0.66, 0.21, 0.49, 0.88)",
+}
+
 export default StyleSheet.create({
   "live-float": {
     position: "absolute",
     top: "45px",
     width: "100%",
     zIndex: 1,
+  },
+  "live-banner": {
+    ...animation,
   },
 });
