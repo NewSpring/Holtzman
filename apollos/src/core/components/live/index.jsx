86a2c8bdd5677ba0d715ffa97949de15e84b97e0
Sam Claridge <st.claridge@gmail.com>
2016-07-13 13:04:23 -0400

    Live bar animations (#773)
    * add live-banner css reference
    
    * add animations to css
    
    * add text animations
    
    * move text color back to markup
    
    * simplify how animations are being called

--- apollos/src/core/components/live/index.jsx
+++ apollos/src/core/components/live/index.jsx
@@ -29,14 +29,25 @@ export default class Live extends Component {
 
   getClasses = () => {
     const classes = [
-      "background--secondary",
       "text-center",
       "soft-half-ends",
+      css(Styles["live-banner"])
     ];
 
     if (this.props.live.float) {
       classes.push(css(Styles["live-float"]));
     }
+
+    return classes.join(" ");
+  }
+
+  getTextClasses = () => {
+    const classes = [
+      "flush",
+      "hard",
+      "text-light-primary",
+      css(Styles["live-text"])
+    ];
 
     return classes.join(" ");
   }
@@ -57,7 +68,7 @@ export default class Live extends Component {
               className={this.getClasses()}
               style={interpolatingStyle}
             >
-              <h7 className="text-light-primary flush hard">
+              <h7 className={this.getTextClasses()}>
                 NewSpring Church Live, Watch Now!
               </h7>
             </div>
