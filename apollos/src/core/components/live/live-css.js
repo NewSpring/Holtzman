dcbcd6bf6be2a368cb538d6bed7668e1aa90aff9
Sam Claridge <st.claridge@gmail.com>
2016-07-13 13:04:23 -0400

    Live bar animations (#773)
    * add live-banner css reference
    
    * add animations to css
    
    * add text animations
    
    * move text color back to markup
    
    * simplify how animations are being called

--- apollos/src/core/components/live/live-css.js
+++ apollos/src/core/components/live/live-css.js
@@ -1,5 +1,37 @@
 import { StyleSheet } from "aphrodite";
 
+const keyframes = {
+  "0%": {
+    backgroundColor: "#5DA23D",
+  },
+  "50%": {
+    backgroundColor: "#3F803C",
+  },
+  "100%": {
+    backgroundColor: "#5DA23D",
+  },
+};
+
+const animation = {
+  animationDuration: "10s",
+  animationIterationCount: "infinite",
+  animationName: keyframes,
+  animationTimingFunction: "cubic-bezier(0.66, 0.21, 0.49, 0.88)",
+}
+
+const phaders = {
+  "from": {
+    opacity: 0,
+  }
+}
+
+const textAnimation = {
+  animationDuration: "1.5s",
+  animationIterationCount: "infinite",
+  animationName: phaders,
+  animationDirection: "alternate",
+}
+
 export default StyleSheet.create({
   "live-float": {
     position: "absolute",
@@ -7,4 +39,6 @@ export default StyleSheet.create({
     width: "100%",
     zIndex: 1,
   },
+  "live-banner": animation,
+  "live-text": textAnimation,
 });
