3a898c6e661af830901f9bd692795f24707c6812
benwiley <ben.wiley@newspring.cc>
2016-07-14 17:05:02 -0400

    Make live banner link to new videos page

---
+++ sites/newspring/imports/pages/video/index.jsx
@@ -0,0 +1,39 @@
+import { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+
+import { Video } from "/imports/components/players"
+
+import navActions from "apollos/dist/core/store/nav";
+
+@connect()
+class Template extends Component {
+
+  componentWillMount() {
+    this.props.dispatch(navActions.setLevel("BASIC_CONTENT"));
+  }
+
+  render() {
+    let { embedCode } = this.props.params;
+    // XXX for testing hardcode embed code
+    embedCode = "M1anI0ZDqWIIFq5VESbHl7Hok1zz0N83";
+
+    return (
+      <div className="locked-ends locked-sides background--dark-primary floating">
+        <div className="floating__item one-whole">
+          <Video id={embedCode} ref="video"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Routes = [
+  {
+    path: "/video/:embedCode",
+    component: Template,
+  },
+];
+
+export default {
+  Routes,
+};
