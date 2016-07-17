7c527eec1988fb50547f1bdf87635c15db515359
John Pinkerton <jpinkerton88@gmail.com>
2016-07-17 18:47:45 -0400

    remove logging

--- apollos/src/core/store/header/saga.js
+++ apollos/src/core/store/header/saga.js
@@ -7,32 +7,20 @@ const canRun = (
 );
 
 function* toggleHeader() {
-  console.log("toggleHeader");
   let { header } = yield select()
-  console.log(header);
-  console.log(header.statusBar);
   if (canRun) {
-    console.log("toggleHeader can run");
     if (!header.statusBar) StatusBar.hide()
     if (header.statusBar) StatusBar.show()
   }
 };
 
 function* setColor({ color }) {
-  console.log("setColor", color);
   if (canRun && color) StatusBar.backgroundColorByHexString(color);
 };
 
 function* setColorFromHeader() {
-  console.log("setColorFromHeader");
   let { header } = yield select()
-  console.log(header);
-  console.log(header.content.color);
-  console.log(canRun);
   if (canRun && header.content.color) {
-    console.log("setColorFromHeader can run");
-    console.log(header);
-    console.log(header.content.color);
     StatusBar.backgroundColorByHexString(header.content.color);
   }
 };
