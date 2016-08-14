6b86fdf5623d13e994968cc32aaf56560fdf23a6
James Baxley <james.baxley@newspring.cc>
2016-08-14 15:32:59 -0400

    introduce auto binding of sentry

--- sites/newspring/client/index.js
+++ sites/newspring/client/index.js
@@ -1,4 +1,6 @@
-
+if (Meteor.settings.public.sentry && typeof Raven != "undefined") {
+  Raven.config(Meteor.settings.public.sentry).install()
+}
 import { GraphQL } from "apollos-core/dist/core/graphql";
 import { run } from "apollos-core/dist/core/router/client";
 import { routes, client } from "/imports"
