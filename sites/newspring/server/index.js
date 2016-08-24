1589a0943fdb041e2b1baca0b39f7ed0ab1b71d2
James Baxley <james.baxley@newspring.cc>
2016-08-24 11:52:51 -0400

    setup bundle prefix

--- sites/newspring/server/index.js
+++ sites/newspring/server/index.js
@@ -1,16 +1,22 @@
 
 if (process.env.WEB && process.env.NODE_ENV === "production") {
 
+  // cdn party
+  if (Meteor.settings.cdnPrefix && __meteor_runtime_config__.ROOT_URL.match("localhost") === null) {
+    Meteor.startup(() => {
+      WebAppInternals.setBundledJsCssPrefix(Meteor.settings.cdnPrefix);
+    })
+  } else {
+    Meteor.startup(() => {
+      WebAppInternals.setBundledJsCssPrefix("/apollos");
+    })
+  }
+
   // load the application
   import { run } from "apollos-core/dist/core/router/server";
   import { routes, client, server } from "/imports";
 
   run(routes, client, server);
 
-  // cdn party
-  if (Meteor.settings.cdnPrefix && __meteor_runtime_config__.ROOT_URL.match("localhost") === null) {
-    Meteor.startup(() => {
-      WebAppInternals.setBundledJsCssPrefix(Meteor.settings.cdnPrefix);
-    })
-  }
+
 }
