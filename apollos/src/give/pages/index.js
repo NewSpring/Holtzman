07ae68ad400c654dd917efbc664c1fdecca8e510
John Pinkerton <jpinkerton88@gmail.com>
2016-08-30 17:14:01 -0400

    redirect last part of giving to safari on iOS

--- apollos/src/give/pages/index.js
+++ apollos/src/give/pages/index.js
@@ -1,16 +1,18 @@
 
 
-import Now from "./now"
-import Campaign from "./campaign"
-import History from "./history"
-import Schedules from "./schedules"
+import Now from "./now";
+import Campaign from "./campaign";
+import History from "./history";
+import Schedules from "./schedules";
+import Review from "./review";
 
 const Routes = [].concat(
   Now.Routes,
   Campaign.Routes,
   History.Routes,
-  Schedules.Routes
-)
+  Schedules.Routes,
+  Review.Routes
+);
 
 
 export default {
@@ -18,8 +20,9 @@ export default {
   Campaign,
   History,
   Schedules,
+  Review,
 
   // combined export of app routes
-  Routes
+  Routes,
 
-}
+};
