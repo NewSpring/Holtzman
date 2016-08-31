09ca86d474bb872c9e03c363cb899c7729f931ce
John Pinkerton <jpinkerton88@gmail.com>
2016-08-31 16:38:58 -0400

    simplify success and error give pages for ios review

--- apollos/src/give/blocks/Give/Err.jsx
+++ apollos/src/give/blocks/Give/Err.jsx
@@ -1,7 +1,7 @@
 
 import { Error } from "../../../core/components/icons"
 
-const Err = ({ msg, goToStepOne }) => (
+const Err = ({ msg, goToStepOne, additionalMessage }) => (
   <div className="soft soft-double-ends push-double-top one-whole text-center">
     <div className="push-double-top">
       <Error />
@@ -9,11 +9,23 @@ const Err = ({ msg, goToStepOne }) => (
       <p className="text-left">
         {msg}
       </p>
-      <div className="one-whole text-center soft-ends">
-        <button onClick={goToStepOne} className="btn--small btn--dark-tertiary one-whole">
-          Try Again
-        </button>
-      </div>
+
+      {(({ goToStepOne }) => {
+        if (!goToStepOne) return;
+        return (
+          <div className="one-whole text-center soft-ends">
+            <button onClick={goToStepOne} className="btn--small btn--dark-tertiary one-whole">
+              Try Again
+            </button>
+          </div>
+        );
+      })({ goToStepOne })}
+
+      {(({ additionalMessage }) => {
+        if (!additionalMessage) return;
+        return <h5>{additionalMessage}</h5>;
+      })({ additionalMessage })}
+
       <p className="test-dark-tertiary text-left"><em>
         If you would like a member of our customer support team to follow up with you regarding this error, click <a target="_blank" href="//rock.newspring.cc/workflows/152?Topic=Stewardship">here</a>
       </em></p>
