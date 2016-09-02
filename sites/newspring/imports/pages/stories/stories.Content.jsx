a7d0d02ec295b4cafa08c0aef0a40c819a97986c
James Baxley <james.baxley@newspring.cc>
2016-09-01 22:26:49 -0400

    small editorial tweaks (#917)

--- sites/newspring/imports/pages/stories/stories.Content.jsx
+++ sites/newspring/imports/pages/stories/stories.Content.jsx
@@ -5,10 +5,11 @@ import Components from "/imports/components";
 import SingleVideoPlayer from "/imports/components/players/video/Player";
 import RelatedContent from "/imports/blocks/content/RelatedContent";
 
+const ratio = window.isTablet ? "2:1" : "1:1"
 const StoryImage = ({ story }) => (
   <div
-    className="one-whole ratio--square background--fill"
-    style={Helpers.backgrounds.styles(story)}>
+    className="one-whole ratio--square ratio--landscape@palm-wide-and-up background--fill"
+    style={Helpers.backgrounds.styles(story, ratio)}>
   </div>
 );
 
@@ -28,7 +29,7 @@ export default class StoriesContent extends Component {
            if (story.content.ooyalaId.length === 0) return <StoryImage story={story} />;
             return <SingleVideoPlayer ooyalaId={story.content.ooyalaId} />;
           })()}
-          <div className="soft push-top">
+          <div className="soft soft-double-sides@palm-wide-and-up push-top">
             <h2 className="capitalize">{story.title}</h2>
             <h4 className="text-dark-tertiary">{story.subtitle}</h4>
             <div dangerouslySetInnerHTML={Helpers.react.markup(story)}></div>
