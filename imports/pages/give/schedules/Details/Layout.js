bf83fd35ed254e346b538e851acf1327b25d63cc
James Baxley <james.baxley@newspring.cc>
2016-09-11 19:21:39 -0400

    fix styling on back button for WEB

--- imports/pages/give/schedules/Details/Layout.js
+++ imports/pages/give/schedules/Details/Layout.js
@@ -80,7 +80,7 @@ export default class Layout extends Component {
                   return (
                     <Link to="/give/schedules" className="locked-top locked-left soft-double@lap-and-up soft h7 text-dark-secondary plain" >
                       <i className="icon-arrow-back soft-half-right display-inline-block" style={{verticalAlign: "middle"}} />
-                      <span className="display-inline-block" style={{verticalAlign: "middle", marginBottom: "2px"}}>Back</span>
+                      <span className="display-inline-block" style={{verticalAlign: "middle", marginTop: "3px"}}>Back</span>
                     </Link>
                   );
                 }
