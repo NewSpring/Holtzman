501c353de733486db2ff159e2556de9a5fd1de99
James Baxley <james.baxley@newspring.cc>
2016-09-13 19:25:32 -0400

    add back in recover

--- imports/pages/give/schedules/Recover/Layout.js
+++ imports/pages/give/schedules/Recover/Layout.js
@@ -106,7 +106,7 @@ export default class Layout extends Component {
           <Left scroll classes={recoverableSchedules.length ? ["background--light-secondary"] : ["background--light-primary"]} ref="container">
             <Link to="/give/schedules" className="locked-top locked-left soft-double@lap-and-up soft h7 text-dark-secondary plain" >
               <i className="icon-arrow-back soft-half-right display-inline-block" style={{verticalAlign: "middle"}} />
-              <span className="display-inline-block" style={{verticalAlign: "middle", marginBottom: "2px"}}>Back</span>
+              <span className="display-inline-block" style={{verticalAlign: "middle", marginTop: "5px"}}>Back</span>
             </Link>
 
             {(() => {
