a468f12ba59589d156bb7b45ce8b80be9822d347
John Pinkerton <jpinkerton88@gmail.com>
2016-09-14 00:59:30 -0400

    lint imports/util/scriptures

--- imports/util/scriptures/scriptures.list.js
+++ imports/util/scriptures/scriptures.list.js
@@ -2,14 +2,12 @@
 
 function scriptureList(contentItem, { commas = true } = {}) {
   const scriptures = contentItem.content.scripture;
-  if (!scriptures && commas) return;
+  if (!scriptures && commas) return [];
   if (!scriptures) return [];
 
-  const { book, passage } = scriptures;
-
-  const combo = scriptures.map(({ book, passage }, i) => {
-    return `${book} ${passage}`;
-  });
+  const combo = scriptures.map(({ book, passage }) => (
+    `${book} ${passage}`
+  ));
 
   if (commas) return combo.join(", ");
   return combo;
