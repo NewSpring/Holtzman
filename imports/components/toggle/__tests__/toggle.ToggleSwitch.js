03c93a0aa2f6a05ce48c374b60fd92a482658471
Jake Dawkins <dawkinsjh@gmail.com>
2016-09-14 16:07:15 -0400

    added toggle tests and snaphot

---
+++ imports/components/toggle/__tests__/toggle.ToggleSwitch.js
@@ -0,0 +1,18 @@
+import ToggleSwitch from "../toggle.ToggleSwitch";
+import renderer from 'react-test-renderer';
+
+describe("Toggle Switch", () => {
+  it("should exist", () => {
+    const toggle = renderer.create(
+      <ToggleSwitch followingId="test" />
+    );
+    expect(toggle).toBeDefined();
+  });
+
+  it("should match snapshot", () => {
+    const toggle = renderer.create(
+      <ToggleSwitch followingId="test" />
+    );
+    expect(toggle).toMatchSnapshot();
+  });
+});
