89adb64d3de0826891873375aeb161587ffc65fe
James Baxley <james.baxley@newspring.cc>
2016-09-14 22:34:59 -0400

    add missing headers

--- imports/pages/locations/index.js
+++ imports/pages/locations/index.js
@@ -1,7 +1,10 @@
+import { Component } from"react";
 import { connect } from "react-apollo";
 import gql from "graphql-tag";
 
 import Layout from "./Layout";
+
+import { header as headerActions } from "../../store";
 
 const mapQueriesToProps = () => ({
   data: {
@@ -26,7 +29,27 @@ const mapQueriesToProps = () => ({
   },
 });
 
-const Template = connect({ mapQueriesToProps })(Layout);
+class LayoutWithData extends Component {
+  componentDidMount(){
+    if (process.env.NATIVE) {
+      const item = {
+        title: "Locations",
+      };
+
+      this.props.dispatch(headerActions.set(item));
+      this.setState({
+        __headerSet: true,
+      });
+    }
+
+  }
+
+  render() {
+    return <Layout {...this.props} />;
+  }
+}
+
+const Template = connect({ mapQueriesToProps })(LayoutWithData);
 
 const Routes = [
   {
