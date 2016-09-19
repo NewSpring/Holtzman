65dca81c902a7b648045e8e3edcc404ec59f3dc8
John Pinkerton <jpinkerton88@gmail.com>
2016-09-19 15:02:37 -0400

    lint settings change password layout

--- imports/pages/profile/settings/ChangePassword/Layout.js
+++ imports/pages/profile/settings/ChangePassword/Layout.js
@@ -1,3 +1,4 @@
+import { PropTypes } from "react";
 import Forms from "../../../../components/forms";
 import Meta from "../../../../components/meta";
 
@@ -8,51 +9,56 @@ const styles = {
   zIndex: 1,
 };
 
-const Layout = ({ submit, save, state }, context) => (
+const Layout = ({ submit, save, state }) => (
   <div
-    className="background--light-primary one-whole text-center push-top push-double-top@lap-and-up push-double-bottom "
+    className={
+      "background--light-primary one-whole text-center " +
+      "push-top push-double-top@lap-and-up push-double-bottom"
+    }
     style={styles}
   >
     <Back />
     <Meta title="Change your password" />
-      <Forms.Form
-        id="reset-password"
-        classes={["soft", "one-whole", "two-thirds@portable", "one-half@anchored", "display-inline-block", "soft-double-sides@palm-wide"]}
-        submit={submit}
-      >
-        <div className="push-double">
-          <h4 className="text-center">
-            Change Password
-          </h4>
-        </div>
+    <Forms.Form
+      id="reset-password"
+      classes={[
+        "soft",
+        "one-whole",
+        "two-thirds@portable",
+        "one-half@anchored",
+        "display-inline-block",
+        "soft-double-sides@palm-wide",
+      ]}
+      submit={submit}
+    >
+      <div className="push-double">
+        <h4 className="text-center">
+          Change Password
+        </h4>
+      </div>
 
-        <Forms.Input
-          name="current"
-          label="Current Password"
-          validation={save}
-          type="password"
-        />
+      <Forms.Input
+        name="current"
+        label="Current Password"
+        validation={save}
+        type="password"
+      />
 
-        <Forms.Input
-          name="newP"
-          label="New Password"
-          validation={save}
-          errorText="New password does not match"
-          type="password"
-        />
+      <Forms.Input
+        name="newP"
+        label="New Password"
+        validation={save}
+        errorText="New password does not match"
+        type="password"
+      />
 
-        <Forms.Input
-          name="newPDup"
-          label="Repeat New Password"
-          validation={save}
-          errorText="New password does not match"
-          type="password"
-        />
-      {/*
-        <Link to="/profile/settings" tabIndex={-1} className="btn--small btn--dark-tertiary display-inline-block">
-          Back
-        </Link>
-         */}
+      <Forms.Input
+        name="newPDup"
+        label="Repeat New Password"
+        validation={save}
+        errorText="New password does not match"
+        type="password"
+      />
         {(() => {
           const btnClasses = [];
           let disabled = true;
@@ -73,5 +79,11 @@ const Layout = ({ submit, save, state }, context) => (
       </Forms.Form>
     </div>
 );
+
+Layout.propTypes = {
+  submit: PropTypes.function.isRequired,
+  save: PropTypes.function.isRequired,
+  state: PropTypes.object.isRequired,
+};
 
 export default Layout;
