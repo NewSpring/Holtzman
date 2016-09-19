def4194a7a74005bf8761a2536d1617e8d3472e6
John Pinkerton <jpinkerton88@gmail.com>
2016-09-19 14:54:35 -0400

    lint profile settings layout

--- imports/pages/profile/settings/Layout.js
+++ imports/pages/profile/settings/Layout.js
@@ -1,14 +1,7 @@
 import { PropTypes } from "react";
-import { Link } from "react-router";
 
 import Split, { Left, Right } from "../../../blocks/split";
 import Meta from "../../../components/meta";
-
-const Close = () => (
-  <Link to="/profile" className="text-light-primary plain soft overlay__item locked-top locked-right">
-    <i className="icon-close h4" />
-  </Link>
-);
 
 const Layout = ({ person, children, mobile, onUpload }) => (
   <div>
@@ -16,7 +9,7 @@ const Layout = ({ person, children, mobile, onUpload }) => (
       title={person.nickName ? `${person.nickName} ${person.lastName}` : "Profile"}
       image={person.photo ? person.photo : null}
     />
-  <Split nav classes={mobile ? ["background--light-secondary"] : ["background--light-primary"]}>
+    <Split nav classes={mobile ? ["background--light-secondary"] : ["background--light-primary"]}>
       <Right
         mobile={mobile}
         classes={["floating", "overlay--solid-dark"]}
@@ -25,14 +18,31 @@ const Layout = ({ person, children, mobile, onUpload }) => (
         blur
       >
         <div className="soft one-whole">
-          <label htmlFor="file"
+          <label
+            htmlFor="file"
             className="background--fill ratio--square round two-fifths display-inline-block"
-            style={{ backgroundImage: `url('${person.photo ? person.photo : ""}')`, position: "relative" }}
+            style={{
+              backgroundImage: `url('${person.photo ? person.photo : ""}')`,
+              position: "relative",
+            }}
           >
-            <input onChange={onUpload} type="file" className="locked-ends locked-sides" style={{ opacity: 0 }} />
+            <input
+              onChange={onUpload}
+              type="file"
+              className="locked-ends locked-sides"
+              style={{ opacity: 0 }}
+            />
           </label>
-        <h4 className="text-light-primary soft-half-top flush-bottom">{person.nickName || person.firstName} {person.lastName}</h4>
-          <p className="text-light-primary flush"><em>{person.home && person.home.city}, {person.home && person.home.state}</em></p>
+          <h4
+            className="text-light-primary soft-half-top flush-bottom"
+          >
+            {person.nickName || person.firstName} {person.lastName}
+          </h4>
+          <p
+            className="text-light-primary flush"
+          >
+            <em>{person.home && person.home.city}, {person.home && person.home.state}</em>
+          </p>
         </div>
 
       </Right>
@@ -47,8 +57,11 @@ const Layout = ({ person, children, mobile, onUpload }) => (
 );
 
 Layout.propTypes = {
-  photo: PropTypes.string,
-  person: PropTypes.object,
+  photo: PropTypes.string.isRequired,
+  person: PropTypes.object.isRequired,
+  children: PropTypes.object.isRequired,
+  mobile: PropTypes.boolean.isRequired,
+  onUpload: PropTypes.function.isRequired,
 };
 
 export default Layout;
