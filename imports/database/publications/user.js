b90d2237768729802a2a07353f85a9e5dee37f59
James Baxley <james.baxley@newspring.cc>
2016-09-20 11:59:56 -0400

    lint database folder (#1152)

--- imports/database/publications/user.js
+++ imports/database/publications/user.js
@@ -2,7 +2,7 @@ import { Meteor } from "meteor/meteor";
 
 if (Meteor.isServer) {
   // must use function because arrow version doesn't like 'this'
-  Meteor.publish("userData", function () {
+  Meteor.publish("userData", function userPublish() {
     return Meteor.users.find(this.userId,
       {
         fields: {
