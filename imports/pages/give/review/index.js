d14c55af9f5ba36b6141d5216d4b8ed9faf21af4
John Pinkerton <jpinkerton88@gmail.com>
2016-09-20 22:08:53 -0400

    lint give reivew index

--- imports/pages/give/review/index.js
+++ imports/pages/give/review/index.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, PropTypes } from "react";
 import { connect } from "react-apollo";
 import Layout from "./Layout";
 
@@ -13,6 +13,11 @@ const mapStateToProps = state => ({
 });
 @connect({ mapStateToProps })
 class Template extends Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    give: PropTypes.object.isRequired,
+  }
 
   componentWillMount() {
     const giveData = this.getGiveData();
@@ -33,6 +38,11 @@ class Template extends Component {
 
   }
 
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.dispatch(giveActions.submit());
+  }
+
   getGiveData = () => {
     try {
       const queryString = {};
@@ -51,40 +61,35 @@ class Template extends Component {
         } else {
           queryString[pair[0]].push(decodeURIComponent(pair[1]));
         }
+        return null;
       });
       return JSON.parse(queryString.giveData);
-    } catch (error) { }
+    } catch (error) { /* do nothing */ }
+    return null;
   }
 
   monentize = (value, fixed) => {
-    if (typeof value === "number") {
-      value = `${value}`;
-    }
+    let strVal = typeof value === "number" ? `${value}` : value;
 
-    if (!value.length) {
+    if (!strVal.length) {
       return "$0.00";
     }
 
-    value = value.replace(/[^\d.-]/g, "");
+    strVal = strVal.replace(/[^\d.-]/g, "");
 
-    const decimals = value.split(".")[1];
+    const decimals = strVal.split(".")[1];
     if ((decimals && decimals.length >= 2) || fixed) {
-      value = Number(value).toFixed(2);
-      value = String(value);
+      strVal = Number(strVal).toFixed(2);
+      strVal = String(strVal);
     }
 
-    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return `$${value}`;
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.props.dispatch(giveActions.submit());
+    strVal = strVal.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `$${strVal}`;
   }
 
   render() {
     const giveData = this.getGiveData();
-    const { state, errors, total, data, transactionType } = this.props.give;
+    const { state, errors, total, data } = this.props.give;
 
     switch (state) {
       case "loading":
@@ -92,14 +97,18 @@ class Template extends Component {
       case "error":
         return (<Err
           msg={errors[Object.keys(errors)[0]].error}
-          additionalMessage="Please click 'Done' in the top left of your screen to get back to the app"
+          additionalMessage={
+            "Please click 'Done' in the top left of your screen to get back to the app"
+          }
         />);
       case "success":
         return (<Success
           total={this.monentize(total.toFixed(2))}
           email={data.personal.email}
           guest={false} // prevent showing giving history
-          additionalMessage="Please click 'Done' in the top left of your screen to get back to the app"
+          additionalMessage={
+            "Please click 'Done' in the top left of your screen to get back to the app"
+          }
         />);
       default:
         return <Layout {...giveData} onSubmit={this.onSubmit} />;
