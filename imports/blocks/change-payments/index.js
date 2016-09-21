37c2651ffe7e7903015f77d517692da4ab7e480c
delianides <github@delianides.com>
2016-09-21 11:49:03 -0400

    blocks/change-payments

--- imports/blocks/change-payments/index.js
+++ imports/blocks/change-payments/index.js
@@ -1,10 +1,7 @@
 import { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 
-import { give as giveActions } from "../../store";
-import {
-  modal,
-} from "../../store";
+import { modal, give as giveActions } from "../../store";
 
 import AccountType from "../../components/accountType";
 
@@ -14,17 +11,21 @@ const map = state => ({
 @connect(map)
 export default class ChangePayments extends Component {
 
+  propTypes = {
+    dispatch: PropTypes.func,
+    savedAccounts: PropTypes.object, // eslint-disable-line
+    currentAccount: PropTypes.object, // eslint-disable-line
+  }
+
   state = {
     savedAccount: null,
   }
 
-  changePaymentHeader = () => {
-    return (
-      <h4 className="text-center flush-bottom">
-        Change Payment Account
-      </h4>
-    );
-  }
+  changePaymentHeader = () => (
+    <h4 className="text-center flush-bottom">
+      Change Payment Account
+    </h4>
+  );
 
 
   changeAccounts = (e) => {
@@ -57,6 +58,7 @@ export default class ChangePayments extends Component {
       selectedAccount = this.state.savedAccount;
     }
 
+    /* eslint-disable */
     return (
       <div className="soft-double-top">
         <div className="soft-sides flush-bottom push-double-top@lap-and-up">
@@ -64,52 +66,54 @@ export default class ChangePayments extends Component {
         </div>
 
         <div className="soft">
-          {this.props.savedAccounts.map((account, key) => {
-            return (
-              <div key={key} style={{ position: "relative", cursor: "pointer" }} id={account.id} onClick={this.choose}>
-                <div className="soft-ends push-double-left text-left hard-right outlined--light outlined--bottom relative">
+          { this.props.savedAccounts.map((account, key) => (
+            <div key={key} style={{ position: "relative", cursor: "pointer" }} id={account.id} onClick={this.choose}>
+              <div className="soft-ends push-double-left text-left hard-right outlined--light outlined--bottom relative">
 
-                  <div className="display-inline-block soft-half-ends one-whole">
-                    <h6 className="flush-bottom float-left text-dark-tertiary">{account.name}</h6>
-                    {/* <button className="h6 flush-bottom float-right text-primary" id={account.id} onClick={this.choose}>Choose</button>*/}
-                  </div>
-
-
-                  <h5 className="hard one-whole flush-bottom text-dark-tertiary">
-                    {account.payment.accountNumber.slice(0, account.payment.accountNumber.length - 5).replace(/./gmi, "*")}{account.payment.accountNumber.slice(-4)}
-                    <span className="float-right soft-half-left">
-                      <AccountType width="40px" height="25px" type={account.payment.paymentType} />
-
-                    </span>
-
-                  </h5>
-
-
+                <div className="display-inline-block soft-half-ends one-whole">
+                  <h6 className="flush-bottom float-left text-dark-tertiary">{account.name}</h6>
+                  {/* <button className="h6 flush-bottom float-right text-primary" id={account.id}
+                    onClick={this.choose}>Choose</button>*/}
                 </div>
-                <div className="locked-ends locked-sides">
-                  <input
-                    type="checkbox"
-                    id={"label" + account.id}
-                    readOnly
-                    checked={Number(account.id) === Number(selectedAccount.id)}
-                    style={{
-                      opacity: 0,
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      padding: "50px",
-                    }}
-                  />
-                <label htmlFor={"label" + account.id} style={{
-                  position: "absolute",
-                  top: "50%",
-                  left: 0,
-                }} />
-                </div>
+
+
+                <h5 className="hard one-whole flush-bottom text-dark-tertiary">
+                  {account.payment.accountNumber.slice(0, account.payment.accountNumber.length - 5).replace(/./gmi, "*")}{account.payment.accountNumber.slice(-4)}
+                  <span className="float-right soft-half-left">
+                    <AccountType width="40px" height="25px" type={account.payment.paymentType} />
+
+                  </span>
+
+                </h5>
+
+
               </div>
-            );
-          })}
-
+              <div className="locked-ends locked-sides">
+                <input
+                  type="checkbox"
+                  id={`label${account.id}`}
+                  readOnly
+                  checked={Number(account.id) === Number(selectedAccount.id)}
+                  style={{
+                    opacity: 0,
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    padding: "50px",
+                  }}
+                />
+                <label
+                  htmlFor={`label${account.id}`}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: 0,
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+          /* eslint-enable */
           <button className="btn one-whole push-double-top soft-sides push-bottom" onClick={this.changeAccounts}>
             Change Account
           </button>
@@ -121,10 +125,7 @@ export default class ChangePayments extends Component {
               </em>
             </small>
           </p>
-
         </div>
-
-
       </div>
     );
   }
