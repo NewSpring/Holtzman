bdaa0177839f998c7afcc4479ed28c0ea8444b12
John Pinkerton <jpinkerton88@gmail.com>
2016-09-20 23:58:00 -0400

    import run correctly

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 /* global Raven */
 import { Meteor } from "meteor/meteor";
-import { run } from "/imports/router/client";
+import run from "/imports/router/client";
 import { routes, client } from "/imports";
 
 if (Meteor.settings.public.sentry && typeof Raven !== "undefined") {
