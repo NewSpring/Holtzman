e48e4dbc511796d862c61a3d4719e9ead0a8dd87
John Pinkerton <jpinkerton88@gmail.com>
2016-09-21 09:59:36 -0400

    lint router inject data

--- imports/router/client/inject-data.js
+++ imports/router/client/inject-data.js
@@ -15,12 +15,12 @@ Meteor.startup(() => {
   // var injectedDataString = $.trim(dom.text());
   const dom = document.querySelectorAll("script[type=\"text/inject-data\"]")[0];
   const injectedDataString = dom && dom.innerText ? dom.innerText.trim() : "";
-  InjectData._data = InjectData._decode(injectedDataString) || {};
+  InjectData._data = InjectData._decode(injectedDataString) || {}; // eslint-disable-line
 });
 
-InjectData.getData = function (key, callback) {
+InjectData.getData = function getData(key, callback) {
   Meteor.startup(() => {
-    callback(InjectData._data[key]);
+    callback(InjectData._data[key]); // eslint-disable-line
   });
 };
 
