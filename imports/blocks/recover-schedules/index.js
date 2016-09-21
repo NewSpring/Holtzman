b78a5f52f4cefac28d41526c37594a6c248f3498
John Pinkerton <jpinkerton88@gmail.com>
2016-09-21 16:13:41 -0400

    lint recorver shcedules index

--- imports/blocks/recover-schedules/index.js
+++ imports/blocks/recover-schedules/index.js
@@ -1,7 +1,6 @@
 import { Component, PropTypes } from "react";
 import { connect } from "react-redux";
-import ReactDOM from "react-dom";
-import Moment from "moment";
+import moment from "moment";
 
 import modalActions from "../../store/modal";
 import giveActions from "../../store/give";
@@ -14,14 +13,13 @@ const map = store => ({ give: store.give });
 @connect(map)
 export default class RecoverSchedules extends Component {
 
-  state = {
-    state: "default", // default, remind, later
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    give: PropTypes.object,
   }
 
-  back = (e) => {
-    e.preventDefault();
-
-    this.setState({ state: "default" });
+  state = {
+    state: "default", // default, remind, later
   }
 
   onRemind = (e) => {
@@ -31,9 +29,9 @@ export default class RecoverSchedules extends Component {
     const value = input.value;
 
     const time = {
-      tomorrow: Moment().add(1, "days").toDate(),
-      nextWeek: Moment().add(7, "days").toDate(),
-      twoWeeks: Moment().add(14, "days").toDate(),
+      tomorrow: moment().add(1, "days").toDate(),
+      nextWeek: moment().add(7, "days").toDate(),
+      twoWeeks: moment().add(14, "days").toDate(),
     };
 
     Meteor.users.update({ _id: Meteor.userId() }, {
@@ -41,12 +39,19 @@ export default class RecoverSchedules extends Component {
         "profile.reminderDate": time[value],
       },
     }, (err, result) => {
+      // eslint-disable-next-line no-console
       console.log(err, result);
     });
 
     this.props.dispatch(giveActions.setReminder(time[value]));
     this.props.dispatch(giveActions.clearData());
     this.setState({ state: "later" });
+  }
+
+  back = (e) => {
+    e.preventDefault();
+
+    this.setState({ state: "default" });
   }
 
 
@@ -60,6 +65,7 @@ export default class RecoverSchedules extends Component {
 
     const arr = [];
     const { recoverableSchedules, reminderDate } = this.props.give;
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
     for (const schedule in recoverableSchedules) {
       arr.push(recoverableSchedules[schedule]);
     }
