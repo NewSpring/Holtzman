8b1cce0b970bbd30ac7af13b7be4101f0f7bbbd4
John Pinkerton <jpinkerton88@gmail.com>
2016-09-21 13:04:56 -0400

    lint store paging reducer

--- imports/store/paging/reducer.js
+++ imports/store/paging/reducer.js
@@ -9,17 +9,26 @@ const initial = {
 export default function paging(state = initial, action) {
   switch (action.type) {
     case "PAGING.INCREMENT":
-      return { ...state, ...{
-        page: state.page + 1,
-      } };
+      return {
+        ...state,
+        ...{
+          page: state.page + 1,
+        },
+      };
     case "PAGING.PAUSE":
-      return { ...state, ...{
-        shouldUpdate: false,
-      } };
+      return {
+        ...state,
+        ...{
+          shouldUpdate: false,
+        },
+      };
     case "PAGING.RESUME":
-      return { ...state, ...{
-        shouldUpdate: true,
-      } };
+      return {
+        ...state,
+        ...{
+          shouldUpdate: true,
+        },
+      };
     case "PAGING.RESET":
       return initial;
     default:
