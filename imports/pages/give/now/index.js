8ae56373e55ed466b6a345979e15f8648474c1df
John Pinkerton <jpinkerton88@gmail.com>
2016-09-20 21:56:01 -0400

    lint give now index

--- imports/pages/give/now/index.js
+++ imports/pages/give/now/index.js
@@ -1,6 +1,4 @@
-import { Component, PropTypes } from "react";
 import { connect } from "react-apollo";
-import ReactMixin from "react-mixin";
 import gql from "graphql-tag";
 
 import { createContainer } from "../../../blocks/meteor/react-meteor-data";
@@ -49,7 +47,7 @@ class Page extends Component {
 // Bind reactive data to component
 const TemplateWithData = createContainer(() => {
   let alive = true;
-  try { alive = serverWatch.isAlive("ROCK"); } catch (e) {}
+  try { alive = serverWatch.isAlive("ROCK"); } catch (e) { /* do nothing */ }
   return { alive };
 },
   connect({ mapQueriesToProps })(props => <Layout {...props} />)
