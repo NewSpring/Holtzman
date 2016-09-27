8946b5679175d117da39fc396eecb03dcc826379
Jake Dawkins <dawkinsjh@gmail.com>
2016-09-27 11:18:48 -0400

    refactored discover feed

--- imports/blocks/discover/feed/index.js
+++ imports/blocks/discover/feed/index.js
@@ -1,39 +1,44 @@
 import { Component, PropTypes } from "react";
-import { connect } from "react-apollo";
+import { graphql } from "react-apollo";
+import { connect } from "react-redux";
 import gql from "graphql-tag";
 
 import Layout from "./Layout";
 
-const mapQueriesToProps = () => ({
-  discover: {
-    query: gql`
-      query GetPromotions($setName: String!) {
-        items: lowReorderSets(setName: $setName) {
-          title
-          id
-          status
-          meta {
-            urlTitle
-            date
-          }
-          content {
-            images(sizes: ["large"]) {
-              fileName
-              fileLabel
-              url
-            }
-          }
+const DISCOVER_QUERY = gql`
+  query GetPromotions($setName: String!) {
+    items: lowReorderSets(setName: $setName) {
+      title
+      id
+      status
+      meta {
+        urlTitle
+        date
+      }
+      content {
+        images(sizes: ["large"]) {
+          fileName
+          fileLabel
+          url
         }
       }
-    `,
+    }
+  }
+`;
+
+const withDiscover = graphql(DISCOVER_QUERY, {
+  name: "discover",
+  options: ownProps => ({
     variables: {
       // XXX if we want app specfic promos
       // setName: proccess.env.WEB ? "promotions_newspring" : "promotions_newspring_app"
       setName: "promotions_newspring",
     },
-  },
+  }),
 });
-@connect({ mapQueriesToProps })
+
+@connect()
+@withDiscover
 export default class Discover extends Component {
 
   static propTypes = {
