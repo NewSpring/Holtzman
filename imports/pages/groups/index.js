1dd71e5538d1ee3b018fe9b116a20d797dc059f3
delianides <github@delianides.com>
2016-09-27 15:08:54 -0400

    gql query was commented out but I converted anyway :bomb:

--- imports/pages/groups/index.js
+++ imports/pages/groups/index.js
@@ -1,31 +1,34 @@
 import Profile from "./profile/index";
 import Finder from "./finder/index";
 
-// const mapQueriesToProps = () => ({
-//   data: {
-//     query: gql`
-//       query GetTransactions($limit: Int, $skip: Int) {
-//         transactions(limit: $limit, skip: $skip) {
+// const TRANSACTIONS_QUERY = gql`
+//   query GetTransactions($limit: Int, $skip: Int) {
+//     transactions(limit: $limit, skip: $skip) {
+//       id
+//       date
+//       status
+//       summary
+//       details {
+//         id
+//         amount
+//         account {
 //           id
-//           date
-//           status
-//           summary
-//           details {
-//             id
-//             amount
-//             account {
-//               id
-//               name
-//             }
-//           }
+//           name
 //         }
 //       }
-//     `,
+//     }
+//   }
+// `;
+
+// const withTransactions(TRANSACTIONS_QUERY, {
+//   options: {
 //     variables: { limit: 20, skip: 0 }
 //   },
 // });
+
 // const defaultArray = [];
-// @connect({ mapQueriesToProps })
+// withTransactions
+// @connect()
 const Template = () => (
   <h1>Lets find some groups!</h1>
 );
