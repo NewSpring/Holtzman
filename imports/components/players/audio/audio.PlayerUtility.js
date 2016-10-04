d062bd8e978270926454c7eb61164d15f0d46153
delianides <github@delianides.com>
2016-10-04 16:08:47 -0400

    not supposed to be commented out

--- imports/components/players/audio/audio.PlayerUtility.js
+++ imports/components/players/audio/audio.PlayerUtility.js
@@ -84,7 +84,7 @@ export default class AudioPlayerUtility extends Component {
 
     if (this.player && this.player.stop) {
       this.player.stop();
-      // this.player.release();
+      this.player.release();
     }
 
     if (track.file.indexOf("http") === -1) {
@@ -175,7 +175,7 @@ export default class AudioPlayerUtility extends Component {
   }
 
   next = () => {
-    // this.player.release();
+    this.player.release();
 
     const { playing, order, repeat } = this.props.audio;
     const playlist = this.tracksWithFiles();
@@ -225,7 +225,7 @@ export default class AudioPlayerUtility extends Component {
   }
 
   previous = () => {
-    // this.player.release();
+    this.player.release();
 
     const { playing, order, repeat } = this.props.audio;
     const playlist = this.tracksWithFiles();
