73d2ff988383220b112c66bf8d724ec9d530d5e6
James Baxley <james.baxley@newspring.cc>
2016-10-26 16:19:31 -0400

    Storybook enhancements (#1375)
    * added comments
    
    * allow custom options

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,6 @@
 import "react-storybook-addon-backgrounds/register";
-// import "@kadira/storybook-addon-options/register";
-// import "@kadira/storybook/addons";
+import "@kadira/storybook-addon-options/register";
 import "@kadira/storybook-addon-knobs/register";
 import "storybook-readme/register";
+import "@kadira/storybook-database-cloud/register";
+import "@kadira/storybook-addon-comments/register";
