a43b86893dd95ed5ee427a4b1ce3835650f71697
James Baxley <james.baxley@newspring.cc>
2016-10-26 15:54:27 -0400

    added comments

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,3 +3,5 @@ import "react-storybook-addon-backgrounds/register";
 // import "@kadira/storybook/addons";
 import "@kadira/storybook-addon-knobs/register";
 import "storybook-readme/register";
+import "@kadira/storybook-database-cloud/register";
+import "@kadira/storybook-addon-comments/register";
