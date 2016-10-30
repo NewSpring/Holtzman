e852042fc44c89b185a631a522dbb738aa3f9da5
Sam Claridge <st.claridge@gmail.com>
2016-10-30 13:02:29 -0400

    add markup with knobs

--- imports/components/cards/SideBySide.story.js
+++ imports/components/cards/SideBySide.story.js
@@ -16,9 +16,27 @@ const story = storiesOf("Cards", module)
   ;
 
 story
-  .add("Side by side", withReadme(Readme, () => <SideBySide />));
+  .add("Side by side", withReadme(
+    Readme,
+    () => {
+      const title = text('Title', 'Step Up Fund');
+      const summary = text('Summary', 'In 2013 we began a new giving campaign as a part of our mission to reach 100,000 people. Step Up exists to put permanent facilities all over South Carolina and create campuses in more cities so that thousands of people will have a place to hear about Jesus!');
+      const button = text('Button', 'Learn More');
+      const url = text('Image', "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Profile_Mario.aead314d435d8e52d9a4e92a6f799c4eee08081e.jpg");
+      const images = [
+        { fileLabel: "2:1", url },
+        { fileLabel: "1:2", url },
+        { fileLabel: "1:1", url },
+      ];
 
-story
-  .add("Custom link", withReadme(Readme,
-    () => <SideBySide Images={text("Image", "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Profile_Mario.aead314d435d8e52d9a4e92a6f799c4eee08081e.jpg")} />
+      return (
+        <SideBySide
+          images={images}
+        >
+          <h3>{title}</h3>
+          <p>{summary}</p>
+          <div className="btn--dark-tertiary btn--small">{button}</div>
+        </SideBySide>
+      )
+    }
   ));
