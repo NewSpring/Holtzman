8adf9415e140d8b485a4c1f7ee86b2447f20b745
richarddubay <richardtdubayjr@gmail.com>
2016-10-30 16:00:56 -0400

    storybook content for the MiniCard.

---
+++ imports/components/cards/__stories__/cards.MiniCard.js
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+import { storiesOf } from "@kadira/storybook";
+import {
+  withKnobs,
+  text,
+  select,
+} from "@kadira/storybook-addon-knobs";
+import withReadme from "storybook-readme/with-readme";
+import backgrounds from "react-storybook-addon-backgrounds";
+import centered from "/.storybook/decorators/centered";
+import defaultColors from "/.storybook/defaults";
+
+import Readme from "./cards.MiniCard.md";
+import MiniCard from "../cards.MiniCard";
+
+const story = storiesOf("Components", module)
+  .addDecorator(withKnobs)
+  .addDecorator(centered)
+  .addDecorator(backgrounds(defaultColors("light-primary", "light-secondary", "dark-primary", "dark-secondary")))
+  ;
+
+// We don't use description quite yet. As such, it's not really styled.
+// So let's not pass it on this story.
+// description={text("description", "MiniCard description")}
+
+story
+  .add("MiniCard with KNOBS", withReadme(Readme, () => {
+    // set channel name options
+    const channelOptions = {
+      articles: "Articles",
+      devotionals: "Devotionals",
+      news: "News",
+      sermons: "Sermons",
+      stories: "Stories",
+    };
+
+    // setup the channel name to get info from the "Type" knob. Default is Articles.
+    const channelName = select("type", channelOptions, "articles");
+
+    const content = {
+      channelName,
+      content: {
+        images: [],
+      },
+      title: "MiniCard Title",
+    };
+
+    const imageObject = {
+      fileLabel: "2:1",
+      url: "https://images.unsplash.com/24/5895672523_2e8ce56485_o.jpg",
+    };
+    content.content.images.push(imageObject);
+
+    const defaultImageURL = "https://images.unsplash.com/24/5895672523_2e8ce56485_o.jpg";
+    content.content.images[0].url = text("image link", defaultImageURL);
+
+    return (
+      <MiniCard
+        title={text("title", content.title)}
+        content={content}
+      />);
+  }));
