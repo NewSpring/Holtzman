0104f922cae3a3a6e2730c6d3c2c2d30a89a8674
richarddubay <richardtdubayjr@gmail.com>
2016-10-31 14:07:58 -0400

    test for MiniCard

---
+++ imports/components/cards/__tests__/cards.MiniCard.js
@@ -0,0 +1,23 @@
+import renderer from "react-test-renderer";
+import MiniCard from "../cards.MiniCard";
+
+const content = {
+  channelName: "articles",
+  content: {
+    images: [{
+      fileLabel: "2:1",
+      url: "https://images.unsplash.com/24/5895672523_2e8ce56485_o.jpg",
+    }],
+  },
+  title: "MiniCard Title",
+};
+
+it("has a channelName of 'articles', an image from Unsplash, and a title of 'MiniCard Title'", () => {
+  const tree = renderer.create(
+    <MiniCard
+      title={content.title}
+      content={content}
+    />
+  );
+  expect(tree).toMatchSnapshot();
+});
