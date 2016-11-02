44eaa0484b03fabb6fbb1084e581700db72d6f84
richarddubay <richardtdubayjr@gmail.com>
2016-11-02 15:58:13 -0400

    added more MiniCard tests.

--- imports/components/cards/__tests__/cards.MiniCard.js
+++ imports/components/cards/__tests__/cards.MiniCard.js
@@ -1,23 +1,61 @@
 import renderer from "react-test-renderer";
 import MiniCard from "../cards.MiniCard";
 
-const content = {
-  channelName: "articles",
-  content: {
-    images: [{
-      fileLabel: "2:1",
-      url: "https://images.unsplash.com/24/5895672523_2e8ce56485_o.jpg",
-    }],
-  },
-  title: "MiniCard Title",
-};
+describe("MiniCard", () => {
+  it("has a channelName of 'articles', an image, and a title of 'MiniCard Title'", () => {
+    const content = {
+      channelName: "articles",
+      content: {
+        images: [{
+          fileLabel: "2:1",
+          url: "https://images.unsplash.com/24/5895672523_2e8ce56485_o.jpg",
+        }],
+      },
+      title: "MiniCard Title",
+    };
 
-it("has a channelName of 'articles', an image, and a title of 'MiniCard Title'", () => {
-  const tree = renderer.create(
-    <MiniCard
-      title={content.title}
-      content={content}
-    />
-  );
-  expect(tree).toMatchSnapshot();
+    const tree = renderer.create(
+      <MiniCard
+        title={content.title}
+        content={content}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("doesn't have an image if one isn't passed", () => {
+    const content = {
+      channelName: "articles",
+      content: {
+        images: [],
+      },
+      title: "MiniCard Title",
+    };
+
+    const tree = renderer.create(
+      <MiniCard
+        title={content.title}
+        content={content}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("has a video icon if it's a sermon", () => {
+    const content = {
+      channelName: "sermons",
+      content: {
+        images: [],
+      },
+      title: "MiniCard Title",
+    };
+
+    const tree = renderer.create(
+      <MiniCard
+        title={content.title}
+        content={content}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
