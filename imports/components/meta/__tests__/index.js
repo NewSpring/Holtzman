3736c513efb9942fe54880b784031fcc5dc9578b
richarddubay <richardtdubayjr@gmail.com>
2016-11-06 15:59:44 -0500

    added/edited tests

--- imports/components/meta/__tests__/index.js
+++ imports/components/meta/__tests__/index.js
@@ -1,21 +1,42 @@
 import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import Meta from "../index";
-import generateData from "../metadata";
+// import generateData from "../metadata";
 
 describe("Meta", () => {
-  it("should have default data if nothing is passed to it.", () => {
+  it("should have default data if nothing is passed to it", () => {
     const wrapper = shallow(
       <Meta />
     );
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
-  it("should have some stuff in it.", () => {
+  it("should override the default title", () => {
     const wrapper = shallow(
       <Meta
-        title={"article.title"}
-        image={"photo"}
+        title={"This is a Test Title"}
+      />
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should override the default image", () => {
+    const wrapper = shallow(
+      <Meta
+        image={"link to the test image"}
+      />
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should have an id", () => {
+    const wrapper = shallow(
+      <Meta
         id={"article.id"}
+      />
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should have an og:type meta property", () => {
+    const wrapper = shallow(
+      <Meta
         meta={[
           { property: "og:type", content: "article" },
         ]}
