433e4ddc4ed188f52507811fc314cb9d8de9a77f
delianides <github@delianides.com>
2016-11-08 14:14:39 -0500

    eslint ignore for index maybe because of flow changes

--- imports/pages/give/index.js
+++ imports/pages/give/index.js
@@ -1,10 +1,11 @@
-
+// XXX with flow this might need to be refactored
 import Campaign from "./campaign";
 import History from "./history";
 import Now from "./now";
 import Review from "./review";
 import Schedules from "./schedules";
 
+/* eslint-disable */
 export default {
   path: "give",
   // indexRoute: { component: Home },
