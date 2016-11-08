d643ed1b943f0d80be4c78010f42b934f4031d9b
delianides <github@delianides.com>
2016-11-08 14:09:32 -0500

    add typedef for connect()
    add typedef for lodash.find module

---
+++ .types/lodash-find.js
@@ -0,0 +1,3 @@
+declare module "lodash.find" {
+  declare var exports: any;
+};
--- .types/react-redux.js
+++ .types/react-redux.js
@@ -1,3 +1,4 @@
 declare module "react-redux" {
   declare function connect(mapStateToProps: Object): Function;
+  declare function connect(): Function;
 }
