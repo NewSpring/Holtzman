b783e3425262157e33e5a71aceb5a6458bd6e76f
delianides <github@delianides.com>
2016-11-08 16:44:36 -0500

    typed give index

--- imports/pages/give/index.js
+++ imports/pages/give/index.js
@@ -1,11 +1,10 @@
-// XXX with flow this might need to be refactored
+// @flow
 import Campaign from "./campaign";
 import History from "./history";
 import Now from "./now";
 import Review from "./review";
 import Schedules from "./schedules";
 
-/* eslint-disable */
 export default {
   path: "give",
   // indexRoute: { component: Home },
