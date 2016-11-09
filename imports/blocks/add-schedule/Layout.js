327fa93a704034f0ae8d0bd414984127be2a1d6a
David Stevens <dcs619@gmail.com>
2016-11-09 11:36:32 -0500

    testing Layout renders, reverted all index changes

--- imports/blocks/add-schedule/Layout.js
+++ imports/blocks/add-schedule/Layout.js
@@ -1,9 +1,8 @@
-// @flow
-
 // $FlowMeteor
 import { Meteor } from "meteor/meteor";
 import Moment from "moment";
 import { css } from "aphrodite";
+import { Component, PropTypes } from "react";
 
 import TertiaryPhrase from "./phrases";
 import Forms from "../../components/forms";
@@ -26,131 +25,158 @@ type ILayout = {
     total: number,
 };
 
-const Layout = ({
-  accounts,
-  existing,
-  format,
-  onSubmitSchedule,
-  ready,
-  save,
-  saveDate,
-  schedules,
-  setFrequency,
-  setFund,
-  state,
-  text,
-  total,
-}: ILayout): Object => {
-  let disableCheckout = false;
-  if (!total || total <= 0 || !ready) {
-    disableCheckout = true;
-  }
+// this definition of Layout doesn't work
+// const Layout = ({
+//   accounts,
+//   existing,
+//   format,
+//   onSubmitSchedule,
+//   ready,
+//   save,
+//   saveDate,
+//   schedules,
+//   setFrequency,
+//   setFund,
+//   state,
+//   text,
+//   total,
+// }: ILayout) => {
 
-  let prefillFund = accounts[0].value;
-  if (existing && existing.details && existing.details.length && existing.details[0].account) {
-    prefillFund = existing.details[0].account.id;
-  }
+// this definition of Layout works
+class Layout extends Component {
 
-  // XXX: this was already commented out, should it be?
-  // let defaultDate = Moment().add(1, "days")
-  let defaultDate = null;
-  if (existing && existing.next && new Date(existing.next) > new Date()) {
-    defaultDate = new Date(existing.next);
-  }
+  static propTypes: ILayout;
 
-  let formInputDefaultValue = null;
-  if (existing && existing.details && existing.details.length && existing.details[0].amount) {
-    formInputDefaultValue = `$${existing.details[0].amount}`;
-  }
+  render() {
+    const {
+      accounts,
+      existing,
+      format,
+      onSubmitSchedule,
+      ready,
+      save,
+      saveDate,
+      schedules,
+      setFrequency,
+      setFund,
+      state,
+      text,
+      total,
+    } = this.props;
 
-  let formSelectDefaultValue = null;
-  if (existing && existing.schedule) {
-    formSelectDefaultValue = existing.schedule.value;
-  }
+/* end flip flop comment section */
 
-  return (
-    <div className="push-top@handheld soft-half-top@lap-and-up">
-      <Forms.Form
-        classes={["text-left", "hard"]}
-        submit={(e) => { e.preventDefault(); }}
-        id="add-to-cart"
-      >
-        <TertiaryPhrase
-          additionalClasses="push-half-right"
-          text="I'd like to give "
-        />
-        <Forms.Input
-          id={state.fundId || -1}
-          name={state.fundLabel || "primary-account"}
-          hideLabel
-          type={Meteor.isCordova ? "text" : "tel"}
-          ref="primary-account"
-          classes={["soft-bottom", "input--active", "display-inline-block"]}
-          inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
-          placeholder="$0.00"
-          validate={save}
-          format={format}
-          style={{ width: "200px" }}
-          defaultValue={formInputDefaultValue}
-        />
-        <TertiaryPhrase
-          text="to "
-        />
-        <Forms.Select
-          items={accounts}
-          name="select-account"
-          id={"select"}
-          hideLabel
-          ref="select-account"
-          classes={["soft-bottom", "display-inline-block", css(Styles.select)]}
-          inputClasses={"outlined--dotted outlined--light h3 hard-top flush-bottom text-light-tertiary"}
-          placeholder="select fund here"
-          onChange={setFund}
-          defaultValue={prefillFund}
-        />
-        <TertiaryPhrase
-          text=" "
-        />
-        <Forms.Select
-          items={schedules}
-          name="schedules"
-          id={"schedules"}
-          hideLabel
-          ref="schedules"
-          classes={["soft-bottom", "display-inline-block", css(Styles.select)]}
-          inputClasses={"outlined--dotted outlined--light h3 hard-top flush-bottom text-light-tertiary"}
-          includeBlank
-          placeholder="choose frequency"
-          onChange={setFrequency}
-          defaultValue={formSelectDefaultValue}
-        />
-        <TertiaryPhrase
-          text=" starting "
-        />
-        <Forms.Date
-          id="start-date"
-          name="start-date"
-          hideLabel
-          ref="start-date"
-          classes={["soft-bottom", "input--active", "display-inline-block"]}
-          inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
-          placeholder="select date"
-          past={false}
-          today={false}
-          format={value => (Moment(value).format("MMM D, YYYY"))} // eslint-disable-line
-          validation={saveDate}
-          defaultValue={defaultDate}
-        />
-        <div className="push-top">
-          <CheckoutButtons
-            disabled={disableCheckout}
-            onClick={onSubmitSchedule}
-            text={text || "Schedule Now"}
+    let disableCheckout = false;
+    if (!total || total <= 0 || !ready) {
+      disableCheckout = true;
+    }
+
+    let prefillFund = accounts[0].value;
+    if (existing && existing.details && existing.details.length && existing.details[0].account) {
+      prefillFund = existing.details[0].account.id;
+    }
+
+      // XXX: this was already commented out, should it be?
+      // let defaultDate = Moment().add(1, "days")
+    let defaultDate = null;
+    if (existing && existing.next && new Date(existing.next) > new Date()) {
+      defaultDate = new Date(existing.next);
+    }
+
+    let formInputDefaultValue = null;
+    if (existing && existing.details && existing.details.length && existing.details[0].amount) {
+      formInputDefaultValue = `$${existing.details[0].amount}`;
+    }
+
+    let formSelectDefaultValue = null;
+    if (existing && existing.schedule) {
+      formSelectDefaultValue = existing.schedule.value;
+    }
+
+    return (
+      <div className="push-top@handheld soft-half-top@lap-and-up">
+        <Forms.Form
+          classes={["text-left", "hard"]}
+          submit={(e) => { e.preventDefault(); }}
+          id="add-to-cart"
+        >
+          <TertiaryPhrase
+            additionalClasses="push-half-right"
+            text="I'd like to give "
           />
-        </div>
-      </Forms.Form>
-    </div>
-  );
-};
+          <Forms.Input
+            id={state.fundId || -1}
+            name={state.fundLabel || "primary-account"}
+            hideLabel
+            type={Meteor.isCordova ? "text" : "tel"}
+            ref="primary-account"
+            classes={["soft-bottom", "input--active", "display-inline-block"]}
+            inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
+            placeholder="$0.00"
+            validate={save}
+            format={format}
+            style={{ width: "200px" }}
+            defaultValue={formInputDefaultValue}
+          />
+          <TertiaryPhrase
+            text="to "
+          />
+          <Forms.Select
+            items={accounts}
+            name="select-account"
+            id={"select"}
+            hideLabel
+            ref="select-account"
+            classes={["soft-bottom", "display-inline-block", css(Styles.select)]}
+            inputClasses={"outlined--dotted outlined--light h3 hard-top flush-bottom text-light-tertiary"}
+            placeholder="select fund here"
+            onChange={setFund}
+            defaultValue={prefillFund}
+          />
+          <TertiaryPhrase
+            text=" "
+          />
+          <Forms.Select
+            items={schedules}
+            name="schedules"
+            id={"schedules"}
+            hideLabel
+            ref="schedules"
+            classes={["soft-bottom", "display-inline-block", css(Styles.select)]}
+            inputClasses={"outlined--dotted outlined--light h3 hard-top flush-bottom text-light-tertiary"}
+            includeBlank
+            placeholder="choose frequency"
+            onChange={setFrequency}
+            defaultValue={formSelectDefaultValue}
+          />
+          <TertiaryPhrase
+            text=" starting "
+          />
+          <Forms.Date
+            id="start-date"
+            name="start-date"
+            hideLabel
+            ref="start-date"
+            classes={["soft-bottom", "input--active", "display-inline-block"]}
+            inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
+            placeholder="select date"
+            past={false}
+            today={false}
+            format={value => (Moment(value).format("MMM D, YYYY"))} // eslint-disable-line
+            validation={saveDate}
+            defaultValue={defaultDate}
+          />
+          <div className="push-top">
+            <CheckoutButtons
+              disabled={disableCheckout}
+              onClick={onSubmitSchedule}
+              text={text || "Schedule Now"}
+            />
+          </div>
+        </Forms.Form>
+      </div>
+    );
+  }
+}
 
 export default Layout;
--- imports/blocks/add-schedule/index.js
+++ imports/blocks/add-schedule/index.js
@@ -1,7 +1,4 @@
-// @flow
-
-import serverWatch from "meteor/bjwiley2:server-watch";
-import { Component } from "react";
+import { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 import moment from "moment";
 import createContainer from "../meteor/react-meteor-data";
@@ -11,54 +8,41 @@ import Offline from "../../components/status/Offline";
 
 import Layout from "./Layout";
 
-// XXX move this to a global constructs file?
-const GIVING_SCHEDULES = [
-    { label: "one time", value: "One-Time" },
-    { label: "every week", value: "Weekly" },
-    { label: "every two weeks", value: "Bi-Weekly" },
-    { label: "once a month", value: "Monthly" },
-];
-
-type ICartContainer = {
-    accounts: Object,
-    addTransactions: Function,
-    alive: boolean,
-    clearAllSchedulesExcept: Function,
-    clearSchedules: Function,
-    clearTransactions: Function,
-    existing: Object, // eslint-disable-line
-    onClick: Function,
-    removeSchedule: Function,
-    saveSchedule: Function,
-    setTransactionType: Function,
-    text: string,
-};
-
-type IState = {
-  fundId: number,
-  fundLabel: string,
-  frequency: string,
-  startDate: string,
-  amount: number,
-};
-
 // We only care about the give state
 const map = (state) => ({ give: state.give });
 
-export const withRedux = connect(map, giveActions);
+@connect(map, giveActions)
+class CartContainer extends Component {
 
-// refactored because decoraters no likey
-// @connect(map, giveActions)
-export class CartContainer extends Component {
-  props: ICartContainer;
+  static propTypes = {
+    accounts: PropTypes.array, // eslint-disable-line
+    addTransactions: PropTypes.func,
+    alive: PropTypes.bool,
+    clearAllSchedulesExcept: PropTypes.func,
+    clearSchedules: PropTypes.func,
+    clearTransactions: PropTypes.func,
+    existing: PropTypes.object, // eslint-disable-line
+    onClick: PropTypes.func,
+    removeSchedule: PropTypes.func,
+    saveSchedule: PropTypes.func,
+    setTransactionType: PropTypes.func,
+    text: PropTypes.string,
+    dataId: PropTypes.string,
+  }
 
-  state: IState;
+  state = {
+    fundId: false,
+    fundLabel: null,
+    frequency: null,
+    startDate: null,
+    amount: null,
+  }
 
   componentWillMount() {
     this.props.clearTransactions();
 
     if (this.props.existing) {
-      const { existing }: { existing: Object } = this.props;
+      const { existing } = this.props;
       if (existing.details && existing.details.length && existing.details[0].account) {
         this.setState({
           fundId: Number(existing.details[0].account.id),
@@ -70,11 +54,11 @@ export class CartContainer extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps: Object) { // eslint-disable-line
-    const { transactions, schedules }: { transactions: Object, schedules: Object } = nextProps.give;
+  componentWillReceiveProps(nextProps) { // eslint-disable-line
+    const { transactions, schedules } = nextProps.give;
 
     if (Object.keys(transactions).length === 0 && Object.keys(schedules).length === 0) {
-      const form: FormElement = (document.getElementById("add-to-cart"): any);
+      const form = document.getElementById("add-to-cart");
       if (form) form.reset();
     }
   }
@@ -83,8 +67,94 @@ export class CartContainer extends Component {
     this.props.clearSchedules();
   }
 
-  // XXX: should remove fixed since it's not being used? DS
-  monetize = (val: string, fixed?: boolean) => {
+  onClick = (e) => {
+    e.preventDefault();
+
+    let keepGoing = true;
+    if (this.state.fundId) {
+      this.props.clearAllSchedulesExcept(Number(this.state.fundId));
+
+      this.props.saveSchedule(this.state.fundId, {
+        label: this.state.fundLabel,
+        frequency: this.state.frequency,
+        start: this.state.startDate,
+      });
+
+      this.props.clearTransactions();
+      this.props.addTransactions({ [this.state.fundId]: {
+        value: Number(this.state.amount),
+        label: this.state.fundLabel,
+      } });
+    }
+
+    if (this.props.onClick) keepGoing = this.props.onClick(e);
+    return keepGoing;
+  }
+
+  setFund = (id) => {
+    const selectedFund = this.props.accounts.filter((fund) => fund.id === Number(id));
+
+    const { name } = selectedFund[0];
+
+    if (this.state.fundId !== id) this.props.removeSchedule(this.state.fundId);
+
+    this.setState({ fundId: id, fundLabel: name });
+    this.props.saveSchedule(id, {
+      label: name,
+      frequency: this.state.frequency,
+      start: this.state.start,
+    });
+
+    this.props.setTransactionType("recurring");
+  }
+
+  setFrequency = (value) => {
+    this.setState({ frequency: value });
+    if (this.state.fundId) {
+      this.props.saveSchedule(this.state.fundId, { frequency: value });
+    }
+  }
+
+  format = (val, target) => {
+    const { id, name } = target;
+
+    const value = this.monentize(val);
+
+    this.setState({
+      fundId: id,
+      fundLabel: name,
+      amount: Number(value.replace(/[^0-9\.]+/g, "")),
+    });
+
+    return value;
+  }
+
+  saveData = (val, target) => {
+    const { id, name } = target;
+
+    const value = this.monentize(val);
+    this.setState({
+      fundId: id,
+      fundLabel: name,
+      amount: Number(value.replace(/[^0-9\.]+/g, "")),
+    });
+
+    return true;
+  }
+
+  saveDate = (value) => {
+    const { fundId } = this.state;
+
+    const date = moment(new Date(value)).format("YYYYMMDD");
+
+    this.setState({ startDate: date });
+
+    if (fundId) this.props.saveSchedule(fundId, { start: new Date(value) });
+    return true;
+  }
+
+
+  monentize = (val, fixed) => {
     let value = val;
     if (typeof value === "number") value = `${value}`;
     if (!value.length) return "$0.00";
@@ -101,98 +171,17 @@ export class CartContainer extends Component {
     return `$${value}`;
   }
 
-  onClick = (e: Event) => {
-    e.preventDefault();
-
-    let keepGoing = true;
-    if (this.state.fundId) {
-      this.props.clearAllSchedulesExcept(Number(this.state.fundId));
-
-      this.props.saveSchedule(this.state.fundId, {
-        label: this.state.fundLabel,
-        frequency: this.state.frequency,
-        start: this.state.startDate,
-      });
-
-      this.props.clearTransactions();
-      this.props.addTransactions(
-        {
-          [this.state.fundId]: {
-            value: Number(this.state.amount),
-            label: this.state.fundLabel,
-          },
-        });
-    }
-
-    if (this.props.onClick) keepGoing = this.props.onClick(e);
-    return keepGoing;
-  }
-
-  setFund = (id: number) => {
-    const selectedFund = this.props.accounts.filter((fund) => fund.id === Number(id));
-    const { name }: { name: string } = selectedFund[0];
-
-    if (this.state.fundId !== id) this.props.removeSchedule(this.state.fundId);
-
-    this.setState({ fundId: id, fundLabel: name });
-    this.props.saveSchedule(id, {
-      label: name,
-      frequency: this.state.frequency,
-      start: this.state.startDate,
-    });
-
-    this.props.setTransactionType("recurring");
-  }
-
-  setFrequency = (value: string) => {
-    this.setState({ frequency: value });
-    if (this.state.fundId) {
-      this.props.saveSchedule(this.state.fundId, { frequency: value });
-    }
-  }
-
-  format = (val: string, target: Object) => {
-    const { id, name }: { id: number, name: string } = target;
-
-    const value = this.monetize(val);
-
-    this.setState({
-      fundId: id,
-      fundLabel: name,
-      amount: Number(value.replace(/[^0-9\.]+/g, "")),
-    });
-
-    return value;
-  }
-
-  saveData = (val: string, target: Object) => {
-    const { id, name }: { id: number, name: string } = target;
-
-    const value = this.monetize(val);
-    this.setState({
-      fundId: id,
-      fundLabel: name,
-      amount: Number(value.replace(/[^0-9\.]+/g, "")),
-    });
-
-    return true;
-  }
-
-  saveDate = (value: string) => {
-    const { fundId }: { fundId: number } = this.state;
-
-    const date = moment(new Date(value)).format("YYYYMMDD");
-
-    this.setState({ startDate: date });
-
-    if (fundId) this.props.saveSchedule(fundId, { start: new Date(value) });
-    return true;
-  }
-
   render() {
     if (!this.props.alive) return <Offline />;
 
-    const mappedAccounts = this.props.accounts.map((x: Object) => ({
+    const schedules = [
+      { label: "one time", value: "One-Time" },
+      { label: "every week", value: "Weekly" },
+      { label: "every two weeks", value: "Bi-Weekly" },
+      { label: "once a month", value: "Monthly" },
+    ];
+
+    const mappedAccounts = this.props.accounts.map((x) => ({
       value: x.id,
       label: x.name,
     }));
@@ -201,34 +190,31 @@ export class CartContainer extends Component {
       return null;
     }
 
-    let isCheckoutReady = false;
-    if (this.state.fundId && this.state.fundLabel && this.state.startDate && this.state.frequency) {
-      isCheckoutReady = true;
-    }
-
+    const { fundId, fundLabel, startDate, frequency } = this.state;
     return (
       <Layout
-        accounts={mappedAccounts}
-        date={this.state.startDate}
-        existing={this.props.existing}
-        format={this.format}
-        onSubmitSchedule={this.onClick}
-        ready={isCheckoutReady}
-        schedules={GIVING_SCHEDULES}
+        schedules={schedules}
         setFrequency={this.setFrequency}
+        accounts={mappedAccounts}
         setFund={this.setFund}
+        state={this.state}
+        format={this.format}
         save={this.saveData}
         saveDate={this.saveDate}
-        state={this.state}
+        total={this.state.amount}
+        existing={this.props.existing}
+        date={this.state.startDate}
         text={this.props.text}
-        total={Number(this.state.amount)}
+        onSubmitSchedule={this.onClick}
+        ready={fundId && fundLabel && startDate && frequency}
+        dataId={this.props.dataId}
       />
     );
   }
 }
 
 export default createContainer(() => {
-  let alive = true;
-  try { alive = serverWatch.isAlive("ROCK"); } catch (e) {} // eslint-disable-line
+  const alive = true;
+  // try { alive = serverWatch.isAlive("ROCK"); } catch (e) {} // eslint-disable-line
   return { alive };
 }, CartContainer);
