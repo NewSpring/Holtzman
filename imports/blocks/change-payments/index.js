8458f829c8dd040d2ebc264b16367228190fac06
edolyne <ed@edolyne.com>
2016-11-09 15:30:14 -0500

    SYS-2540 created Layout file for change payments

--- imports/blocks/change-payments/index.js
+++ imports/blocks/change-payments/index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { modal, give as giveActions } from "../../store";
 
-import AccountType from "../../components/accountType";
+import Layout from "./Layout";
 
 const map = (state) => ({
   savedAccount: state.give.savedAccount,
@@ -21,12 +21,12 @@ export default class ChangePayments extends Component {
     savedAccount: null,
   }
 
-  changePaymentHeader = () => (
-    <h4 className="text-center flush-bottom">
-      Change Payment Account
-    </h4>
-  );
+  paymentAccount = (account) => {
+    const accountFistEight = account.payment.accountNumber.slice(0, account.payment.accountNumber.length - 5).replace(/./gmi, "*");
+    const accountLastFour = account.payment.accountNumber.slice(-4);
 
+    return accountFistEight + accountLastFour;
+  }
 
   changeAccounts = (e) => {
     e.preventDefault();
@@ -58,74 +58,13 @@ export default class ChangePayments extends Component {
       selectedAccount = this.state.savedAccount;
     }
 
-    /* eslint-disable */
     return (
-      <div className="soft-double-top">
-        <div className="soft-sides flush-bottom push-double-top@lap-and-up">
-          {this.changePaymentHeader()}
-        </div>
-
-        <div className="soft">
-          { this.props.savedAccounts.map((account, key) => (
-            <div key={key} style={{ position: "relative", cursor: "pointer" }} id={account.id} onClick={this.choose}>
-              <div className="soft-ends push-double-left text-left hard-right outlined--light outlined--bottom relative">
-
-                <div className="display-inline-block soft-half-ends one-whole">
-                  <h6 className="flush-bottom float-left text-dark-tertiary">{account.name}</h6>
-                  {/* <button className="h6 flush-bottom float-right text-primary" id={account.id}
-                    onClick={this.choose}>Choose</button>*/}
-                </div>
-
-
-                <h5 className="hard one-whole flush-bottom text-dark-tertiary">
-                  {account.payment.accountNumber.slice(0, account.payment.accountNumber.length - 5).replace(/./gmi, "*")}{account.payment.accountNumber.slice(-4)}
-                  <span className="float-right soft-half-left">
-                    <AccountType width="40px" height="25px" type={account.payment.paymentType} />
-
-                  </span>
-
-                </h5>
-
-
-              </div>
-              <div className="locked-ends locked-sides">
-                <input
-                  type="checkbox"
-                  id={`label${account.id}`}
-                  readOnly
-                  checked={Number(account.id) === Number(selectedAccount.id)}
-                  style={{
-                    opacity: 0,
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    padding: "50px",
-                  }}
-                />
-                <label
-                  htmlFor={`label${account.id}`}
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: 0,
-                  }}
-                />
-              </div>
-            </div>
-          ))}
-          <button className="btn one-whole push-double-top soft-sides push-bottom" onClick={this.changeAccounts}>
-            Change Account
-          </button>
-
-          <p >
-            <small>
-              <em>
-                You can enter a new payment account before confirming your contribution.
-              </em>
-            </small>
-          </p>
-        </div>
-      </div>
+      <Layout
+        savedAccounts={this.props.savedAccounts}
+        selectedAccount={selectedAccount}
+        chooseAccount={this.choose}
+        changeAccounts={this.changeAccounts}
+      />
     );
   }
 }
