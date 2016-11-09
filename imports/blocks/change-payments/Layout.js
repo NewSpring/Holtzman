5e0aceb23fd424e1162dced9bca89369bd325215
edolyne <ed@edolyne.com>
2016-11-09 16:14:20 -0500

    SYS-2505 adding typed definitions to layout

--- imports/blocks/change-payments/Layout.js
+++ imports/blocks/change-payments/Layout.js
@@ -1,6 +1,4 @@
 
-import { Component, PropTypes } from "react";
-
 import PaymentCard from "./paymentCard";
 
 const paymentAccount = (account) => {
@@ -10,57 +8,53 @@ const paymentAccount = (account) => {
   return accountFistEight + accountLastFour;
 };
 
-export default class Layout extends Component {
-
-  static propTypes = {
-    savedAccounts: PropTypes.array,
-    selectedAccount: PropTypes.string,
-    chooseAccount: PropTypes.func,
-    changeAccounts: PropTypes.func,
-  }
-
-  render() {
-    const {
-      savedAccounts,
-      selectedAccount,
-      chooseAccount,
-      changeAccounts,
-    } = this.props;
-    return (
-      <div className="soft-double-top">
-        <div className="soft-sides flush-bottom push-double-top@lap-and-up">
-          <h4 className="text-center flush-bottom">
-            Change Payment Account
-          </h4>
-        </div>
-
-        <div className="soft">
-          { savedAccounts.map((account, key) => (
-            <PaymentCard
-              key={key}
-              onClick={chooseAccount}
-              accountId={account.id}
-              paymentAccount={paymentAccount(account)}
-              paymentType={account.payment.paymentType}
-              selectedAccountId={selectedAccount.id}
-            />
-          ))}
-          <button
-            className="btn one-whole push-double-top soft-sides push-bottom"
-            onClick={changeAccounts}
-          >
-            Change Account
-          </button>
-
-          <p >
-            <small>
-              <em>
-                You can enter a new payment account before confirming your contribution.
-              </em>
-            </small>
-          </p>
-        </div>
-      </div>
-    );
-  }
+type ILayout = {
+  savedAccounts: Object[],
+  selectedAccount: string,
+  chooseAccount: Function,
+  changeAccounts: Function,
 }
+
+const Layout = ({
+  savedAccounts,
+  selectedAccount,
+  chooseAccount,
+  changeAccounts,
+}: ILayout) => (
+  <div className="soft-double-top">
+    <div className="soft-sides flush-bottom push-double-top@lap-and-up">
+      <h4 className="text-center flush-bottom">
+        Change Payment Account
+      </h4>
+    </div>
+
+    <div className="soft">
+      { savedAccounts.map((account, key) => (
+        <PaymentCard
+          key={key}
+          onClick={chooseAccount}
+          accountId={account.id}
+          paymentAccount={paymentAccount(account)}
+          paymentType={account.payment.paymentType}
+          selectedAccountId={selectedAccount.id}
+        />
+      ))}
+      <button
+        className="btn one-whole push-double-top soft-sides push-bottom"
+        onClick={changeAccounts}
+      >
+        Change Account
+      </button>
+
+      <p>
+        <small>
+          <em>
+            You can enter a new payment account before confirming your contribution.
+          </em>
+        </small>
+      </p>
+    </div>
+  </div>
+);
+
+export default Layout;
