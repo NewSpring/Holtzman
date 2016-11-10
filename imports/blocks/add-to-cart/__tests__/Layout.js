d060180de4135bfc602db4900da17c8fc0687e54
James Baxley <james.baxley@newspring.cc>
2016-11-09 20:34:08 -0500

    allow for rendering both SubFunds

--- imports/blocks/add-to-cart/__tests__/Layout.js
+++ imports/blocks/add-to-cart/__tests__/Layout.js
@@ -1,4 +1,4 @@
-
+import { Component } from "react";
 import { mount, shallow } from "enzyme";
 import { mountToJson, shallowToJson } from "enzyme-to-json";
 // import ReactTestUtils from 'react-addons-test-utils' // ES6
@@ -11,20 +11,22 @@ import { reset, startBuffering } from "aphrodite/lib/inject";
 
 import Layout from "../Layout";
 
+import Subfund from "../Subfund";
+
 jest.mock("../Subfund", () => jest.fn(() => <div />));
 jest.mock("../../checkout-buttons", () => jest.fn(() => <div />));
 
 const additionalAccounts = [
-  {value: 1, label: "TEST 1"},
-  {value: 2, label: "TEST 2"},
-  {value: 3, label: "TEST 3"},
+  { value: 1, label: "TEST 1" },
+  { value: 2, label: "TEST 2" },
+  { value: 3, label: "TEST 3" },
 ];
 
-const generateComponent = (additionalProps={}) => {
+const generateComponent = (additionalProps = {}) => {
   const store = createStore(jest.fn());
   const defaultProps = {
     accounts: [
-      {value: "test"}
+      { value: "test" }
     ],
     monentize: jest.fn(),
     preFill: () => {},
@@ -43,13 +45,30 @@ afterEach(() => {
   reset();
 });
 
-xit ("should render with minimal props", () => {
+it("should render with minimal props", () => {
   const component = shallow(generateComponent());
   expect(shallowToJson(component)).toMatchSnapshot();
 });
 
 // XXX this will work fully when the stuff isn't mocked'
-xit ("should handle multiple accounts", () => {
+it("should handle multiple accounts", () => {
+  // Mock the subfund lifecycle where it updates the parent component
+  // this lets it set the second input on load
+  // XXX it would be nice if we could do this without the immediate rerender for SSR
+  class LocalSubFund extends Component {
+    state = {
+      amount: null
+    }
+    componentWillMount() {
+      if (this.props.primary) {
+        this.props.update(this.props.instance, this.props.accounts[0].value, this.state.amount);
+      }
+    }
+    render() {
+      return null;
+    }
+  }
+  Subfund.mockImplementationOnce((props) => <LocalSubFund {...props} />);
   const component = mount(generateComponent({accounts: additionalAccounts}));
   expect(mountToJson(component)).toMatchSnapshot();
 });
