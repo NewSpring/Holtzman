5194ca646845d250a831acbf26446511d116fa68
John Pinkerton <jpinkerton88@gmail.com>
2016-11-10 12:52:34 -0500

    move billing to index and layout files

---
+++ imports/blocks/give/fieldsets/billing/Layout.js
@@ -0,0 +1,162 @@
+import { PropTypes } from "react";
+import Forms from "../../../../components/forms";
+
+const Header = ({ override }) => {
+  if (override) return override;
+  return (
+    <h4 className="text-center">
+      Billing Address
+    </h4>
+  );
+};
+
+Header.propTypes = {
+  override: PropTypes.object,
+};
+
+const Layout = ({
+  back,
+  billing,
+  children,
+  city,
+  countries,
+  header,
+  next,
+  saveCountry,
+  saveState,
+  states,
+  streetAddress,
+  streetAddress2,
+  zip,
+}) => (
+  <div>
+    <div className="push-double@lap-and-up push">
+      <Header override={header} />
+    </div>
+
+    {children}
+
+    <div className="soft">
+
+
+      <Forms.Input
+        name="streetAddress"
+        label="Street Address"
+        errorText="Please enter your address"
+        validation={streetAddress}
+        defaultValue={billing.streetAddress}
+        autoFocus
+      />
+      <Forms.Input
+        name="streetAddress2"
+        label="Street Address (optional)"
+        validation={streetAddress2}
+        defaultValue={billing.streetAddress2}
+      />
+
+      <Forms.Select
+        name="country"
+        label="Country"
+        errorText="Please enter your country"
+        defaultValue={billing.country ? billing.country : "US"}
+        items={countries}
+        validation={saveCountry}
+        includeBlank
+      />
+
+      <Forms.Input
+        name="city"
+        label="City"
+        errorText="Please enter your city"
+        defaultValue={billing.city}
+        validation={city}
+      />
+
+      <div className="grid">
+        {(() => {
+          if (!billing.country || billing.country === "US" || billing.country === "CA") {
+            return (
+              <div className="grid__item one-half">
+                <Forms.Select
+                  name="state"
+                  label="State/Territory"
+                  errorText="Please enter your state"
+                  defaultValue={billing.state ? billing.state : "SC"}
+                  items={states}
+                  validation={saveState}
+                  includeBlank
+                />
+              </div>
+            );
+          }
+          return null;
+        })()}
+        {(() => {
+          let length = "one-whole";
+          if (!billing.country || billing.country === "US" || billing.country === "CA") {
+            length = "one-half";
+          }
+          return (
+            <div className={`grid__item ${length}`}>
+              <Forms.Input
+                name="zip"
+                label="Zip/Postal"
+                errorText="Please enter your zip"
+                defaultValue={billing.zip}
+                onChange={zip}
+                validation={zip}
+              />
+            </div>
+          );
+        })()}
+
+
+      </div>
+
+    </div>
+
+
+    <div>
+      <a href="" tabIndex={-1} onClick={back} className="btn--small btn--dark-tertiary display-inline-block">
+        Back
+      </a>
+
+      {(() => {
+        const btnClasses = ["push-left"];
+        let disabled = false;
+        if (!billing.streetAddress || !billing.city) {
+          btnClasses.push("btn--disabled");
+          disabled = true;
+        } else {
+          btnClasses.push("btn");
+        }
+
+        return (
+          <button className={btnClasses.join(" ")} disabled={disabled} type="submit" onClick={next}>
+            Next
+          </button>
+        );
+      })()}
+
+    </div>
+
+  </div>
+);
+
+Layout.propTypes = {
+  back: PropTypes.func,
+  billing: PropTypes.object,
+  children: PropTypes.object,
+  city: PropTypes.func,
+  countries: PropTypes.array,
+  header: PropTypes.object,
+  next: PropTypes.func,
+  saveCountry: PropTypes.func,
+  saveState: PropTypes.func,
+  states: PropTypes.array,
+  streetAddress: PropTypes.func,
+  streetAddress2: PropTypes.func,
+  zip: PropTypes.func,
+};
+
+export default Layout;
