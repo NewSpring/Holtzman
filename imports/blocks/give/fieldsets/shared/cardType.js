3dc9a5ef0c310e438f99b82e2bdea754807af49c
John Pinkerton <jpinkerton88@gmail.com>
2016-11-13 15:55:19 -0500

    type cardType

--- imports/blocks/give/fieldsets/shared/cardType.js
+++ imports/blocks/give/fieldsets/shared/cardType.js
@@ -1,4 +1,5 @@
-const cardType = (payment, savedAccount) => {
+// @flow
+const cardType = (payment: Object, savedAccount: Object) => {
   if (savedAccount && savedAccount.payment && savedAccount.payment.paymentType) {
     return savedAccount.payment.paymentType;
   }
