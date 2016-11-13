afda201a6e3ff9e166fc43ef280ebf2184eb7ee3
John Pinkerton <jpinkerton88@gmail.com>
2016-11-13 14:23:56 -0500

    refactor card type return

--- imports/blocks/give/fieldsets/shared/cardType.js
+++ imports/blocks/give/fieldsets/shared/cardType.js
@@ -3,9 +3,7 @@ const cardType = (payment, savedAccount) => {
     return savedAccount.payment.paymentType;
   }
 
-  if (payment.type === "ach") {
-    return "Bank";
-  }
+  if (payment.type === "ach") return "Bank";
 
   if (payment.type === "cc") {
     const d = /^6$|^6[05]$|^601[1]?$|^65[0-9][0-9]?$|^6(?:011|5[0-9]{2})[0-9]{0,12}$/gmi;
