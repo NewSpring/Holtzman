00f5123a654a481da5814b1f79426fcf13888c2c
richarddubay <richardtdubayjr@gmail.com>
2016-11-13 15:11:14 -0500

    early return

--- imports/blocks/add-to-cart/Subfund/Layout.js
+++ imports/blocks/add-to-cart/Subfund/Layout.js
@@ -23,29 +23,27 @@ const RenderAmountInput = ({
   preFill,
   inputVal,
 }: IRenderAmountInput) => {
-  if (state.fund) {
-    return (
-      <div className="display-block">
-        <h3 className={`${classes} push-half-bottom push-half-right display-inline-block`}>
-          with
-        </h3>
-        <Forms.Input
-          id={state.id}
-          name={state.fund || "secondary-account"}
-          hideLabel
-          type={Meteor.isCordova ? "text" : "tel"}
-          classes={["soft-bottom", "input--active", "display-inline-block"]}
-          inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
-          placeholder="$0.00"
-          format={format}
-          defaultValue={preFill(state.id)}
-          style={{ maxWidth: "150px" }}
-          value={inputVal}
-        />
-      </div>
-    );
-  }
-  return null;
+  if (!state.fund) return null;
+  return (
+    <div className="display-block">
+      <h3 className={`${classes} push-half-bottom push-half-right display-inline-block`}>
+        with
+      </h3>
+      <Forms.Input
+        id={state.id}
+        name={state.fund || "secondary-account"}
+        hideLabel
+        type={Meteor.isCordova ? "text" : "tel"}
+        classes={["soft-bottom", "input--active", "display-inline-block"]}
+        inputClasses={`outlined--dotted outlined--light h3 hard-top flush-bottom text-dark-primary ${css(Styles["show-placeholder"])}`}
+        placeholder="$0.00"
+        format={format}
+        defaultValue={preFill(state.id)}
+        style={{ maxWidth: "150px" }}
+        value={inputVal}
+      />
+    </div>
+  );
 };
 
 type ILayout = {
