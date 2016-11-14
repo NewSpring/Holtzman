e94a0d2ffb53ec25eb26d0edb55f9b9a9676f765
John Pinkerton <jpinkerton88@gmail.com>
2016-11-13 20:47:40 -0500

    add tests for monetize

--- imports/blocks/give/fieldsets/shared/monetize.js
+++ imports/blocks/give/fieldsets/shared/monetize.js
@@ -1,14 +1,8 @@
 // @flow
-const monetize = (amount: number | string, fixed?: boolean) => {
-  let value;
+const monetize = (amount: number | string, fixed?: boolean): string => {
+  let value = typeof amount === "number" ? `${amount}` : amount;
 
-  if (typeof amount === "number") {
-    value = `${amount}`;
-  }
-
-  if (!value || !value.length) {
-    return "$0.00";
-  }
+  if (!value || !value.length) return "$0.00";
 
   value = value.replace(/[^\d.-]/g, "");
 
