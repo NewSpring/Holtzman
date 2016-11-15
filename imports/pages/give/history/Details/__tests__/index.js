1e07c15b38a3c2ffab8f8e2e329870a658117638
John Pinkerton <jpinkerton88@gmail.com>
2016-11-15 16:36:16 -0500

    test details index

---
+++ imports/pages/give/history/Details/__tests__/index.js
@@ -0,0 +1,54 @@
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import { DetailsWithoutData as Details } from "../";
+import {
+  nav as navActions,
+} from "../../../../../store";
+
+const defaultProps = {
+  dispatch: jest.fn(),
+  data: {
+    transaction: {},
+  },
+  entries: {
+    loading: false,
+    entries: [{}, {}],
+  },
+};
+
+const generateComponent = (additionalProps = {}) => {
+  const newProps = {
+    ...defaultProps,
+    ...additionalProps,
+  };
+  return <Details { ...newProps } />;
+};
+
+it("renders with props", () => {
+  const wrapper = shallow(generateComponent());
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("updates nav on mount", () => {
+  const mockDispatch = jest.fn();
+  navActions.setLevel = jest.fn();
+  const wrapper = shallow(generateComponent({
+    dispatch: mockDispatch,
+  }));
+  expect(mockDispatch).toHaveBeenCalledTimes(1);
+  expect(navActions.setLevel).toHaveBeenCalledTimes(1);
+  expect(navActions.setLevel).toHaveBeenCalledWith("BASIC_CONTENT");
+});
+
+it("updates nav on unmount", () => {
+  const mockDispatch = jest.fn();
+  navActions.setLevel = jest.fn();
+  const wrapper = shallow(generateComponent({
+    dispatch: mockDispatch,
+  }));
+  wrapper.unmount();
+  expect(mockDispatch).toHaveBeenCalledTimes(2);
+  expect(navActions.setLevel).toHaveBeenCalledTimes(2);
+  expect(navActions.setLevel).toHaveBeenCalledWith("BASIC_CONTENT");
+  expect(navActions.setLevel).toHaveBeenCalledWith("TOP");
+});
