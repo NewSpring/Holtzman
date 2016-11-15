db8daae732008a4ab9205cebfbcec8b189e008f1
edolyne <ed@edolyne.com>
2016-11-15 11:08:08 -0500

    SYS-2504 Moved paymentAccount function to paymentCard

--- imports/blocks/change-payments/paymentCard/index.js
+++ imports/blocks/change-payments/paymentCard/index.js
@@ -2,6 +2,13 @@
 
 import AccountType from "../../../components/accountType";
 
+export const obfuscateAccount = (account: string) => {
+  const accountFistEight = account.slice(0, account.length - 4).replace(/./gmi, "*");
+  const accountLastFour = account.slice(-4);
+
+  return accountFistEight + accountLastFour;
+};
+
 const cardStyle = {
   position: "relative",
   cursor: "pointer",
@@ -13,16 +20,24 @@ const labelStyle = {
   left: 0,
 };
 
+const inputStyle = {
+  opacity: 0,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  padding: "50px",
+};
+
 type IPaymentCard = {
   onClick: Function,
   accountName: string,
   accountId: string,
-  paymentAccount: Function,
-  selectedAccountId: number,
+  paymentAccount: string,
+  selectedAccountId: string,
   paymentType: string,
 }
 
-const PaymentCard = ({
+export default ({
   accountName,
   accountId,
   paymentAccount,
@@ -34,11 +49,13 @@ const PaymentCard = ({
     <div className="soft-ends push-double-left text-left hard-right outlined--light outlined--bottom relative">
 
       <div className="display-inline-block soft-half-ends one-whole">
-        <h6 className="flush-bottom float-left text-dark-tertiary">{accountName}</h6>
+        <h6 className="flush-bottom float-left text-dark-tertiary">
+          {accountName}
+        </h6>
       </div>
 
       <h5 className="hard one-whole flush-bottom text-dark-tertiary">
-        {paymentAccount}
+        {obfuscateAccount(paymentAccount)}
         <span className="float-right soft-half-left">
           <AccountType
             width="40px"
@@ -56,13 +73,7 @@ const PaymentCard = ({
         id={`label${accountId}`}
         readOnly
         checked={Number(accountId) === Number(selectedAccountId)}
-        style={{
-          opacity: 0,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          padding: "50px",
-        }}
+        style={inputStyle}
       />
       <label
         htmlFor={`label${accountId}`}
@@ -71,5 +82,3 @@ const PaymentCard = ({
     </div>
   </div>
 );
-
-export default PaymentCard;
