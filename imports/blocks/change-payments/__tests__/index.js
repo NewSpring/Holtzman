ac58d9a41f46618c2fc83aee064440fc01e99231
edolyne <ed@edolyne.com>
2016-11-16 16:50:15 -0500

    SYS-2506 testing index for changeAccounts and choose

--- imports/blocks/change-payments/__tests__/index.js
+++ imports/blocks/change-payments/__tests__/index.js
@@ -4,102 +4,117 @@ import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import { ChangePaymentsWithoutData as ChangePayments, map } from "../";
 
-// jest.mock("../index");
+it("maps savedAccount to state", () => {
+  const dummyState = {
+    give: {
+      savedAccount: "test",
+    },
+  };
 
-// it("maps savedAccount to state", () => {
-//   const dummyState = {
-//     give: {
-//       savedAccount: "test",
-//     },
-//   };
+  expect(map(dummyState)).toEqual({ savedAccount: dummyState.give.savedAccount });
+});
 
-//   expect(map(dummyState)).toEqual({ savedAccount: dummyState.give.savedAccount });
-// });
+const accountShape = [
+  {
+    name: "Test Card 1",
+    id: 1,
+    date: "2016-11-16T21:45:41.275Z",
+    payment: {
+      accountNumber: "4111111111111111",
+      paymentType: "Visa",
+    }
+  },
+  {
+    name: "Test Card 2",
+    id: 2,
+    date: "2016-11-16T21:45:41.275Z",
+    payment: {
+      accountNumber: "4111111111111111",
+      paymentType: "Visa",
+    }
+  }
+];
+
+const currentAccount = {
+    name: "Test Card 1",
+    id: 1,
+    date: new Date().toISOString(),
+    payment: {
+      accountNumber: "4111111111111111",
+      paymentType: "Visa",
+    }
+  };
+
+const mockedEvent = {
+  preventDefault: jest.fn(),
+  currentTarget: {
+    id: "1"
+  }
+};
+
+let component;
+let changeAccounts;
+
+beforeEach(() => {
+  component = shallow(
+    <ChangePayments
+      savedAccounts={accountShape}
+      currentAccount={currentAccount}
+      dispatch={jest.fn()}
+    />
+  );
+
+  changeAccounts = component.instance().changeAccounts;
+});
+
+afterEach(() => {
+  component = undefined;
+  changeAccounts = undefined;
+});
 
 describe("changeAccounts", () => {
-  const accountShape = [
-    {
-      name: "Test Card 1",
-      id: 1,
-      date: new Date().toISOString(),
-      payment: {
-        accountNumber: "4111111111111111",
-        paymentType: "Visa",
-      }
-    },
-    {
-      name: "Test Card 2",
-      id: 2,
-      date: new Date().toISOString(),
-      payment: {
-        accountNumber: "4111111111111111",
-        paymentType: "Visa",
-      }
-    }
-  ];
-
-  const currentAccount = {
-      name: "Test Card 1",
-      id: 1,
-      date: new Date().toISOString(),
-      payment: {
-        accountNumber: "4111111111111111",
-        paymentType: "Visa",
-      }
-    };
 
   const mockedEvent = {
     preventDefault: jest.fn(),
   };
 
-  let layout;
-
-  // beforeEach(() => {
-
-  // });
-
-  // afterEach(() => {
-  //   layout = undefined;
-  // });
-
-  it("takes an event",() => {
-    // layout.setProps({
-    //   dispatch: jest.fn(),
-    // });
-
-    layout = shallow(
-      <ChangePayments
-        savedAccounts={accountShape}
-        currentAccount={currentAccount}
-        dispatch={jest.fn()}
-      />
-    );
-
-    const { changeAccounts } = layout.instance();
+  it("takes an event, and calls preventDefault",() => {
     expect(changeAccounts).toBeDefined();
     changeAccounts(mockedEvent);
     expect(mockedEvent.preventDefault).toBeCalled();
-
-    console.log(layout.props());
-    expect(shallowToJson(layout)).toMatchSnapshot();
-    expect(layout.props().dispatch).toHaveBeenCalledTimes(2);
-
-    // expect(changeAccounts(mockedEvent));
-    // changeAccounts(mockedEvent);
-
-    // expect(changeAccounts(mockedEvent.onClick));
-    // expect(changeAccounts(jest.fn()))
-
-    // const wrapper = shallow(<ChangePayments />);
-    // console.log(wrapper);
   });
 
-  // it("prevents default", () => {
-  //   const { changeAccounts } = layout.instance();
+  it("dispatches an change to setAccount", () => {
+    changeAccounts(mockedEvent);
+    expect(component.instance().props.dispatch).toHaveBeenCalledTimes(2);
 
-  // });
+    const dispatchCalls = component.instance().props.dispatch.mock.calls[0];
 
+    expect(dispatchCalls).toMatchSnapshot();
+  });
 
-  // it("dispatches an change to setAccount");
-  // it("dispatches modal.hide()");
+  it("dispatches modal.hide()", () => {
+    changeAccounts(mockedEvent);
+    expect(component.instance().props.dispatch).toHaveBeenCalledTimes(2);
+
+    const dispatchCalls = component.instance().props.dispatch.mock.calls[1];
+    expect(dispatchCalls).toMatchSnapshot();
+  });
+});
+
+describe("choose", () => {
+
+  it("takes an event, and calls preventDefault",() => {
+    let { choose } = component.instance();
+    expect(choose).toBeDefined();
+    choose(mockedEvent);
+    expect(mockedEvent.preventDefault).toBeCalled();
+  });
+
+  it("set's a savedAccount state", () => {
+    let { choose } = component.instance();
+    choose(mockedEvent);
+    const savedAccountState = component.instance().state.savedAccount;
+    expect(savedAccountState).toMatchSnapshot();
+  });
 });
