86cc916ac7241b59cc6bdfe128e8e8eac74ae712
John Pinkerton <jpinkerton88@gmail.com>
2016-11-15 19:46:44 -0500

    test history index

---
+++ imports/pages/give/history/__tests__/index.js
@@ -0,0 +1,41 @@
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import { TemplateWithoutData as Template } from "../";
+
+const defaultProps = {
+  loading: false,
+  transactions: [],
+  changeDates: jest.fn(),
+  changeFamily: jest.fn(),
+  Loading: jest.fn(),
+  done: false,
+  filter: {
+    family: [],
+  },
+  dispatch: jest.fn(),
+};
+
+const generateComponent = (additionalProps = {}) => {
+  const newProps = {
+    ...defaultProps,
+    ...additionalProps,
+  };
+  return <Template { ...newProps } />;
+};
+
+it("renders with props", () => {
+  const wrapper = shallow(generateComponent());
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("wrapRefetch updates refetching state", () => {
+  const wrapper = shallow(generateComponent());
+  const mockRefetch = jest.fn().mockReturnValue(new Promise((r) => r("test")));
+  // call function returned by wrapRefetch
+  wrapper.instance().wrapRefetch(mockRefetch)().then((x) => {
+    expect(x).toBe("xest");
+    expect(mockRefetch).toHaveBeenCalledTimes(1);
+    expect(wrapper.state().refetching).toBe(false);
+  });
+  expect(wrapper.state().refetching).toBe(true);
+});
