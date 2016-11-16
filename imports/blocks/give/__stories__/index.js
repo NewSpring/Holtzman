e8f1fb5f19a3c06613fce9118c0dd77b60c0fc3a
richarddubay <richardtdubayjr@gmail.com>
2016-11-16 11:16:55 -0500

    removed unnecessary things

--- imports/blocks/give/__stories__/index.js
+++ imports/blocks/give/__stories__/index.js
@@ -12,11 +12,6 @@ import Provider from "/.storybook/mocks/Provider";
 
 import Readme from "./index.md";
 import Give from "../";
-// import Err from "../Err";
-// import Layout from "../Layout";
-// import Loading from "../Loading";
-// import Success from "../Success";
-import { modal, give as giveActions } from "../../../store";
 
 const story = storiesOf("Give", module)
   .addDecorator(withKnobs)
@@ -26,9 +21,7 @@ const story = storiesOf("Give", module)
 
 const defaultProps = {
   give: {
-    savedAccount: {
-      id: "1",
-    }
+    savedAccount: {}
   },
   dispatch: () => {},
   data: {
@@ -47,42 +40,3 @@ story
       </Provider>
     )
   }));
-  // .add("Err", withReadme(Readme, () => {
-  //   return (
-  //     <Err />
-  //   )
-  // }))
-  // .add("Layout", withReadme(Readme, () => {
-  //   return (
-  //     <Layout />
-  //   )
-  // }))
-  // .add("Loading", withReadme(Readme, () => {
-  //   return (
-  //     <Loading />
-  //   )
-  // }))
-  // .add("Success", withReadme(Readme, () => {
-  //   return (
-  //     <Success />
-  //   )
-  // }));
-  // .add("With one account", withReadme(Readme, () => {
-  //   const name = text("Fund", "General Fund");
-  //   return (
-  //     <Provider>
-  //       <AddToCart accounts={[{ name, id: 1 }]} />
-  //     </Provider>
-  //   );
-  // }))
-  // .add("With two accounts", withReadme(Readme, () => {
-  //   const accounts = [
-  //     { name: text("Fund 1", "General Fund"), id: 1 },
-  //     { name: text("Fund 2", "Building Fund"), id: 2 }
-  //   ]
-  //   return (
-  //     <Provider>
-  //       <AddToCart accounts={accounts} />
-  //     </Provider>
-  //   );
-  // }));
