0ab126dec14d497a690ab4e2fd93593d5da4c48d
John Pinkerton <jpinkerton88@gmail.com>
2016-11-16 16:29:56 -0500

    test group finder layout

---
+++ imports/pages/groups/finder/__tests__/Layout.js
@@ -0,0 +1,43 @@
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import Layout from "../Layout";
+
+const defaultProps = {
+  tags: [
+    { value: "test" },
+    { value: "test" },
+  ],
+  tagOnClick: jest.fn(),
+  submitTags: jest.fn(),
+  canSearchTags: true,
+  findByQuery: jest.fn(),
+  inputOnChange: jest.fn(),
+  content: [{}, {}],
+};
+
+const generateComponent = (additionalProps = {}) => {
+  const newProps = {
+    ...defaultProps,
+    ...additionalProps,
+  };
+  return <Layout { ...newProps } />;
+};
+
+it("renders with props", () => {
+  const wrapper = shallow(generateComponent());
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders disabled tags", () => {
+  const wrapper = shallow(generateComponent({
+    canSearchTags: false,
+  }));
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders loading if no tags", () => {
+  const wrapper = shallow(generateComponent({
+    tags: [],
+  }));
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
