32ef6291813b5b3cda00e055debf60d6ba0e0cdc
John Pinkerton <jpinkerton88@gmail.com>
2016-11-17 13:59:37 -0500

    test home address index

---
+++ imports/pages/profile/settings/HomeAddress/__tests__/index.js
@@ -0,0 +1,103 @@
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import {
+  nav,
+} from "../../../../../store";
+import {
+  HomeAddressWithoutData as HomeAddress,
+  PERSON_HOME_QUERY,
+} from "../";
+
+jest.mock("../../../../../methods/accounts/browser", () => ({
+  updateHome: jest.fn(),
+}));
+jest.mock("../../../../../store", () => ({
+  nav: {
+    setLevel: jest.fn(),
+  },
+}));
+
+const defaultProps = {
+  dispatch: jest.fn(),
+  data: {
+    refetch: jest.fn(),
+    person: {
+      home: {},
+    },
+  },
+};
+
+const generateComponent = (additionalProps = {}) => {
+  const newProps = {
+    ...defaultProps,
+    ...additionalProps,
+  };
+  return <HomeAddress { ...newProps } />;
+};
+
+it("renders with props", () => {
+  const wrapper = shallow(generateComponent());
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders without person home", () => {
+  const wrapper = shallow(generateComponent({
+    data: {
+      person: {
+        home: null,
+      },
+    },
+  }));
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders error version", () => {
+  const wrapper = shallow(generateComponent());
+  wrapper.setState({ state: "error" });
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders loading version", () => {
+  const wrapper = shallow(generateComponent());
+  wrapper.setState({ state: "loading" });
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("renders success version", () => {
+  const wrapper = shallow(generateComponent());
+  wrapper.setState({ state: "success" });
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("parses query", () => {
+  expect(PERSON_HOME_QUERY).toMatchSnapshot();
+});
+
+it("updates nav on mount", () => {
+  const mockDispatch = jest.fn();
+  nav.setLevel = jest.fn();
+  const wrapper = shallow(generateComponent({
+    dispatch: mockDispatch,
+  }));
+  expect(mockDispatch).toHaveBeenCalledTimes(1);
+  expect(nav.setLevel).toHaveBeenCalledTimes(1);
+  expect(nav.setLevel).toHaveBeenCalledWith("BASIC_CONTENT");
+});
+
+it("updates nav on unmount", () => {
+  const mockDispatch = jest.fn();
+  nav.setLevel = jest.fn();
+  const wrapper = shallow(generateComponent({
+    dispatch: mockDispatch,
+  }));
+  wrapper.unmount();
+  expect(mockDispatch).toHaveBeenCalledTimes(2);
+  expect(nav.setLevel).toHaveBeenCalledTimes(2);
+  expect(nav.setLevel).toHaveBeenCalledWith("TOP");
+});
+
+it("updateAddress calls updateHome with data", () => {
+  const wrapper = shallow(generateComponent());
+  wrapper.instance().updateAddress();
+  expect(wrapper.state().state).toBe("loading");
+});
