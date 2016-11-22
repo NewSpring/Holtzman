ce5f7d8ffdcdc9fd56ac36467aeb242369f60ad0
John Pinkerton <jpinkerton88@gmail.com>
2016-11-21 22:31:07 -0500

    test discover feed index

---
+++ imports/blocks/discover/feed/__tests__/index.js
@@ -0,0 +1,47 @@
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import { print } from "graphql-tag/printer";
+import {
+  DiscoverWithoutData as Discover,
+  DISCOVER_QUERY,
+} from "../";
+
+const defaultProps = {
+  discover: {
+    loading: false,
+    items: [
+      { status: "featured" },
+      { status: "featured" },
+      { status: "featured" },
+      { status: "open" },
+      { status: "open" },
+      { status: "open" },
+    ],
+  },
+};
+
+const generateComponent = (additionalProps = {}) => {
+  const newProps = {
+    ...defaultProps,
+    ...additionalProps,
+  };
+  return <Discover { ...newProps } />;
+};
+
+it("renders with props", () => {
+  const wrapper = shallow(generateComponent());
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("doesn't render if loading", () => {
+  const wrapper = shallow(generateComponent({
+    discover: {
+      loading: true,
+    },
+  }));
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("parses query correctly", () => {
+  expect(print(DISCOVER_QUERY)).toMatchSnapshot();
+});
