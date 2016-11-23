4832cba53068d6fe25f037778a714d176c1e5b5b
John Pinkerton <jpinkerton88@gmail.com>
2016-11-23 14:27:44 -0500

    test query and cash tag check

--- imports/pages/_/__tests__/index.js
+++ imports/pages/_/__tests__/index.js
@@ -1,6 +1,12 @@
 import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
-import { Template } from "../";
+import { print } from "graphql-tag/printer";
+import { GraphQL } from "../../../graphql";
+import {
+  Template,
+  CASH_TAG_QUERY,
+  matchAccountToCashTag,
+} from "../";
 
 const generateComponent = () => (
   <Template>
@@ -11,4 +17,45 @@ const generateComponent = () => (
 it("renders with children", () => {
   const wrapper = shallow(generateComponent());
   expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it("parses query", () => {
+  expect(print(CASH_TAG_QUERY)).toMatchSnapshot();
+});
+
+it("matchAccountToCashTag queries based of url", () => {
+  const mockThen = jest.fn();
+  GraphQL.query = jest.fn(() => ({
+    then: mockThen,
+  }));
+  const mockLocation = {
+    params: {
+      splat: "$test/10",
+    }
+  };
+  const mockReplaceState = jest.fn();
+  const mockCallback = jest.fn();
+  matchAccountToCashTag(mockLocation, mockReplaceState, mockCallback);
+  expect(GraphQL.query).toHaveBeenCalledTimes(1);
+  expect(GraphQL.query).toHaveBeenCalledWith({
+    query: CASH_TAG_QUERY,
+    variables: {
+      tag: "$test",
+    },
+  });
+  expect(mockThen).toHaveBeenCalledTimes(1);
+  // simulate promise
+  mockThen.mock.calls[0][0]({
+    data: {
+      account: {
+        name: "test",
+      },
+    },
+  });
+  expect(mockReplaceState).toHaveBeenCalledTimes(1);
+  expect(mockReplaceState).toHaveBeenCalledWith(
+    null,
+    "/give/campaign/test?test=10"
+  );
+  expect(mockCallback).toHaveBeenCalledTimes(1);
 });
