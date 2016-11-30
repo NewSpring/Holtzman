1c8e8ee767d0b1bf0522172d90598caa1dcd1ea8
richarddubay <richardtdubayjr@gmail.com>
2016-11-30 13:42:50 -0500

    added knobs for changing the graph data

--- imports/components/graphs/lineGraph/__stories__/index.js
+++ imports/components/graphs/lineGraph/__stories__/index.js
@@ -3,7 +3,7 @@ import { Meteor } from "meteor/meteor";
 import { storiesOf } from "@kadira/storybook";
 import {
   withKnobs,
-  text,
+  number,
 } from "@kadira/storybook-addon-knobs";
 import withReadme from "storybook-readme/with-readme";
 import backgrounds from "react-storybook-addon-backgrounds";
@@ -72,6 +72,20 @@ story
         amount: 500,
       },
     ];
+
+    // knobs for changing data
+    data[0].amount = number("January", data[0].amount);
+    data[1].amount = number("February", data[1].amount);
+    data[2].amount = number("March", data[2].amount);
+    data[3].amount = number("April", data[3].amount);
+    data[4].amount = number("May", data[4].amount);
+    data[5].amount = number("June", data[5].amount);
+    data[6].amount = number("July", data[6].amount);
+    data[7].amount = number("August", data[7].amount);
+    data[8].amount = number("September", data[8].amount);
+    data[9].amount = number("October", data[9].amount);
+    data[10].amount = number("November", data[10].amount);
+    data[11].amount = number("December", data[11].amount);
 
     return (
       <LineGraph data={data} />
