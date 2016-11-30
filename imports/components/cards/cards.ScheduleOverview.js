933ddc9c7803e8c49564be02faa048c27aa502f2
edolyne <ed@edolyne.com>
2016-11-30 11:03:40 -0500

    SYS-2555  style and markup refinement for card

--- imports/components/cards/cards.ScheduleOverview.js
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -10,8 +10,8 @@ type IScheduleOverviewCard = {
 };
 
 const getCurrencySymbol = (amount:string) => amount.charAt(0);
-const getDollarAmount = (amount:string) => amount.split(".")[0].slice(1);
-const getCentsAmount = (amount:string) => amount.split(".")[1];
+const getDollars = (amount:string) => amount.split(".")[0].slice(1);
+const getCents = (amount:string) => amount.split(".")[1];
 
 const ScheduleOverviewCard = ({
   amount,
@@ -23,15 +23,18 @@ const ScheduleOverviewCard = ({
 }: IScheduleOverviewCard) => (
   <div className="card">
     <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
-      <h3 className="flush">
-        <span className="h5" style={{ verticalAlign: "middle" }}>{getCurrencySymbol(amount)}</span>
-        <span className="h3" style={{ verticalAlign: "middle" }}>{getDollarAmount(amount)}</span>
-        <span className="h5" style={{ verticalAlign: "middle" }}>.{getCentsAmount(amount)}</span>
-      </h3>
-      <h4>{fund} <span className="h6 text-dark-tertiary" style={{ verticalAlign: "middle" }}>{frequency}</span></h4>
-      <p><span className="h6 text-dark-tertiary" style={{ verticalAlign: "middle" }}>Started: </span>{started}</p>
-      <p><span className="h6 text-dark-tertiary" style={{ verticalAlign: "middle" }}>Latest: </span>{latest}</p>
-      <button className="float" onClick={onEditClick}>Edit</button>
+      <button className="float-right h6 text-dark-tertiary outlined--bottom" style={{ borderColor: "inherit", borderWidth: "2px" }} onClick={onEditClick}>Edit</button>
+      <div className="floating text-left text-dark-primary">
+        <h4 className="floating__item flush" style={{ paddingRight: "5px" }}>{getCurrencySymbol(amount)}</h4>
+        <h2 className="floating__item flush">{getDollars(amount)}</h2>
+        <h4 className="floating__item flush">.{getCents(amount)}</h4>
+      </div>
+      <div className="floating text-left push-bottom">
+        <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
+        <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
+      </div>
+      <p className="flush" style={{ marginBottom: "5px" }}><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Started: </span><em className="text-dark-primary">{started}</em></p>
+      <p className="flush"><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Latest: </span><em className="text-dark-primary">{latest}</em></p>
     </div>
   </div>
 );
