db82e67987312beaf9d66035feefcb5354c27ad9
Edolyne Long <edolyne@users.noreply.github.com>
2016-12-01 14:00:39 -0500

    Create schedule overview card (#1514)
    * SYS-2555 Inital work on schedule overview card
    
    * SYS-2555
     style and markup refinement for card
    
    * SYS-2555 modifying and implimenting appropriate knobs
    
    * SYS-2555 adds overview of component
    
    * SYS-2555 refining codeS
    
    * SYS-2555 added check for latest to add margin
    
    * SYS-2556 Testing schedule overview card
    
    * SYS-2555 added fallback for currency and cents
    
    * SYS-2555 added fallback for dollars
    
    * SYS-2555 moving fallback to the top level function
    
    * SYS-2556 adding fallback snapshot tests
    
    * SYS-2556 Testing Gone Wild

---
+++ imports/components/cards/__stories__/cards.ScheduleOverview.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+import { storiesOf } from "@kadira/storybook";
+import { withKnobs, text } from "@kadira/storybook-addon-knobs";
+import withReadme from "storybook-readme/with-readme";
+import backgrounds from "react-storybook-addon-backgrounds";
+import centered from "/.storybook/decorators/centered";
+import defaultColors from "/.storybook/defaults";
+
+import Readme from "./cards.ScheduleOverview.md";
+import ScheduleOverview from "../cards.ScheduleOverview.js";
+
+const story = storiesOf("Cards", module)
+  .addDecorator(withKnobs)
+  .addDecorator(centered)
+  .addDecorator(backgrounds(defaultColors("light-primary", "light-secondary")))
+  ;
+
+story
+  .add("Schedule Overview", withReadme(
+    Readme,
+    () => {
+      const amount = text("Amount", "$420.00");
+      const fund = text("Fund", "Step Up Fund");
+      const frequency = text("Frequency", "Once A Month");
+      const started = text("Started", "Nov 1, 2015");
+      const latest = text("Latest", "Sep 15, 2016");
+
+      return (
+        <div className={"floating"}>
+          <div className={"grid__item text-left"} style={{ maxWidth: "960px" }}>
+            <ScheduleOverview
+              amount={amount}
+              fund={fund}
+              frequency={frequency}
+              started={started}
+              latest={latest}
+              onEditClick={() => alert('42')}
+            />
+          </div>
+        </div>
+      );
+    }
+  ));
---
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -0,0 +1,47 @@
+// @flow
+
+type IScheduleOverviewCard = {
+  amount: string,
+  fund: string,
+  frequency: string,
+  started: string,
+  latest: string,
+  onEditClick: Function,
+};
+
+// eslint-disable-next-line max-len
+const currencySymbolRegex = /[\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/;
+
+export const getCurrencySymbol = (amount:string) => amount.match(currencySymbolRegex) || "$";
+export const getNegative = (amount:string) => amount.match(/-/);
+export const getDollars = (amount:string) => amount.replace(currencySymbolRegex, "").replace("-", "").split(".")[0] || "0";
+export const getCents = (amount:string) => amount.split(".")[1] || "00";
+
+const ScheduleOverviewCard = ({
+  amount,
+  fund,
+  frequency,
+  started,
+  latest,
+  onEditClick,
+}: IScheduleOverviewCard) => (
+  <div className="card">
+    <div className="card__item soft push-half-ends one-whole">
+      <button className="float-right h6 text-dark-tertiary outlined--bottom" style={{ borderColor: "inherit", borderWidth: "2px" }} onClick={onEditClick}>Edit</button>
+      <div className="floating text-left text-dark-primary">
+        <h4 className="floating__item flush" style={{ paddingRight: "5px" }}>{getCurrencySymbol(amount)}</h4>
+        {getNegative(amount) && <h4 className="floating__item flush" style={{ paddingRight: "3px" }}>{getNegative(amount)}</h4>}
+        <h2 className="floating__item flush">{getDollars(amount)}</h2>
+        <h4 className="floating__item flush">.{getCents(amount)}</h4>
+      </div>
+      <div className="floating text-left push-bottom">
+        <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
+        <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
+      </div>
+      <p className="flush" style={latest ? { marginBottom: "5px" } : {}}><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Started: </span><em className="text-dark-primary">{started}</em></p>
+      {latest && <p className="flush"><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Latest: </span><em className="text-dark-primary">{latest}</em></p>}
+    </div>
+  </div>
+);
+
+export default ScheduleOverviewCard;
