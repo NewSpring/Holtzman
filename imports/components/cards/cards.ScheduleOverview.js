ebe547710aeb7a81af945e8b78569589f66dca6d
edolyne <ed@edolyne.com>
2016-12-01 11:44:29 -0500

    SYS-2556 Testing Gone Wild

--- imports/components/cards/cards.ScheduleOverview.js
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -12,10 +12,10 @@ type IScheduleOverviewCard = {
 // eslint-disable-next-line max-len
 const currencySymbolRegex = /[\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/;
 
-const getCurrencySymbol = (amount:string) => amount.match(currencySymbolRegex) || "$";
-const getNegative = (amount:string) => amount.match(/-/);
-const getDollars = (amount:string) => amount.replace(currencySymbolRegex, "").replace("-", "").split(".")[0] || "0";
-const getCents = (amount:string) => amount.split(".")[1] || "00";
+export const getCurrencySymbol = (amount:string) => amount.match(currencySymbolRegex) || "$";
+export const getNegative = (amount:string) => amount.match(/-/);
+export const getDollars = (amount:string) => amount.replace(currencySymbolRegex, "").replace("-", "").split(".")[0] || "0";
+export const getCents = (amount:string) => amount.split(".")[1] || "00";
 
 const ScheduleOverviewCard = ({
   amount,
