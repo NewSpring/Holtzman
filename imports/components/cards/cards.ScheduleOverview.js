de29dc386a6ad8767bcc1e0d74d7191dafad3a84
edolyne <ed@edolyne.com>
2016-12-01 10:36:32 -0500

    SYS-2555 added check for latest to add margin

--- imports/components/cards/cards.ScheduleOverview.js
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -13,7 +13,7 @@ type IScheduleOverviewCard = {
 const currencySymbolRegex = /[\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/;
 
 const getCurrencySymbol = (amount:string) => amount.match(currencySymbolRegex);
-const getNegative = (amount:string) => amount.match(/-/g);
+const getNegative = (amount:string) => amount.match(/-/);
 const getDollars = (amount:string) => amount.replace(currencySymbolRegex, "").replace("-", "").split(".")[0];
 const getCents = (amount:string) => amount.split(".")[1];
 
@@ -26,7 +26,7 @@ const ScheduleOverviewCard = ({
   onEditClick,
 }: IScheduleOverviewCard) => (
   <div className="card">
-    <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
+    <div className="card__item soft push-half-ends one-whole">
       <button className="float-right h6 text-dark-tertiary outlined--bottom" style={{ borderColor: "inherit", borderWidth: "2px" }} onClick={onEditClick}>Edit</button>
       <div className="floating text-left text-dark-primary">
         <h4 className="floating__item flush" style={{ paddingRight: "5px" }}>{getCurrencySymbol(amount)}</h4>
@@ -38,7 +38,7 @@ const ScheduleOverviewCard = ({
         <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
         <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
       </div>
-      <p className="flush" style={{ marginBottom: "5px" }}><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Started: </span><em className="text-dark-primary">{started}</em></p>
+      <p className="flush" style={latest ? { marginBottom: "5px" } : {}}><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Started: </span><em className="text-dark-primary">{started}</em></p>
       {latest && <p className="flush"><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Latest: </span><em className="text-dark-primary">{latest}</em></p>}
     </div>
   </div>
