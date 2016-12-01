69d1b78b802b470a5f09c5ce0f72651ea2eee18e
edolyne <ed@edolyne.com>
2016-12-01 10:37:51 -0500

    SYS-2556 Testing schedule overview card

---
+++ imports/components/cards/__tests__/cards.ScheduleOverview.js
@@ -0,0 +1,55 @@
+import renderer from "react-test-renderer";
+import { reset } from "aphrodite/lib/inject";
+import ScheduleOverview from "../cards.ScheduleOverview";
+
+describe("ScheduleOverview", () => {
+  beforeEach(() => {
+    reset();
+  });
+
+  afterEach(() => {
+    reset();
+  });
+
+  it("has an amount, fund, frequency, started, latest and onEditClick.", () => {
+    const tree = renderer.create(
+      <ScheduleOverview
+        amount="$420.00"
+        fund="Step Up Fund"
+        frequency="Once A Month"
+        started="Jun 15, 2015"
+        latest="Nov 15, 2016"
+        onEditClick={jest.fn()}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("correctly displays a negative value.", () => {
+    const tree = renderer.create(
+      <ScheduleOverview
+        amount="$-420.00"
+        fund="Step Up Fund"
+        frequency="Once A Month"
+        started="Jun 15, 2015"
+        latest="Nov 15, 2016"
+        onEditClick={jest.fn()}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("doesn't have a latest transaction, if one isn't passed", () => {
+    const tree = renderer.create(
+      <ScheduleOverview
+        amount="$420.00"
+        fund="Step Up Fund"
+        frequency="Once A Month"
+        started="Jun 15, 2015"
+        onEditClick={jest.fn()}
+      />
+    )
+
+    expect(tree).toMatchSnapshot();
+  });
+});
