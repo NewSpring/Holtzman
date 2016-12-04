bf358c2cabff4f647357beb0310ad5aadd45d5e8
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-04 11:36:54 -0500

    added knobs to select button style

--- imports/components/buttons/__stories__/small.js
+++ imports/components/buttons/__stories__/small.js
@@ -5,11 +5,27 @@ import withReadme from "storybook-readme/with-readme";
 import backgrounds from "react-storybook-addon-backgrounds";
 import centered from "/.storybook/decorators/centered";
 import defaultColors from "/.storybook/defaults";
+import {
+  withKnobs,
+  select,
+} from "@kadira/storybook-addon-knobs";
 
 import SmallButton from "../small";
 import Readme from "./small-button.md";
 
+const classes=[
+  "btn--disabled",
+  "btn--corners",
+  "btn--light",
+  "btn--filled",
+  "btn--dark-primary",
+  "btn--dark-secondary",
+  "btn--dark-tertiary",
+  "btn--alert",
+];
+
 const story = storiesOf("SmallButton", module)
+  .addDecorator(withKnobs)
   .addDecorator(centered)
   .addDecorator(backgrounds(defaultColors("light-primary", "light-secondary")))
   ;
@@ -18,7 +34,10 @@ story
   .add("Basic Small Button", withReadme(
     Readme,
     () => (
-      <SmallButton text="See All"/>
+      <SmallButton
+        text="See All"
+        className={select("className", classes, "btn--dark-secondary")}
+      />
     )
   ))
   .add("With Link", withReadme(
@@ -27,16 +46,10 @@ story
       <SmallButton linkUrl="http://newspring.cc" text="See All"/>
     )
   ))
-  .add("Disabled (no link)", withReadme(
+  .add("Disabled", withReadme(
     Readme,
     () => (
       <SmallButton disabled text="See All"/>
-    )
-  ))
-  .add("Disabled (with link)", withReadme(
-    Readme,
-    () => (
-      <SmallButton disabled linkUrl="http://newspring.cc" text="See All"/>
     )
   ))
   .add("With onClick Action", withReadme(
