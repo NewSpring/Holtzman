d84906388736696891f37027320f3c1c8374a3d7
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-04 10:51:03 -0500

    removed styled-components

--- imports/components/buttons/small.js
+++ imports/components/buttons/small.js
@@ -3,73 +3,43 @@
 
 import { Link } from "react-router";
 
-import styled from "styled-components";
-
-const defaultStyles = `
-  padding: 5px 15px 4px;
-  font-weight: normal;
-  font-size: .75em;
-  border-radius: 50px;
-  border: 2px solid #858585;
-  color: #858585;
-  border-radius: 25px;
-  cursor: pointer;
-  &:hover {
-    background-color: transparent;
-    color: #505050;
-    border-color: #505050;
-  }
-`;
-
-const disabledStyles = `
-  color: #dddddd;
-  border-color: #dddddd;
-  &:hover {
-    color: #dddddd;
-    border-color: #dddddd;
-  }
-`;
-
-const SmallButtonWrapper = styled.button`${defaultStyles}`;
-const StyledLink = styled(Link)`${defaultStyles}`;
-
 type ISmallButton = {
-  linkUrl?: String,
+  linkUrl?: string,
   onClick?: Function,
   disabled?: Boolean,
   text: String,
-  className?: String,
+  className?: string,
   style?: Object,
 };
 
 const SmallButton = (props: ISmallButton) => {
-  let ButtonWrapper;
 
-  // if there's a url, we need a react-router Link component
-  props.linkUrl ? (ButtonWrapper = StyledLink) : (ButtonWrapper = SmallButtonWrapper);
+  const classes = `
+    btn btn--small
+    ${props.disabled ? "btn--disabled" : ""}
+    ${props.className || ""}
+  `;
 
-  // create a disabled style for the component chosen
-  const DisabledButtonWrapper = styled(ButtonWrapper)`${disabledStyles}`;
-
-  if (props.disabled) {
+  if (props.linkUrl) {
     return (
-      <DisabledButtonWrapper
-        className={props.className || ""}
+      <Link
+        className={classes}
         style={props.style || {}}
+        onClick={props.onClick}
+        to={props.linkUrl}
       >
-        {props.text || "See All"}
-      </DisabledButtonWrapper>
+        {props.text}
+      </Link>
     );
   } else {
     return (
-      <ButtonWrapper
-        className={props.className || ""}
-        onClick={props.onClick}
+      <button
+        className={classes}
         style={props.style || {}}
-        to={props.linkUrl || ""}
+        onClick={props.onClick}
       >
-        {props.text || "See All"}
-      </ButtonWrapper>
+        {props.text}
+      </button>
     );
   }
 };
