2f5e8c6bbcb57f8318b45a5c6635fc7fe941c6be
Edolyne Long <edolyne@users.noreply.github.com>
2016-12-04 09:50:19 -0500

    SYS-2555 SYS-2556 Creation of currency component (#1530)
    * SYS-2555 SYS-2556 Creation of currency component
    
    * Removing obsolete snapshots

---
+++ imports/components/currency/__stories__/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import { storiesOf } from "@kadira/storybook";
+import { withKnobs, text } from "@kadira/storybook-addon-knobs";
+import withReadme from "storybook-readme/with-readme";
+import backgrounds from "react-storybook-addon-backgrounds";
+import centered from "/.storybook/decorators/centered";
+import defaultColors from "/.storybook/defaults";
+
+import Readme from "./index.md";
+import Currency from "../";
+
+const story = storiesOf("Currency", module)
+  .addDecorator(withKnobs)
+  .addDecorator(centered)
+  .addDecorator(backgrounds(defaultColors("light-primary", "light-secondary")))
+  ;
+
+story
+  .add("Currency", withReadme(
+    Readme,
+    () => {
+      const amount = text("Amount", "$420.00");
+
+      return (
+        <div className={"floating"}>
+          <div className={"grid__item text-left"} style={{ maxWidth: "960px" }}>
+            <Currency
+              amount={amount}
+            />
+          </div>
+        </div>
+      );
+    }
+  ));
---
+++ imports/components/currency/__stories__/index.md
@@ -0,0 +1,19 @@
+## Currency component
+
+### Usage
+
+```js
+import Currency from "../../components/currency";
+
+<Currency
+  amount="$420.00"
+/>
+```
+
+### Properties
+
+* **amount** - a string that contains the monetary value.
+
+### Purpose
+
+This component is the primary way that we display currency across our sites.
