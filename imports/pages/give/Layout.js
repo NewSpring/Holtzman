39816281220276bdf07480985ac71d97b31afdb5
richarddubay <richardtdubayjr@gmail.com>
2016-12-04 15:18:16 -0500

    beginning layout

---
+++ imports/pages/give/Layout.js
@@ -0,0 +1,50 @@
+// @flow
+import Split, { Left, Right } from "../../blocks/split";
+import Dashboard from "../../components/dashboard/";
+
+const subNav = [
+  {
+    isActive: true,
+    linkUrl: "https://my.newspring.cc/give",
+    onClick: () => {},
+    title: "Home",
+  },
+  {
+    isActive: false,
+    linkUrl: "https://my.newspring.cc/give/now",
+    onClick: () => {},
+    title: "Give Now",
+  },
+  {
+    isActive: false,
+    linkUrl: "https://my.newspring.cc/give/history",
+    onClick: () => {},
+    title: "History",
+  },
+];
+
+type ILayout = {
+  title: string,
+};
+
+const Layout = ({
+  title,
+}: ILayout) => (
+  <div>
+    <Split>
+      <Right
+        background="//s3.amazonaws.com/ns.images/newspring/christmasoffering/christmas_offering_1x1.png"
+      />
+    </Split>
+    <Left>
+      <Dashboard
+        title="Your Giving"
+        subNav={subNav}
+      >
+        <div>Left Side</div>
+      </Dashboard>
+    </Left>
+  </div>
+);
+
+export default Layout;
