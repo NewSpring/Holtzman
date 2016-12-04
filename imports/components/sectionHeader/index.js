699c80a63fea3ea6193457c308c18e6e6b843630
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-04 16:52:57 -0500

    abstracted component

---
+++ imports/components/sectionHeader/index.js
@@ -0,0 +1,22 @@
+
+const halvesClasses = "one-half floating display-inline-block soft-half";
+
+type ISectionHeader = {
+  title?: string,
+  link?: Object, // React component
+};
+
+const SectionHeader = ({ title, link }: ISectionHeader) =>
+  <div className="background--light-secondary one-whole">
+    <div className={`${halvesClasses} floating--left`}>
+      <h6 className="floating__item text-dark-tertiary flush">
+        {title}
+      </h6>
+    </div>
+    <div className={`${halvesClasses} floating--right`}>
+      {link}
+    </div>
+  </div>
+  ;
+
+export default SectionHeader;
