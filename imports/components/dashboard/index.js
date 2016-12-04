d3ff96e1f5ad081be70a0e688067bcff19eb0dd7
richarddubay <richardtdubayjr@gmail.com>
2016-12-04 15:05:08 -0500

    fix dashboard styles

--- imports/components/dashboard/index.js
+++ imports/components/dashboard/index.js
@@ -50,8 +50,8 @@ const Dashboard = ({
   children,
 }: IDashboard) => (
   <div>
-    <div className="soft-half@handheld hard-bottom soft-double@lap-and-up soft-double-top@lap-wide-and-up background--light-primary">
-      <h1 className="soft-bottom">{title}</h1>
+    <div className="push-top soft-left@handheld soft-top@handheld soft-double-left soft-double-top background--light-primary">
+      <h1 className="soft-half-bottom@handheld soft-bottom">{title}</h1>
       <div className="floating text-left">
         {getLinks(subNav)}
       </div>
