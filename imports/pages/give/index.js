438e4286463251fe2bf57b884494e0b272d3896e
richarddubay <richardtdubayjr@gmail.com>
2016-12-04 20:58:00 -0500

    updated child routes for give home page

--- imports/pages/give/index.js
+++ imports/pages/give/index.js
@@ -1,6 +1,7 @@
 // @flow
 import Campaign from "./campaign";
 import History from "./history";
+import Home from "./home";
 import Now from "./now";
 import Review from "./review";
 import Schedules from "./schedules";
@@ -9,9 +10,11 @@ import Layout from "./Layout";
 export default {
   path: "give",
   component: Layout,
+  indexRoute: { onEnter: (nextState: Object, replace: Function) => replace("/give/home") },
   childRoutes: [
     ...Campaign.Routes,
     ...History.Routes,
+    ...Home.Routes,
     ...Now.Routes,
     ...Review.Routes,
     ...Schedules.Routes,
