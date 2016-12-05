a7ad01b7298f19d8bb9540766d34f3a1d6069711
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-05 10:11:11 -0500

    Added component

--- imports/components/progressBar/__stories__/index.js
+++ imports/components/progressBar/__stories__/index.js
@@ -6,56 +6,33 @@ import centered from "/.storybook/decorators/centered";
 import defaultColors from "/.storybook/defaults";
 import styled from "styled-components";
 
+import ProgressBar from "../";
 import Readme from "./progress-bar.md";
-
-const Wrapper = styled.div`
-  background-color: #6bac43;
-  color: white;
-  height: 500px;
-  padding: 8px;
-`;
-const Title = styled.h6`
-  display: inline-block;
-`;
-const Total = styled.h5`
-  display: inline-block;
-`;
-const Progress = styled.div`
-  &:after {
-    background-color: #1c683e;
-  }
-`;
-const ProgressItem = styled.div`
-  width: 50%;
-  height: 100%;
-  background-color: #ffffff;
-`;
-
-const ProgressBar = () => {
-  return (
-    <Wrapper>
-      <div className="floating floating--left one-half" style={{display:"inline-block"}}>
-        <Title className="floating--item">Fund Title</Title>
-      </div>
-      <div className="floating floating--right one-half" style={{display:"inline-block"}}>
-        <Total>$1000</Total>
-      </div>
-      <Progress className="progress">
-        <ProgressItem className="" />
-      </Progress>
-    </Wrapper>
-  );
-};
 
 const story = storiesOf("ProgressBar", module)
   .addDecorator(centered)
-  .addDecorator(backgrounds(defaultColors("light-primary", "light-secondary")))
+  .addDecorator(backgrounds(defaultColors()))
   ;
 
 story
   .add("Dark", withReadme(
     Readme,
     () => (
-      <ProgressBar />
+      <ProgressBar
+        title="My Fund"
+        total="1000"
+        percentDone="35"
+      />
+    )
+  ))
+  .add("Light", withReadme(
+    Readme,
+    () => (
+      <ProgressBar
+        title="My Fund"
+        total="1000"
+        percentDone="35"
+        theme="light"
+      />
     )
   ));
--- imports/components/progressBar/index.js
+++ imports/components/progressBar/index.js
@@ -1,6 +1,64 @@
+/*
+<ProgressBar
+  title={string}
+  total={string}
+  percentDone={?number}
+  theme={?string = "light" || "dark" } // default dark
+  style={?object}
+/>
+*/
+
+const inlineBlock = { display: "inline-block" };
+
+const themeStyles = (theme, percentDone) => {
+  if (theme === "light") {
+    return {
+      wrapper: {
+        color: "#6bac43",
+      },
+      progressBackground: {
+        width: "100%",
+        height: "3px",
+        backgroundColor: "#f7f7f7",
+      },
+      progress: {
+        height: "100%",
+        width: `${percentDone || 0}%`,
+        backgroundColor: "#6bac43",
+      },
+    };
+  }
+  return {
+    wrapper: {
+      color: "#fff",
+    },
+    progressBackground: {
+      width: "100%",
+      height: "3px",
+      backgroundColor: "#2a4930",
+    },
+    progress: {
+      height: "100%",
+      width: `${percentDone || 0}%`,
+      backgroundColor: "white",
+    },
+  };
+};
 
 const ProgressBar = (props) => {
-  return ();
-}
+  return (
+    <div className="soft-half" style={themeStyles(props.theme).wrapper}>
+      <div className="floating floating--left one-half" style={inlineBlock}>
+        <h6 className="floating--item" style={inlineBlock}>{props.title}</h6>
+      </div>
+      <div className="floating floating--right one-half" style={inlineBlock}>
+        <h5 style={inlineBlock}>${props.total}</h5>
+      </div>
+      <div style={themeStyles(props.theme).progressBackground}>
+        <div style={themeStyles(props.theme, props.percentDone).progress} />
+      </div>
+    </div>
+  );
+};
 
 export default ProgressBar;
