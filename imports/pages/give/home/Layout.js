0c9130d3b252f0eec669062f54dcd08ef4684040
richarddubay <richardtdubayjr@gmail.com>
2016-12-04 20:56:53 -0500

    added give home pages

---
+++ imports/pages/give/home/Layout.js
@@ -0,0 +1,12 @@
+// @flow
+import { Component } from "react";
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>Welcome Home</div>
+    );
+  }
+}
+
+export default Layout;
