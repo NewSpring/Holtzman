5c39a188fefda4d0ef048a6ebd761b725690dd38
richarddubay <richardtdubayjr@gmail.com>
2016-12-04 20:58:30 -0500

    updated layout to work with child routes

--- imports/pages/give/Layout.js
+++ imports/pages/give/Layout.js
@@ -1,28 +1,8 @@
 // @flow
+import find from "lodash.find";
 import { Component } from "react";
 import Split, { Left, Right } from "../../blocks/split";
 import Dashboard from "../../components/dashboard/";
-
-const subNav = [
-  {
-    isActive: true,
-    linkUrl: "https://my.newspring.cc/give",
-    onClick: () => {},
-    title: "Home",
-  },
-  {
-    isActive: false,
-    linkUrl: "https://my.newspring.cc/give/now",
-    onClick: () => {},
-    title: "Give Now",
-  },
-  {
-    isActive: false,
-    linkUrl: "https://my.newspring.cc/give/history",
-    onClick: () => {},
-    title: "History",
-  },
-];
 
 type ILayout = {
   children?: React$Element<any>,
@@ -30,6 +10,63 @@ type ILayout = {
 
 class Layout extends Component {
   props: ILayout;
+  state = {
+    subNav: [
+      {
+        isActive: false,
+        linkUrl: "/give/home",
+        onClick: () => {},
+        title: "Home",
+      },
+      {
+        isActive: false,
+        linkUrl: "/give/now",
+        onClick: () => {},
+        title: "Give Now",
+      },
+      {
+        isActive: false,
+        linkUrl: "/give/history",
+        onClick: () => {},
+        title: "History",
+      },
+    ],
+  }
+
+  componentWillMount() {
+    this.updateActive(this.props);
+  }
+
+  componentWillReceiveProps(props: Object) {
+    this.updateActive(props);
+  }
+
+  updateActive = (props: Object) => {
+    const { pathname } = props.location;
+
+    this.setState((state) => state.subNav.map((x) => {
+      const nav = x;
+      nav.isActive = false;
+      if (nav.linkUrl === pathname) {
+        nav.isActive = true;
+      }
+
+      return nav;
+    }));
+  }
+
+  getActive = (props: Object) => {
+    const active = props.location.pathname.split("/");
+    active.shift();
+    active.shift();
+    return find(props.routes, { path: active[0] });
+  }
+
+  getRightComponent = (props: Object) => {
+    const { rightComponent } = this.getActive(props);
+    // XXX handle function as children
+    return rightComponent || null;
+  }
 
   render() {
     return (
@@ -37,12 +74,14 @@ class Layout extends Component {
         <Split>
           <Right
             background="//s3.amazonaws.com/ns.images/newspring/christmasoffering/christmas_offering_1x1.png"
-          />
+          >
+            {this.getRightComponent(this.props)}
+          </Right>
         </Split>
         <Left>
           <Dashboard
             title="Your Giving"
-            subNav={subNav}
+            subNav={this.state.subNav}
           >
             {this.props.children}
           </Dashboard>
