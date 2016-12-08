54ee57654f4247ef78a0ef3decccd6a6f2a736e9
edolyne <ed@edolyne.com>
2016-12-08 11:17:15 -0500

    SYS-2574 finalizing markup

--- imports/components/cards/cards.SavedPayment.js
+++ imports/components/cards/cards.SavedPayment.js
@@ -1,17 +1,10 @@
 // @flow
 
 // NPM based imports
-import { Link } from "react-router";
 import AccountType from "../../components/accountType";
 
-
 type ISavedPaymentCard = {
-  id: string,
-  name: string,
-  payment: {
-    accountNumber: string,
-    paymentType: string,
-  },
+  payment: Object,
   onClick: Function,
 };
 
@@ -19,22 +12,27 @@ type ISavedPaymentCard = {
 // it should less intelligent and use the other props directly
 // Unused props: icon, link, type, images
 const SavedPaymentCard = ({
-  id,
-  name,
   payment,
   onClick,
 }: ISavedPaymentCard) => (
   <div className="card" onClick={onClick}>
     <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
-      <h5 className="text-dark-primary capitalize">{name}</h5>
-      <div className="floating text-left">
-        <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.accountNumber.slice(-4)}</h6>
-        <div className="floating__item">
-          <AccountType
-            width="40px"
-            height="25px"
-            type={payment.paymentType}
-          />
+      <div className="floating grid">
+        <div className="floating__item grid__item two-thirds text-left">
+          <h5 className="text-dark-primary capitalize push-half-bottom">{payment.name}</h5>
+          <div className="floating text-left">
+            <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.payment.accountNumber.slice(-4)}</h6>
+            <div className="floating__item">
+              <AccountType
+                width="40px"
+                height="25px"
+                type={payment.payment.paymentType}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="floating__item grid__item one-third text-right">
+          <i className="icon-arrow-next" />
         </div>
       </div>
     </div>
