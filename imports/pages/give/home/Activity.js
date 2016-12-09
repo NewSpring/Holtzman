6dbc417bc0bed27d996c4c5c043808e5c5cecd49
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-08 20:48:12 -0500

    Activity Cards in Dashboard (#1556)
    * beginning layout
    
    * removed the layout type
    
    * updated give/now and history to not have right/left and fixed the routes
    
    * added give home pages
    
    * updated child routes for give home page
    
    * updated layout to work with child routes
    
    * redirect to /give/now if not logged in
    
    * redirect /give/schedules to /give/home
    
    * fixed lint errors
    
    * fixed snapshots and flow typing for meteor
    
    * using the third array item
    
    * remove left and right from give history transaction page
    
    * removed the back button
    
    * the campaign route is no longer a child
    
    * edits to allow right side component or image
    
    * fixed flow error
    
    * clone the children
    
    * updated to use setRightProps in the component and the tests
    
    * updated to use setRightProps in the component and the tests
    
    * updated to use setRightProps
    
    * removed split and updated tests
    
    * added custom image and updated tests
    
    * changed custom image back to original
    
    * update the custom right side image
    
    * fixed image paths
    
    * added link
    
    * redirect saved payments to /give/home
    
    * the home menu option takes you to /give/home instead of /give/now
    
    * updated image paths for history and history/detail pages
    
    * added logic to activity component, rendered header
    
    * activity feed works
    
    * added linkUrl for successful transactions
    
    * flow typings
    
    * commented out the accounts parsing
    
    * added tests for activity, and removed accounts for now

---
+++ imports/pages/give/home/Activity.js
@@ -0,0 +1,165 @@
+
+// @flow
+
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import ActivityCard from "../../../components/cards/cards.Activity";
+
+const ACTIVITY_QUERY = gql`
+  query userFeed($filters: [String]!) {
+    userFeed(filters: $filters) {
+      ... on Transaction {
+        id
+        date
+        summary
+        status
+        statusMessage
+        schedule {
+          id
+        }
+        details {
+          amount
+          account {
+            name
+          }
+        }
+      }
+      ... on SavedPayment {
+        name
+        expirationYear
+        expirationMonth
+      }
+    }
+  }
+`;
+const withActivityData = graphql(ACTIVITY_QUERY, {
+  name: "feed",
+  options: {
+    variables: { filters: ["GIVING_DASHBOARD"] },
+  },
+});
+
+type IGivingActivity = {
+  feed: Object,
+};
+
+export class GivingActivity extends Component {
+  props: IGivingActivity;
+
+  filterActivity = (data: [Object]): [any] => {
+    if (!Array.isArray(data)) return [];
+
+    const transactions = [];
+    // const accounts = [];
+    const activityToShow = [];
+
+    // separate feed by transactions and accoutns
+    data.forEach((feedItem: Object) => {
+      if (typeof feedItem.status !== "undefined") {
+        transactions.push(feedItem);
+      }
+      // else {
+      //   accounts.push(feedItem);
+      // }
+    });
+
+    // XXX TODO: filter accounts by soon expiring
+
+    // accounts.map((account) => {
+    //   if (activityToShow.length < 3) {
+    //     activityToShow.push(account);
+    //   }
+    //   return null;
+    // });
+
+    transactions.map((transaction) => {
+      if (activityToShow.length < 3) {
+        activityToShow.push(transaction);
+      }
+      return null;
+    });
+
+    return activityToShow;
+  };
+
+  // used by renderActivity to render a transasction card
+  renderTransaction = (transaction: Object): any => {
+    let message;
+    let status;
+    let linkText;
+    let linkUrl;
+    const scheduled = transaction.schedule !== null;
+
+    if (transaction.status === null || transaction.status === "Success") {
+      status = "success";
+      linkText = "View Gift";
+      linkUrl = `/give/history/${transaction.id}`;
+      message =
+        `Your ${scheduled ? "scheduled " : ""}gift of $${transaction.details[0].amount} to
+        ${transaction.details[0].account.name} was successful`;
+    } else if (transaction.status === "Failed") {
+      status = "failed";
+      message =
+        `Your ${scheduled ? "scheduled " : ""}contribution to ${transaction.details[0].account.name} failed.
+        ${transaction.statusMessage !== null && transaction.statusMessage !== ""
+          ? `Unfortunately, ${transaction.statusMessage}.` : ""}`;
+    } else if (transaction.status === "Pending") {
+      status = "success";
+      message =
+        `Your ${scheduled ? "scheduled " : ""}contribution to ${transaction.details[0].account.name}
+        is pending.`;
+    } else {
+      return null;
+    }
+
+    return (
+      <ActivityCard
+        key={transaction.id}
+        status={status}
+        date={transaction.date}
+        message={message}
+        linkText={linkText}
+        linkUrl={linkUrl}
+      />
+    );
+  };
+
+  renderExpiringAccount = (account: Object): any =>
+    <ActivityCard
+      key={account.id}
+      status=""
+      message={`Your saved payment ${account.name} is expiring soon.`}
+      linkText={"Update it Now"}
+      linkUrl={"/"}
+      date={"Tue Nov 01 2016 07:53:00 GMT-0400 (EDT)"}
+    />
+  ;
+
+  renderActivity = (feedItems: ?[Object]): ?[any] => {
+    if (Array.isArray(feedItems) && feedItems.length > 0) {
+      return feedItems.map((feedItem: Object) => {
+        if (feedItem.__typename === "Transaction") { //eslint-disable-line
+          return this.renderTransaction(feedItem);
+        }
+        return this.renderExpiringAccount(feedItem);
+      });
+    }
+    return null;
+  };
+
+  render() {
+    if (!this.props.feed || !this.props.feed.userFeed) return null;
+
+    const data = this.filterActivity(this.props.feed.userFeed);
+    if (!Array.isArray(data) && data.length === 0) return null;
+
+    return (
+      <div className="soft-half hard-top">
+        {this.renderActivity(data)}
+      </div>
+    );
+  }
+}
+
+export default withActivityData(GivingActivity);
