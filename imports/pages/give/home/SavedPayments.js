73563784cd287840ab19ca69cf71525a57d6f95a
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-11 11:30:40 -0500

    Added saved payments to dashboard (#1560)
    * added giving schedules to dashboard
    
    * updated snapshots
    
    * moved date logic to card
    
    * Extracted new schedule button to own component
    
    * added links, typing for withRouter, and null return
    
    * added component
    
    * added classes prop to savedPayments card
    
    * added wrapper for cards to add spacing
    
    * corrected card spacing
    
    * finished saved payments on dashboard

---
+++ imports/pages/give/home/SavedPayments.js
@@ -0,0 +1,76 @@
+
+// @flow
+
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { withRouter } from "react-router";
+import SavedPaymentCard from "../../../components/cards/cards.SavedPayment";
+import SectionHeader from "../../../components/sectionHeader";
+import SmallButton from "../../../components/buttons/small";
+
+const SavedPaymentsButton = () =>
+  <SmallButton
+    text="Add Account"
+    linkUrl="/give/history"
+    className="btn--dark-tertiary flush"
+  />;
+
+const PAYMENTS_QUERY = gql`
+  query savedPayments {
+    savedPayments{
+      id
+      name
+      payment {
+        accountNumber
+        paymentType
+      }
+    }
+  }
+`;
+const withSavedPaymentsData = graphql(PAYMENTS_QUERY, {
+  name: "payments",
+});
+
+type ISavedPaymentsList = {
+  payments: Object,
+  router: Object,
+};
+
+export class SavedPaymentsList extends Component {
+  props: ISavedPaymentsList;
+
+  renderPayments(payments: Object) {
+    if (!Array.isArray(payments)) return null;
+    const numberOfAccounts = payments.length;
+    return payments.map((payment) =>
+      <SavedPaymentCard
+        classes={`
+          grid__item soft-half-sides hard-right
+          ${numberOfAccounts > 1 ? " one-half@lap-wide-and-up" : " one-whole"} `}
+        key={`${payment.id}_${payment.name}`}
+        payment={payment}
+        onClick={() => {
+          this.props.router.push(`/give/saved-payments/edit/${payment.id}`);
+        }}
+      />
+    );
+  }
+
+  render() {
+    if (!this.props.payments || !this.props.payments.savedPayments) return null;
+    return (
+      <div>
+        <SectionHeader
+          title="Saved Accounts"
+          link={<SavedPaymentsButton />}
+        />
+        <div className="grid flush soft-half-right">
+          {this.renderPayments(this.props.payments.savedPayments)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(withSavedPaymentsData(SavedPaymentsList));
