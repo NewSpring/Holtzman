b65f038f6b309627cb1365379166d54f55c6671a
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-11 11:30:40 -0500

    Added saved payments to dashboard (#1560)
    * added giving schedules to dashboard
    
    * updated snapshots
    
    * moved date logic to card
    
    * Extracted new schedule button to own component
    
    * added links, typing for withRouter, and null return
    
    * added component
    
    * added classes prop to savedPayments card
    
    * added wrapper for cards to add spacing
    
    * corrected card spacing
    
    * finished saved payments on dashboard

--- imports/components/cards/cards.SavedPayment.js
+++ imports/components/cards/cards.SavedPayment.js
@@ -6,6 +6,7 @@ import AccountType from "../../components/accountType";
 type ISavedPaymentCard = {
   payment: Object,
   onClick: Function,
+  classes: string,
 };
 
 // XXX right now this uses the content prop for everything
@@ -14,25 +15,28 @@ type ISavedPaymentCard = {
 const SavedPaymentCard = ({
   payment,
   onClick,
+  classes,
 }: ISavedPaymentCard) => (
-  <div className="card" onClick={onClick}>
-    <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
-      <div className="floating grid">
-        <div className="floating__item grid__item two-thirds text-left">
-          <h5 className="text-dark-primary capitalize push-half-bottom">{payment.name}</h5>
-          <div className="floating text-left">
-            <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.payment.accountNumber.slice(-4)}</h6>
-            <div className="floating__item">
-              <AccountType
-                width="40px"
-                height="25px"
-                type={payment.payment.paymentType}
-              />
+  <div className={`${classes || ""}`} onClick={onClick}>
+    <div className="card">
+      <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
+        <div className="floating grid">
+          <div className="floating__item grid__item two-thirds text-left">
+            <h5 className="text-dark-primary capitalize push-half-bottom">{payment.name}</h5>
+            <div className="floating text-left">
+              <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.payment.accountNumber.slice(-4)}</h6>
+              <div className="floating__item">
+                <AccountType
+                  width="40px"
+                  height="25px"
+                  type={payment.payment.paymentType}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="floating__item grid__item one-third text-right">
-          <i className="icon-arrow-next" />
+          <div className="floating__item grid__item one-third text-right">
+            <i className="icon-arrow-next" />
+          </div>
         </div>
       </div>
     </div>
