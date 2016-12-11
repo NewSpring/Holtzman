38dce87e58434c28ac31c4c1b33c9cb742ae773b
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-11 10:10:46 -0500

    added wrapper for cards to add spacing

--- imports/components/cards/cards.SavedPayment.js
+++ imports/components/cards/cards.SavedPayment.js
@@ -17,24 +17,26 @@ const SavedPaymentCard = ({
   onClick,
   classes,
 }: ISavedPaymentCard) => (
-  <div className={`card ${classes || ""}`} onClick={onClick}>
-    <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
-      <div className="floating grid">
-        <div className="floating__item grid__item two-thirds text-left">
-          <h5 className="text-dark-primary capitalize push-half-bottom">{payment.name}</h5>
-          <div className="floating text-left">
-            <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.payment.accountNumber.slice(-4)}</h6>
-            <div className="floating__item">
-              <AccountType
-                width="40px"
-                height="25px"
-                type={payment.payment.paymentType}
-              />
+  <div className={`${classes || ""}`} onClick={onClick}>
+    <div className="card">
+      <div className="card__item soft push-half-ends one-whole" style={{ verticalAlign: "middle" }}>
+        <div className="floating grid">
+          <div className="floating__item grid__item two-thirds text-left">
+            <h5 className="text-dark-primary capitalize push-half-bottom">{payment.name}</h5>
+            <div className="floating text-left">
+              <h6 className="text-dark-tertiary flush soft-half-right floating__item">{payment.payment.accountNumber.slice(-4)}</h6>
+              <div className="floating__item">
+                <AccountType
+                  width="40px"
+                  height="25px"
+                  type={payment.payment.paymentType}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="floating__item grid__item one-third text-right">
-          <i className="icon-arrow-next" />
+          <div className="floating__item grid__item one-third text-right">
+            <i className="icon-arrow-next" />
+          </div>
         </div>
       </div>
     </div>
