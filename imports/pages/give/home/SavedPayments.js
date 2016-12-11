ec35706c9feb004191bf8cb6660318ecbec58abb
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-11 10:10:57 -0500

    corrected card spacing

--- imports/pages/give/home/SavedPayments.js
+++ imports/pages/give/home/SavedPayments.js
@@ -4,6 +4,7 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { withRouter } from "react-router";
 import SavedPaymentCard from "../../../components/cards/cards.SavedPayment";
 import SectionHeader from "../../../components/sectionHeader";
 import SmallButton from "../../../components/buttons/small";
@@ -15,41 +16,61 @@ const SavedPaymentsButton = () =>
     className="btn--dark-tertiary flush"
   />;
 
-const SCHEDULE_QUERY = gql`
-  query scheduledTransactions {
-    scheduledTransactions {
+const PAYMENTS_QUERY = gql`
+  query savedPayments {
+    savedPayments{
       id
-      start
-      details {
-        account {
-          name
-        }
-        amount
-      }
-      transactions {
-        date
-      }
-      schedule {
-        description
+      name
+      payment {
+        accountNumber
+        paymentType
       }
     }
   }
 `;
-const withSchedules = graphql(SCHEDULE_QUERY, {
-  name: "schedules",
+const withSavedPaymentsData = graphql(PAYMENTS_QUERY, {
+  name: "payments",
 });
 
 type ISavedPaymentsList = {
-  schedules: Object,
+  payments: Object,
+  router: Object,
 };
 
 export class SavedPaymentsList extends Component {
   props: ISavedPaymentsList;
 
+  renderPayments(payments: Object){
+    if (!Array.isArray(payments)) return null;
+    return payments.map((payment) =>
+      <SavedPaymentCard
+        classes="grid__item one-half@lap-wide-and-up soft-half-sides hard-right"
+        key={payment.id}
+        payment={payment}
+        onClick={() => {
+          this.props.router.push(`/give/saved-payments/edit/${payment.id}`);
+        }}
+      />
+    );
+  }
+
   render() {
-    if (!this.props.schedules || !this.props.schedules.scheduledTransactions) return <div />;
-    return (<div />);
+    if (!this.props.payments || !this.props.payments.savedPayments) return null;
+    return (
+      <div>
+        <SectionHeader
+          title="Saved Accounts"
+          link={<SavedPaymentsButton />}
+        />
+        <div className="grid flush soft-half-right">
+          {this.renderPayments(this.props.payments.savedPayments)}
+          {this.renderPayments(this.props.payments.savedPayments)}
+          {this.renderPayments(this.props.payments.savedPayments)}
+
+        </div>
+      </div>
+    );
   }
 }
 
-export default withSchedules(SavedPaymentsList);
+export default withRouter(withSavedPaymentsData(SavedPaymentsList));
