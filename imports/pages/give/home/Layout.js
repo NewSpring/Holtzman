42ca688f18ed83aa2107aeedd720ebb33b32615a
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-11 11:26:38 -0500

    Moved SectionHeader for activity

--- imports/pages/give/home/Layout.js
+++ imports/pages/give/home/Layout.js
@@ -1,30 +1,14 @@
 // @flow
 
-import SectionHeader from "../../../components/sectionHeader";
-import SmallButton from "../../../components/buttons/small";
-
 import Activity from "./Activity";
 import Schedules from "./Schedules";
 import SavedPayments from "./SavedPayments";
 
 const Layout = () => (
   <div>
-    <SectionHeader
-      title="Activity"
-      link={
-        <SmallButton
-          text="See All"
-          linkUrl="/give/history"
-          className="btn--dark-tertiary flush"
-        />
-      }
-    />
     <Activity />
-
     <Schedules />
-
     <SavedPayments />
-
   </div>
 );
 
