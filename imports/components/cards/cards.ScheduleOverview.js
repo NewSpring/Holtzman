ff35e99adccb33f6bc3fb292260bf45168748af4
Edolyne Long <edolyne@users.noreply.github.com>
2016-12-13 23:30:21 -0500

    Updates to currency component to add rounding, classes and font colors (#1573)
    * Updates to currency component to add rounding, classes and font colors
    
    * Updating component, and modifying story
    
    * Updating story and tests
    
    * Updating components to reflect breaking changes
    
    * Updating snapshots after breaking changes fixed
    
    * Requested changes to currency component
    
    * Updating snapshots
    
    * Updated failing snapshot
    
    * fix failing snapshots
    
    * update merge commit snapshot
    
    * update failing tests and fix missing schedule code

--- imports/components/cards/cards.ScheduleOverview.js
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -1,12 +1,5 @@
 // @flow
-
-import moment from "moment";
-import {
-  getCurrencySymbol,
-  getNegative,
-  getDollars,
-  getCents,
-} from "./../currency";
+import Currency from "./../currency";
 
 type IScheduleOverviewCard = {
   amount: string,
@@ -15,7 +8,6 @@ type IScheduleOverviewCard = {
   started: string,
   latest: string,
   onEditClick: Function,
-  classes: string,
 };
 
 const ScheduleOverviewCard = ({
@@ -25,25 +17,38 @@ const ScheduleOverviewCard = ({
   started,
   latest,
   onEditClick,
-  classes,
 }: IScheduleOverviewCard) => (
-  <div className={`${classes || ""}`}>
-    <div className="card">
-      <div className="card__item soft push-half-ends one-whole">
-        <button className="float-right h6 text-dark-tertiary outlined--bottom" style={{ borderColor: "inherit", borderWidth: "2px" }} onClick={onEditClick}>Edit</button>
-        <div className="floating text-left text-dark-primary">
-          <h4 className="floating__item flush" style={{ paddingRight: "5px" }}>{getCurrencySymbol(amount)}</h4>
-          {getNegative(amount) && <h4 className="floating__item flush" style={{ paddingRight: "3px" }}>{getNegative(amount)}</h4>}
-          <h2 className="floating__item flush">{getDollars(amount)}</h2>
-          <h4 className="floating__item flush">.{getCents(amount)}</h4>
-        </div>
-        <div className="floating text-left push-bottom">
-          <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
-          <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
-        </div>
-        <p className="flush" style={latest ? { marginBottom: "5px" } : {}}><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Started: </span><em className="text-dark-primary">{ moment(started).format("MMM D, YYYY") }</em></p>
-        {latest && <p className="flush"><span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>Latest: </span><em className="text-dark-primary">{ moment(latest).format("MMM D, YYYY") }</em></p>}
+  <div className="card">
+    <div className="card__item soft push-half-ends one-whole">
+      <button
+        className="float-right h6 text-dark-tertiary outlined--bottom"
+        style={{ borderColor: "inherit", borderWidth: "2px" }}
+        onClick={onEditClick}
+      >
+        Edit
+      </button>
+      <Currency
+        amount={amount}
+        className="text-left"
+      />
+      <div className="floating text-left push-bottom">
+        <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
+        <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
       </div>
+      <p className="flush" style={latest ? { marginBottom: "5px" } : {}}>
+        <span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>
+          Started:
+        </span>
+        <em className="text-dark-primary">{started}</em>
+      </p>
+      {latest && (
+        <p className="flush">
+          <span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>
+            Latest:
+          </span>
+          <em className="text-dark-primary">{latest}</em>
+        </p>
+      )}
     </div>
   </div>
 );
