283e011657c4ecbaacb08555dc6794378fbb3025
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-14 09:13:55 -0500

    wip

---
+++ imports/pages/give/home/components/AddSavedPayment.js
@@ -0,0 +1,16 @@
+
+export const StepOne = () =>
+  <div>Step 1</div>
+  ;
+
+export const StepTwo = () =>
+  <div>Step 2</div>
+  ;
+
+export const StepThree = () =>
+  <div>Step 3</div>
+  ;
+
+export const StepFour = () =>
+  <div>Step 4</div>
+  ;
