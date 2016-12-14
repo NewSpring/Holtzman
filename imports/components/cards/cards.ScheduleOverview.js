9bcbe2e946ed534e553f8dbc079c7c1453568022
edolyne <ed@edolyne.com>
2016-12-14 15:45:02 -0500

    Fund removed from card, added schedule detail button

--- imports/components/cards/cards.ScheduleOverview.js
+++ imports/components/cards/cards.ScheduleOverview.js
@@ -5,22 +5,22 @@ import Currency from "./../currency";
 
 type IScheduleOverviewCard = {
   amount: string,
-  fund: string,
   frequency: string,
   started: string,
   classes: string,
   latest: string,
   onEditClick: Function,
+  onDetailClick: Function,
 };
 
 const ScheduleOverviewCard = ({
   amount,
-  fund,
   frequency,
   started,
   latest,
   classes,
   onEditClick,
+  onDetailClick,
 }: IScheduleOverviewCard) => (
   <div className={classes}>
     <div className="card">
@@ -36,10 +36,9 @@ const ScheduleOverviewCard = ({
           amount={amount}
           className="text-left"
         />
-        <div className="floating text-left push-bottom">
-          <h5 className="floating__item soft-half-right flush text-dark-primary">{fund}</h5>
-          <h6 className="floating__item flush text-dark-tertiary">{frequency}</h6>
-        </div>
+        <h5 className="floating__item soft-half-right push-bottom text-dark-primary">
+          {frequency}
+        </h5>
         <p className="flush" style={latest ? { marginBottom: "5px" } : {}}>
           <span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>
             Started:
@@ -47,13 +46,27 @@ const ScheduleOverviewCard = ({
           <em className="text-dark-primary">{moment(started).format("MMM D, YYYY")}</em>
         </p>
         {latest && (
-          <p className="flush">
+          <p>
             <span className="h7 text-dark-tertiary push-half-right" style={{ verticalAlign: "middle" }}>
               Latest:
             </span>
             <em className="text-dark-primary">{moment(latest).format("MMM D, YYYY")}</em>
           </p>
         )}
+        <button
+          className="text-primary"
+          style={{ borderColor: "inherit", borderWidth: "2px" }}
+          onClick={onDetailClick}
+        >
+          <h6 className="display-inline-block soft-half-right">View Schedule Details</h6>
+          <span
+            className="text-primary display-inline-block icon-arrow-next"
+            style={{
+              right: "-5px",
+              top: "1px",
+            }}
+          />
+        </button>
       </div>
     </div>
   </div>
