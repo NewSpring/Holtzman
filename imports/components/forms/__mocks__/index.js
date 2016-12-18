68cc0b88197426db42802e1a061ed55b9d1e1aa6
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-18 00:14:41 -0500

    added form mocks

---
+++ imports/components/forms/__mocks__/index.js
@@ -0,0 +1,20 @@
+
+const Checkbox = "Checkbox";
+const Input = "Input";
+const Select = "Select";
+const Form = "Form";
+const Fieldset = "Fieldset";
+const Label = "Label";
+const Date = "Date";
+const TextArea = "TextArea";
+
+export {
+  Checkbox,
+  Input,
+  Select,
+  Form,
+  Fieldset,
+  Label,
+  Date,
+  TextArea,
+};
