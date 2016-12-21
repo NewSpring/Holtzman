05e58eb595c26ad44561a9c7615e5a452a15f9ca
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-20 20:18:09 -0500

    Fix SVG scaling on Safari (#1646)
    * added didMount funcction to remove height from chart svg
    
    * fixed lint errors

--- imports/components/graphs/lineGraph/index.js
+++ imports/components/graphs/lineGraph/index.js
@@ -6,6 +6,7 @@ import {
   VictoryScatter,
   VictoryArea,
 } from "victory";
+import React, { Component } from "react";
 
 type ILineGraph = {
   axisStyles: Object,
@@ -46,62 +47,77 @@ const GradientGroup = ({ style, events, transform, children, gradientColor }: IG
   </g>
 );
 
-const LineGraph = ({
-  axisStyles,
-  data,
-  dotColor,
-  dotSize,
-  lineColor,
-  lineWidth,
-}: ILineGraph) => (
-  <div className="">
-    <VictoryChart
-      height={160}
-      padding={{ top: 5, left: 10, right: 10, bottom: 20 }}
-      animate={{ duration: 2000 }}
-    >
-      <VictoryAxis
-        style={{
-          axis: {
-            stroke: `${axisStyles.axis.lineColor}`,
-            strokeWidth: `${axisStyles.axis.lineWidth}`,
-          },
-          tickLabels: {
-            fontFamily: "colfax-web, sans-serif",
-            fontSize: `${axisStyles.tickLabels.fontSize}`,
-            fill: `${axisStyles.tickLabels.fill}`,
-          },
-        }}
-        tickFormat={getTickFormat(data)}
-      />
-      <VictoryArea
-        data={data}
-        x="month"
-        y="amount"
-        groupComponent={<GradientGroup gradientColor={lineColor} />}
-        style={{
-          data: { fill: "url(#gradient)", stroke: "none", opacity: 0.5 },
-        }}
-      />
-      <VictoryLine
-        data={data}
-        x="month"
-        y="amount"
-        style={{
-          data: { stroke: `${lineColor}`, strokeWidth: `${lineWidth}` },
-        }}
-      />
-      <VictoryScatter
-        data={data}
-        x="month"
-        y="amount"
-        size={dotSize}
-        style={{
-          data: { fill: dotColor },
-        }}
-      />
-    </VictoryChart>
-  </div>
-);
+class LineGraph extends Component {
+  props: ILineGraph;
+
+  componentDidMount() {
+    const renderedChart = this.graphContainer.querySelectorAll("[aria-labelledby='title desc']");
+    if (renderedChart && renderedChart.length) renderedChart[0].removeAttribute("height");
+  }
+
+  render() {
+    const {
+      axisStyles,
+      data,
+      dotColor,
+      dotSize,
+      lineColor,
+      lineWidth,
+    } = this.props;
+    return (
+      <div
+        className=""
+        ref={(el) => (this.graphContainer = el)}
+      >
+        <VictoryChart
+          height={160}
+          padding={{ top: 5, left: 10, right: 10, bottom: 20 }}
+          animate={{ duration: 2000 }}
+        >
+          <VictoryAxis
+            style={{
+              axis: {
+                stroke: `${axisStyles.axis.lineColor}`,
+                strokeWidth: `${axisStyles.axis.lineWidth}`,
+              },
+              tickLabels: {
+                fontFamily: "colfax-web, sans-serif",
+                fontSize: `${axisStyles.tickLabels.fontSize}`,
+                fill: `${axisStyles.tickLabels.fill}`,
+              },
+            }}
+            tickFormat={getTickFormat(data)}
+          />
+          <VictoryArea
+            data={data}
+            x="month"
+            y="amount"
+            groupComponent={<GradientGroup gradientColor={lineColor} />}
+            style={{
+              data: { fill: "url(#gradient)", stroke: "none", opacity: 0.5 },
+            }}
+          />
+          <VictoryLine
+            data={data}
+            x="month"
+            y="amount"
+            style={{
+              data: { stroke: `${lineColor}`, strokeWidth: `${lineWidth}` },
+            }}
+          />
+          <VictoryScatter
+            data={data}
+            x="month"
+            y="amount"
+            size={dotSize}
+            style={{
+              data: { fill: dotColor },
+            }}
+          />
+        </VictoryChart>
+      </div>
+    );
+  }
+}
 
 export default LineGraph;
