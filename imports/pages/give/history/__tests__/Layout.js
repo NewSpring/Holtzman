069dc0aad7917288a18e112207bb6fc8fdd3afd9
Richard Dubay <richardtdubayjr@gmail.com>
2016-12-21 13:26:41 -0500

    History tests (#1670)
    * testing history layout
    
    * added tests for /history/layout
    
    * added tests to /history/details/layout
    
    * fixed obsolete snapshot

--- imports/pages/give/history/__tests__/Layout.js
+++ imports/pages/give/history/__tests__/Layout.js
@@ -1,7 +1,7 @@
-import { shallow } from "enzyme";
-import { shallowToJson } from "enzyme-to-json";
+import { mount, shallow } from "enzyme";
+import { mountToJson, shallowToJson } from "enzyme-to-json";
 
-import Layout from "../Layout";
+import Layout, { TransactionList } from "../Layout";
 
 describe("Layout", () => {
   const defaultProps = {
@@ -28,6 +28,9 @@ describe("Layout", () => {
     changeDates: jest.fn(),
     reloading: false,
     family: [],
+    filterTransactions: jest.fn(),
+    onPrintClick: jest.fn(),
+    printLoading: false,
   };
 
   const generateComponent = (additionalProps = {}) => {
@@ -78,11 +81,134 @@ describe("Layout", () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 
+  it("renders with print loading false and there are transactions", () => {
+    const transactions = [
+      {
+        date: "2012-12-12",
+        details: [
+          { account: "test", amount: -1 },
+        ],
+      },
+      {
+        date: "2012-12-12",
+        details: [
+          { account: "Other Test", amount: 2 },
+          { account: "Third Test", amount: 2 },
+        ],
+      },
+    ];
+    const wrapper = shallow(generateComponent({ transactions }));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("renders with print loading true and there are transactions", () => {
+    const transactions = [
+      {
+        date: "2012-12-12",
+        details: [
+          { account: "test", amount: -1 },
+        ],
+      },
+      {
+        date: "2012-12-12",
+        details: [
+          { account: "Other Test", amount: 2 },
+          { account: "Third Test", amount: 2 },
+        ],
+      },
+    ];
+    const wrapper = shallow(generateComponent({
+      transactions,
+      printLoading: true
+    }));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
   it("renders no transactions if there are none and ready", () => {
     const wrapper = shallow(generateComponent({
       transactions: [],
       ready: true,
     }));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("renders transactions across years", () => {
+    const transactions = [
+      {
+        date: "2012-12-12",
+        person: {nickname: "1", photo:"1",firstName:"1",lastName:"1"},
+        details: [
+          { account: "test", amount: 2 },
+          { account: "test", amount: 2 },
+        ],
+      },
+      {
+        date: "2013-12-12",
+        person: {nickname: "1", photo:"1",firstName:"1",lastName:"1"},
+        details: [
+          { account: "Other Test", amount: 2 },
+          { account: "Third Test", amount: 2 },
+        ],
+      },
+    ];
+    const wrapper = shallow(generateComponent({ transactions }));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
+
+describe("TransactionList", () => {
+  const defaultProps = {
+    transactions: [
+      {
+        date: "2012-12-11",
+        details: [
+          { account: "test", amount: 1 },
+          { account: "test", amount: 2 },
+        ],
+      },
+      {
+        date: "2012-12-12",
+        details: [
+          { account: "test", amount: 3 },
+          { account: "test", amount: 4 },
+        ],
+      },
+    ],
+  };
+
+  const generateComponent = (additionalProps = {}) => {
+    const newProps = {
+      ...defaultProps,
+      ...additionalProps,
+    };
+    return <TransactionList { ...newProps } />;
+  };
+
+  it("renders with props", () => {
+    const wrapper = shallow(generateComponent());
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("renders transactions across years", () => {
+    const transactions = [
+      {
+        date: "2012-12-12",
+        person: {nickname: "1", photo:"1",firstName:"1",lastName:"1"},
+        details: [
+          { account: "test", amount: 1 },
+          { account: "test", amount: 2 },
+        ],
+      },
+      {
+        date: "2013-12-12",
+        person: {nickname: "1", photo:"1",firstName:"1",lastName:"1"},
+        details: [
+          { account: "test", amount: 3 },
+          { account: "test", amount: 4 },
+        ],
+      },
+    ];
+    const wrapper = shallow(generateComponent({ transactions }));
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 });
