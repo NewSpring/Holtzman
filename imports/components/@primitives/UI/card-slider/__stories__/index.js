46bb2fd1f3f57d1b578439bafb56106dab6fea2a
richarddubay <richardtdubayjr@gmail.com>
2017-01-08 12:42:27 -0500

    removed unnecessary comment

--- imports/components/@primitives/UI/card-slider/__stories__/index.js
+++ imports/components/@primitives/UI/card-slider/__stories__/index.js
@@ -35,7 +35,6 @@ story
     salvationList[1].count = text("Card 2 Count", salvationList[1].count);
     salvationList[1].label = text("Card 2 Label", salvationList[1].label);
 
-    // <h1>{label}</h1>
     return (
       <div className={"floating"}>
         <div className={"grid__item"} style={{ maxWidth: "480px" }}>
