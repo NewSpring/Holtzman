fbefe3344ef5b02007b413c39416a99e6323128c
Jake Dawkins <dawkinsjh@gmail.com>
2017-01-09 07:45:37 -0500

    reverted tag case change

--- .dev/deploy/app.sh
+++ .dev/deploy/app.sh
@@ -19,7 +19,7 @@ yecho () {
 # force script to error out at first error
 set -e
 
-CURRENT_TAG=$(git describe --exact-match --abbrev=0 --tags) | tr '[:upper:]' '[:lower:]'
+CURRENT_TAG=$(git describe --exact-match --abbrev=0 --tags)
 
 PREVIOUS_TAG=$(git describe HEAD^1 --abbrev=0 --tags)
 GIT_HISTORY=$(git log --no-merges --format="- %s" "$PREVIOUS_TAG"..HEAD)
