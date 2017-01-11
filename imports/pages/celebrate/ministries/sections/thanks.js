7493327decb045deebfd8156da79f109915bc7d1
edolyne <ed@edolyne.com>
2017-01-11 10:59:22 -0500

    Spacing for section on desktop

--- imports/pages/celebrate/ministries/sections/thanks.js
+++ imports/pages/celebrate/ministries/sections/thanks.js
@@ -1,16 +1,20 @@
 
 /* eslint-disable max-len */
 const Thanks = () => (
-  <div className="background--primary soft-sides@portable soft-double-sides@lap-and-up soft-double-ends@lap-and-up text-light-primary">
-    <div className="constrain-copy text-center">
-      <h2>Thanks For Reading!</h2>
-      <p>Thanks so much for taking the time to read our Annual Report.
-      We hope you&#39;re excited as us about what God is doing in South Carolina, and around the world.</p>
-      <button
-        className="h6 btn--small btn--light soft-sides@portable one-whole@handheld"
-      >
-        Get Involved Now
-      </button>
+  <div className="background--primary text-light-primary">
+    <div className="grid flush">
+      <div className="grid__item soft soft-double@lap-and-up push-ends">
+        <div className="constrain-copy text-center soft-ends">
+          <h2>Thanks For Reading!</h2>
+          <p>Thanks so much for taking the time to read our Annual Report.
+          We hope you&#39;re excited as us about what God is doing in South Carolina, and around the world.</p>
+          <a
+            className="h6 btn--small flush-bottom btn--light soft-sides@portable one-whole@handheld"
+          >
+            Get Involved Now
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 );
