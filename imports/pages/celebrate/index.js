1a82a6a85b31d5f3a5b41e55e08581da07cb8548
richarddubay <richardtdubayjr@gmail.com>
2017-01-11 00:12:01 -0500

    added the roughed-in dashboard and fixed some background colors

--- imports/pages/celebrate/index.js
+++ imports/pages/celebrate/index.js
@@ -1,22 +1,53 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import Meta from "../../components/shared/meta";
 import Finances from "./finances";
 import NextSteps from "./next-steps";
+import DashboardLayout from "../../components/@primitives/layout/dashboard";
 
 type ITemplate = {
   children: React$Element<any>,
 }
 
-const Template = ({
-  children,
-}:ITemplate) => (
-  <div>
-    <Meta title="Annual Report" />
-    <h1>HELLO FROM THE ANNUAL REPORT</h1>
-    {children}
-  </div>
-);
+class Template extends Component {
+  props: ITemplate;
+  state = {
+    subNav: [
+      {
+        isActive: false,
+        linkUrl: "/celebrate/finances",
+        onClick: () => {},
+        title: "Finances",
+      },
+      {
+        isActive: false,
+        linkUrl: "/celebrate/next-steps",
+        onClick: () => {},
+        title: "Next Steps",
+      },
+      {
+        isActive: false,
+        linkUrl: "/celebrate/ministries",
+        onClick: () => {},
+        title: "Ministries",
+      },
+    ],
+  }
+
+  render() {
+    return (
+      <div>
+        <Meta title="Annual Report" />
+        <DashboardLayout
+          title="2016 Annual Report"
+          subNav={this.state.subNav}
+        >
+          {this.props.children}
+        </DashboardLayout>
+      </div>
+    );
+  }
+}
 
 const Routes = [
   {
