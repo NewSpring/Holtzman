5f9fd32b2c834a3332ac1d80db2484253c677098
Jake Dawkins <dawkinsjh@gmail.com>
2017-01-25 15:07:55 -0500

    toggles likes :tada:

--- imports/components/@enhancers/can-like/index.js
+++ imports/components/@enhancers/can-like/index.js
@@ -22,71 +22,59 @@ const withToggleLike = graphql(TOGGLE_LIKE_MUTATION);
 /*
   THIS NEEDS TO BE WRAPPED IN A CONNECT CALL TO WORK
 */
-
-export class LikesWrapper extends Component {
-  constructor(props){
-    super(props);
-    this.state = { liked: false, id: null }
-    this.toggleLike = this.toggleLike.bind(this);
-  }
-
-  componentWillMount(){
-    // TODO get whether already liked and set the button state
-  }
-
-  componentWillReceiveProps(nextProps){
-    // gets the node id from gql data passed in. sets the local state `id`
-    this.getNodeId(nextProps);
-    console.log("PROPS", nextProps);
-  }
-
-  getNodeId(props) {
-    if(props){
-      const keys = Object.keys(props);
-      const contentKey = keys.find((key) => {
-        return props[key] && props[key].content
-      });
-
-      if(!contentKey) return null;
-      const id = props[contentKey].content.id || props[contentKey].content.entryId;
-      if(this.state.id != id){
-        this.setState({ id: id });
-      }
-      return id;
-    }
-  }
-
-  toggleLike() {
-    console.log("TOGGLE LIKE CLICKED");
-    //if not logged in, show login modal
-    if (!Meteor.userId()) {
-      this.props.dispatch(modal.render(OnBoard, {
-        coverHeader: true,
-        modalBackground: "light",
-      }));
-    } else { // if logged in, toggle like state in redux, remote with gql query
-      this.props.dispatch(likedActions.toggle({
-        entryId: this.state.id,
-      }));
-      // this.updateDatabase(entry);
-      // this.props.mutate
-    }
-
-    return {
-      type: "FALSY",
-      payload: {},
-    };
-  }
-
-  render() {
-    return <WrappedComponent {...this.props} onLike={this.toggleLike} />
-  }
-}
-
-// wrap the wrapper class in a gql HOC
-export const WithMutation = withToggleLike(LikesWrapper);
-
-//wrap the data-wrapped class with a function
 export default (WrappedComponent) => {
-  return WithMutation;
-};
+  class LikesWrapper extends Component {
+    constructor(props){
+      super(props);
+      this.state = { liked: false, id: null }
+      this.toggleLike = this.toggleLike.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps){
+      // gets the node id from gql data passed in. sets the local state `id`
+      this.getNodeId(nextProps);
+      console.log("PROPS", nextProps);
+    }
+
+    getNodeId(props) {
+      if(props){
+        const keys = Object.keys(props);
+        const contentKey = keys.find((key) => {
+          return props[key] && props[key].content
+        });
+
+        if(!contentKey) return null;
+        const id = props[contentKey].content.id || props[contentKey].content.entryId;
+        if(this.state.id != id){
+          this.setState({ id: id });
+        }
+        return id;
+      }
+    }
+
+    toggleLike() {
+      if (!Meteor.userId()) { //if not logged in, show login modal
+        this.props.dispatch(modal.render(OnBoard, {
+          coverHeader: true,
+          modalBackground: "light",
+        }));
+      } else { // if logged in, toggle like state in redux, remote with gql query
+        this.props.dispatch(likedActions.toggle({
+          entryId: this.state.id,
+        }));
+        this.props.mutate({ variables: { nodeId: this.state.id } })
+          // .then((x) => console.log("toggled:", x));
+      }
+
+      return {
+        type: "FALSY",
+        payload: {},
+      };
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} onLike={this.toggleLike} />
+    }
+  }
+  return withToggleLike(LikesWrapper); // the actual component to be wrapped in a function
+}
