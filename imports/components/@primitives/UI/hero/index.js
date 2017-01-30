a3972e8cab1a84b0b1c561a732786e10612ab13d
edolyne <ed@edolyne.com>
2017-01-30 16:13:20 -0500

    SYS-3303 updaing hero based on mini card changes

--- imports/components/@primitives/UI/hero/index.js
+++ imports/components/@primitives/UI/hero/index.js
@@ -39,7 +39,7 @@ export default class Hero extends Component {
             <h3 className="text-light-primary flush soft-half-bottom capitalize">{heroItem.title}</h3>
             <i className={iconClasses} />
             <h7 className="text-light-primary">{ready ? categories.name(heroItem) : ""}</h7>
-            {!heroItem.hideDate && <h7 className="text-light-primary text-right float-right" style={this.timeStampStyles}>
+            {(heroItem && !heroItem.hideDate) && <h7 className="text-light-primary text-right float-right" style={this.timeStampStyles}>
               {ready ? time.relative(heroItem) : ""}
             </h7>}
           </div>
