f3370a1544e1c7104eb060699c093d7b9f63ccf3
James Baxley <james.baxley@newspring.cc>
2017-01-30 10:39:38 -0500

    Add deep linking (#1759)
    * SYS-3259 added cordova deep linking plugin
    
    * SYS-3259 added cordova config settings
    
    * SYS-3259 fixing the config file
    
    * SYS-3259 updating config to add events for each route
    
    * Removing individual campus location route
    
    * SYS-3259 Adding team id and android launch mode
    
    * SYS-3259 Adding events, and functions for universalLinks
    
    * added android links to the head of the my. site
    
    * fixed the link
    
    * fixed lint errors
    
    * added underscore routes
    
    * SYS-3259 Unifies event for universal links
    
    * SYS-3259 Unifies event for universal links
    
    * SYS-3259 Adds loading state for universal links
    
    * added global and started on universal link subscribe
    
    * SYS-3259 Fixes loading state conditionals
    
    * SYS-3259 Work on routing schemes
    
    * added the underscore link to the main html for android
    
    * updates for making sure the url gets pushed by the router to go to the page specified
    
    * Working routing for main pages, and watch and read.
    
    * fixed issues in comments, still not loading correctly
    
    * updated mobile config and main files for a star route and working on paths
    
    * SYS-3294 added app site association, and updated mobile config
    
    * all paths working
    
    * added default values and switch statements
    
    * one query call
    
    * SYS-3298 Refactoring universal link routing
    
    * SYS-3298 moved routing into a single switch statement
    
    * SYS-3298 changes based on review feedback
    
    * adding tests for deep links (#1772)

--- mobile-config.js
+++ mobile-config.js
@@ -11,46 +11,46 @@ App.info({
 
 App.icons({
   // iOS
-  "iphone_2x": "assets/icons/ios/icon-60x60@2x.png",
-  "iphone_3x": "assets/icons/ios/icon-60x60@3x.png",
-  "ipad": "assets/icons/ios/icon-76x76.png",
-  "ipad_2x": "assets/icons/ios/icon-76x76@2x.png",
-  "ipad_pro": "assets/icons/ios/icon-83.5x83.5@2x.png",
-  "ios_settings": "assets/icons/ios/icon-29x29.png",
-  "ios_settings_2x": "assets/icons/ios/icon-29x29@2x.png",
-  "ios_settings_3x": "assets/icons/ios/icon-29x29@3x.png",
-  "ios_spotlight": "assets/icons/ios/icon-40x40.png",
-  "ios_spotlight_2x": "assets/icons/ios/icon-40x40@2x.png",
+  iphone_2x: "assets/icons/ios/icon-60x60@2x.png",
+  iphone_3x: "assets/icons/ios/icon-60x60@3x.png",
+  ipad: "assets/icons/ios/icon-76x76.png",
+  ipad_2x: "assets/icons/ios/icon-76x76@2x.png",
+  ipad_pro: "assets/icons/ios/icon-83.5x83.5@2x.png",
+  ios_settings: "assets/icons/ios/icon-29x29.png",
+  ios_settings_2x: "assets/icons/ios/icon-29x29@2x.png",
+  ios_settings_3x: "assets/icons/ios/icon-29x29@3x.png",
+  ios_spotlight: "assets/icons/ios/icon-40x40.png",
+  ios_spotlight_2x: "assets/icons/ios/icon-40x40@2x.png",
 
   // Android
-  "android_mdpi": "assets/icons/android/icon-48x48-mdpi.png",
-  "android_hdpi": "assets/icons/android/icon-72x72-hdpi.png",
-  "android_xhdpi": "assets/icons/android/icon-96x96-xhdpi.png",
-  "android_xxhdpi": "assets/icons/android/icon-144x144-xxhdpi.png",
-  "android_xxxhdpi": "assets/icons/android/icon-192x192-xxxhpdi.png",
+  android_mdpi: "assets/icons/android/icon-48x48-mdpi.png",
+  android_hdpi: "assets/icons/android/icon-72x72-hdpi.png",
+  android_xhdpi: "assets/icons/android/icon-96x96-xhdpi.png",
+  android_xxhdpi: "assets/icons/android/icon-144x144-xxhdpi.png",
+  android_xxxhdpi: "assets/icons/android/icon-192x192-xxxhpdi.png",
 });
 
 App.launchScreens({
   // iOS
-  "iphone_2x": "assets/splash/ios/splash-320x480@2x.png",
-  "iphone5": "assets/splash/ios/splash-320x568@2x.png",
-  "iphone6": "assets/splash/ios/splash-667h.png",
-  "iphone6p_portrait": "assets/splash/ios/splash-1242h-portrait.png",
-  "iphone6p_landscape": "assets/splash/ios/splash-1242h-landscape.png",
-  "ipad_portrait": "assets/splash/ios/splash-768x1024.png",
-  "ipad_portrait_2x": "assets/splash/ios/splash-768x1024@2x.png",
-  "ipad_landscape": "assets/splash/ios/splash-1024x768.png",
-  "ipad_landscape_2x": "assets/splash/ios/splash-1024x768@2x.png",
+  iphone_2x: "assets/splash/ios/splash-320x480@2x.png",
+  iphone5: "assets/splash/ios/splash-320x568@2x.png",
+  iphone6: "assets/splash/ios/splash-667h.png",
+  iphone6p_portrait: "assets/splash/ios/splash-1242h-portrait.png",
+  iphone6p_landscape: "assets/splash/ios/splash-1242h-landscape.png",
+  ipad_portrait: "assets/splash/ios/splash-768x1024.png",
+  ipad_portrait_2x: "assets/splash/ios/splash-768x1024@2x.png",
+  ipad_landscape: "assets/splash/ios/splash-1024x768.png",
+  ipad_landscape_2x: "assets/splash/ios/splash-1024x768@2x.png",
 
   // Android
-  "android_mdpi_portrait": "assets/splash/android/splash-320x470.png",
-  "android_mdpi_landscape": "assets/splash/android/splash-470x320.png",
-  "android_hdpi_portrait": "assets/splash/android/splash-480x640.png",
-  "android_hdpi_landscape": "assets/splash/android/splash-640x480.png",
-  "android_xhdpi_portrait": "assets/splash/android/splash-720x960.png",
-  "android_xhdpi_landscape": "assets/splash/android/splash-960x720.png",
-  "android_xxhdpi_portrait": "assets/splash/android/splash-1080x1440.png",
-  "android_xxhdpi_landscape": "assets/splash/android/splash-1440x1080.png",
+  android_mdpi_portrait: "assets/splash/android/splash-320x470.png",
+  android_mdpi_landscape: "assets/splash/android/splash-470x320.png",
+  android_hdpi_portrait: "assets/splash/android/splash-480x640.png",
+  android_hdpi_landscape: "assets/splash/android/splash-640x480.png",
+  android_xhdpi_portrait: "assets/splash/android/splash-720x960.png",
+  android_xhdpi_landscape: "assets/splash/android/splash-960x720.png",
+  android_xxhdpi_portrait: "assets/splash/android/splash-1080x1440.png",
+  android_xxhdpi_landscape: "assets/splash/android/splash-1440x1080.png",
 });
 
 App.accessRule("*");
@@ -69,3 +69,28 @@ App.configurePlugin("cordova-fabric-plugin", {
   FABRIC_API_KEY: process.env.FABRIC_API_KEY,
   FABRIC_API_SECRET: process.env.FABRIC_API_SECRET,
 });
+
+App.appendToConfig(`
+    <universal-links>
+      <ios-team-id value="7TM8C6252Z" />
+      <preference name="AndroidLaunchMode" value="singleInstance" />
+      <host name="beta.newspring.cc" scheme="https" event="universalLinkRoute">
+        <path url="/articles" />
+        <path url="/articles/*" />
+        <path url="/devotionals" />
+        <path url="/devotionals/*" />
+        <path url="/studies" />
+        <path url="/studies/*" />
+        <path url="/studies/*/*" />
+        <path url="/stories" />
+        <path url="/stories/*" />
+        <path url="/sermons" />
+        <path url="/sermons/*" />
+        <path url="/sermons/*/*" />
+        <path url="/music/*" />
+        <path url="/locations" />
+        <path url="/watchandread" />
+      </host>
+      <host name="beta-my.newspring.cc" scheme="https" event="universalLinkRoute" />
+    </universal-links>
+`);
