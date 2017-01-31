1119c614e77422aa6ee21437d85286b34765bf08
edolyne <ed@edolyne.com>
2017-01-31 17:30:20 -0500

    SYS-3323 Data & Layout for NewSpring Now

--- imports/components/@primitives/players/video/Player.js
+++ imports/components/@primitives/players/video/Player.js
@@ -2,16 +2,20 @@ import { PropTypes } from "react";
 
 import Video from "../video";
 
+// DONT USE THIS THING
+
 // XXX Why does this exist?
 // XXX answer: it used to be a lot more complicated
-const SingleVideoPlayer = ({ ooyalaId }) => (
+const SingleVideoPlayer = ({ ooyalaId, autoPlay = true }) => (
   <Video
     id={ooyalaId}
+    autoplay={autoPlay}
   />
 );
 
 SingleVideoPlayer.propTypes = {
   ooyalaId: PropTypes.string.isRequired,
+  autoPlay: PropTypes.boolean,
 };
 
 export default SingleVideoPlayer;
