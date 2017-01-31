19bb19532d0106cad65a550bb1acb2adb860a0c0
edolyne <ed@edolyne.com>
2017-01-30 20:18:03 -0500

    Updating the home hero, back to it's original state

--- imports/pages/home/index.js
+++ imports/pages/home/index.js
@@ -98,7 +98,9 @@ class HomeWithoutData extends Component {
             aspect="square"
             link={heroLink}
           >
+
             <HomeHero item={heroItem || {}} />
+
           </Right>
         </Split>
         <Left scroll>
