47e7ff89d6b50d8a27abd19f97cc77389d7c0c74
richarddubay <richardtdubayjr@gmail.com>
2017-02-12 21:13:32 -0500

    tests for the text area

--- imports/pages/groups/profile/__tests__/Join.js
+++ imports/pages/groups/profile/__tests__/Join.js
@@ -1,62 +1,104 @@
 import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
-import Join from "../Join";
+import Join, { ShowPhoneTextArea } from "../Join";
 
-const defaultProps = {
-  onClick: jest.fn(),
-  group: {
-    name: "happy group",
-    members: [
-      { role: "leader", person: { firstName: "jim" } },
-      { role: "leader", person: { nickName: "bob" } },
-    ],
-  },
-  onExit: jest.fn(),
-};
-
-const generateComponent = (additionalProps = {}) => {
-  const newProps = {
-    ...defaultProps,
-    ...additionalProps,
+describe("Join", () => {
+  const defaultProps = {
+    onClick: jest.fn(),
+    group: {
+      name: "happy group",
+      members: [
+        { role: "leader", person: { firstName: "jim" } },
+        { role: "leader", person: { nickName: "bob" } },
+      ],
+    },
+    onExit: jest.fn(),
+    loading: false,
+    phones: [],
   };
-  return <Join { ...newProps } />;
-};
 
-it("renders with props", () => {
-  const wrapper = shallow(generateComponent());
-  expect(shallowToJson(wrapper)).toMatchSnapshot();
-});
+  const generateComponent = (additionalProps = {}) => {
+    const newProps = {
+      ...defaultProps,
+      ...additionalProps,
+    };
+    return <Join { ...newProps } />;
+  };
 
-it("onClick calls updates state and calls onClick", () => {
-  const mockOnClick = jest.fn();
-  const wrapper = shallow(generateComponent({
-    onClick: mockOnClick,
-  }));
-  wrapper.instance().onClick("e");
-  expect(wrapper.state().state).toBe("loading");
-  expect(mockOnClick).toHaveBeenCalledTimes(1);
-  expect(mockOnClick.mock.calls[0][0]).toBe("e");
+  it("renders with props", () => {
+    const wrapper = shallow(generateComponent());
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
 
-  // callback without error
-  const result = mockOnClick.mock.calls[0][1](null);
-  expect(wrapper.state().state).toBe("success");
-  expect(result).toBe(null);
-});
+  it("onClick calls updates state and calls onClick", () => {
+    const mockOnClick = jest.fn();
+    const wrapper = shallow(generateComponent({
+      onClick: mockOnClick,
+    }));
+    wrapper.instance().onClick("e");
+    expect(wrapper.state().state).toBe("loading");
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+    expect(mockOnClick.mock.calls[0][0]).toBe("e");
 
-it("onClick calls updates state and calls onClick with error", () => {
-  jest.useFakeTimers();
-  const mockOnClick = jest.fn();
-  const wrapper = shallow(generateComponent({
-    onClick: mockOnClick,
-  }));
-  wrapper.instance().onClick("e");
-  expect(wrapper.state().state).toBe("loading");
-  expect(mockOnClick).toHaveBeenCalledTimes(1);
-  expect(mockOnClick.mock.calls[0][0]).toBe("e");
+    // callback without error
+    const result = mockOnClick.mock.calls[0][1](null);
+    expect(wrapper.state().state).toBe("success");
+    expect(result).toBe(null);
+  });
 
-  // callback without error
-  mockOnClick.mock.calls[0][1](true);
-  expect(wrapper.state().state).toBe("error");
-  jest.runAllTimers();
-  expect(wrapper.state().state).toBe("default");
-});
+  it("onClick calls updates state and calls onClick with error", () => {
+    jest.useFakeTimers();
+    const mockOnClick = jest.fn();
+    const wrapper = shallow(generateComponent({
+      onClick: mockOnClick,
+    }));
+    wrapper.instance().onClick("e");
+    expect(wrapper.state().state).toBe("loading");
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+    expect(mockOnClick.mock.calls[0][0]).toBe("e");
+
+    // callback without error
+    mockOnClick.mock.calls[0][1](true);
+    expect(wrapper.state().state).toBe("error");
+    jest.runAllTimers();
+    expect(wrapper.state().state).toBe("default");
+  });
+})
+
+describe("ShowPhoneTextArea", () => {
+  const defaultProps = {
+    loading: false,
+    phones: [],
+    show: true,
+  };
+
+  const generateComponent = (additionalProps = {}) => {
+    const newProps = {
+      ...defaultProps,
+      ...additionalProps,
+    };
+    return <ShowPhoneTextArea { ...newProps } />;
+  };
+
+  it("renders with props", () => {
+    const wrapper = shallow(generateComponent());
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("shouldn't render if there is a phone number", () => {
+    const additionalProps = {
+      phones: ["(555) 555-5555"],
+    }
+    const wrapper = shallow(generateComponent(additionalProps));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("returns null if still loading", () => {
+    const additionalProps = {
+      loading: true,
+      phones: ["(555) 555-5555"],
+    }
+    const wrapper = shallow(generateComponent(additionalProps));
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  })
+})
