397d95dd1a8e99a2eff38e4491ff565be74f9a62
Jake Dawkins <dawkinsjh@gmail.com>
2017-02-13 17:22:11 -0500

    linted

--- imports/components/@primitives/UI/live/index.js
+++ imports/components/@primitives/UI/live/index.js
@@ -137,13 +137,13 @@ const withRedux = connect(map);
 
 // XXX FOR TESTING ONLY
 const withLive = graphql(LIVE_QUERY, {
-  props: ({ data }) => ({
+  props: () => ({
     data: {
       live: {
         live: true,
         embedCode: "12345",
-      }
-    }
+      },
+    },
   }),
   options: { pollInterval: 60000 },
 });
