a05875201dccae89230381b47d23856cbbd8760b
James Baxley <james.baxley@newspring.cc>
2017-03-05 08:58:05 -0500

    fix slow logouts

--- imports/deprecated/database/publications/user.js
+++ imports/deprecated/database/publications/user.js
@@ -3,13 +3,19 @@ import { Meteor } from "meteor/meteor";
 if (Meteor.isServer) {
   // must use function because arrow version doesn't like 'this'
   Meteor.publish("userData", function userPublish() {
-    return Meteor.users.find(this.userId,
-      {
-        fields: {
-          topics: 1,
-          profile: 1,
-        },
-      }
-    );
+    // only publish if logged in
+    if (this.userId) {
+      return Meteor.users.find(this.userId,
+        {
+          fields: {
+            topics: 1,
+            profile: 1,
+          },
+        }
+      );
+    }
+    // otherwise respond ready
+    this.ready();
+    return;
   });
 }
