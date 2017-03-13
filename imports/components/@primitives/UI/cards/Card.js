f09f604cc77a7289ee5eb01f42a37316018a8e24
Jake Dawkins <dawkinsjh@gmail.com>
2017-03-13 00:03:26 -0400

    tested card logic

--- imports/components/@primitives/UI/cards/Card.js
+++ imports/components/@primitives/UI/cards/Card.js
@@ -1,183 +1,124 @@
+
+// @flow
 import { Component, PropTypes } from "react";
 import { Link } from "react-router";
+// $FlowMeteor
+import { ifElse, compose, join, flatten, append, merge, identity, concat } from "ramda";
 
 import { ImageLoader } from "../../UI/loading";
 
-import ifElse from "ramda/src/ifElse";
-import compose from "ramda/src/compose";
-import join from "ramda/src/join";
-import flatten from "ramda/src/flatten";
-import append from "ramda/src/append";
-import merge from "ramda/src/merge";
+type IWrapper = { children: Object };
+export const Wrapper = (props: IWrapper) => <div {...props}>{props.children}</div>;
 
-// import { ImageLoader } from "/imports/components/loading";
-// import Styles from "../loading/FeedItemSkeleton-css";
+export const createItemClasses = (itemClasses?: [string], linkAll: boolean): string =>
+  compose(
+    join(" "),
+    flatten,
+    ifElse(() => itemClasses, append(itemClasses), x => x),
+    ifElse(() => linkAll, append("background--light-primary"), x => x)
+  )([ "card__item", "soft", "text-center", "soft-double-ends" ]);
 
-const Wrapper = (props) => (
-  <div {...props}>
-    {props.children}
-  </div>
+export const cardClasses = (classes: [string]): string =>
+  compose(
+    join(" "),
+    flatten,
+    ifElse(() => classes, append(classes), x => x)
+  )(["card"]);
+
+export const createStyles = (linkAll: boolean): Object =>
+  compose(
+    ifElse(() => linkAll, merge({ color: "inherit", textDecoration: "none" }), x => x)
+  )({ overflow: "hidden", display: "block" });
+
+export const imageStyles = (full: boolean, url: string): Object =>
+  ifElse(() => full, () => ({ backgroundImage: `url('${url}')`}), () => ({}))();
+
+// [string] -> -> Object<ReactComponent>
+export const preloader = (imageClasses: [?string] = []) => (): Object =>
+  <div className={`${join(" ")(imageClasses)}`}><div className="ratio__item"/></div>;
+
+// [string] -> Object -> Object<ReactComponent>
+export const renderActualElement = (imageClasses: [?string] = [], style: Object): Object =>
+  <div className={join(" ")(imageClasses)} style={style}><div className="ratio__item" /></div>;
+
+// context from ImageLoader
+function renderElement(){ return renderActualElement(this.imageclasses, this.style); }
+
+// string -> [String] -> string -> boolean -> ([string] -> Object -> Object<ReactComponent>) ->
+//  ([string] -> Object<ReactComponent>) -> Object<ReactComponent>
+export const createImage = (
+  ratio: string,
+  imageClasses: [string],
+  url: string,
+  full: boolean,
+  renderElement: Function,
+  preloader: Function,
+) =>
+  compose(
+    (classes) =>
+      <ImageLoader
+        src={url}
+        imageclasses={classes}
+        renderElement={renderElement}
+        preloader={preloader ? preloader(classes) : null}
+        style={ full !== true ? ({ backgroundImage: `url('${url}')` }) : ({})}
+        data-spec="card-image-loader"
+      />,
+    flatten,
+    ifElse(() => imageClasses, append(imageClasses), identity),
+    ifElse(() => ratio, append(`ratio--${ratio}`), append("ratio--landscape"))
+  )(["background--fill", "card__image", "background-light-tertiary"]);
+
+// boolean -> strng
+export const createWrapperClasses = compose(
+  concat("plain "),
+  ifElse((mobile) => mobile === false, append("visuallyhidden@handheld "), x => x),
 );
 
-Wrapper.propTypes = {
-  children: PropTypes.any, // eslint-disable-line
+type ICard = {
+  classes: [string],
+  theme?: string,
+  link: string,
+  image: Object,
+  styles?: Object,
+  children?: Object,
+  itemClasses?: any, //string/[string]
+  linkAll: boolean,
+  imageclasses: [string],
+  itemTheme: string,
+  itemStyles: Object,
+  mobile?: boolean,
+  wrapperClasses: string,
 };
 
-export default class Card extends Component {
+export default ({
+  classes, theme, link, image, styles, children, itemClasses,
+  linkAll, imageclasses, itemTheme, itemStyles, mobile, wrapperClasses,
+}: ICard): any => {
+    type IImage = { url: string, ratio: string, full: boolean };
+    const { url, ratio, full }: IImage = image;
 
-  static propTypes = {
-    classes: PropTypes.array, // eslint-disable-line
-    theme: PropTypes.string,
-    link: PropTypes.string,
-    image: PropTypes.object, // eslint-disable-line
-    styles: PropTypes.object, // eslint-disable-line
-    children: PropTypes.any, // eslint-disable-line
-    itemClasses: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.array,
-    ]),
-    linkAll: PropTypes.any, // eslint-disable-line
-    imageclasses: PropTypes.array, // eslint-disable-line
-    itemTheme: PropTypes.string,
-    itemStyles: PropTypes.object, // eslint-disable-line
-    mobile: PropTypes.bool,
-    wrapperClasses: PropTypes.string,
-  }
-
-  // [string] -> boolean -> string
-  createItemClasses = (itemClasses, linkAll) =>
-    compose(
-      join(" "),
-      flatten,
-      ifElse(() => itemClasses, append(itemClasses), x => x),
-      ifElse(() => linkAll, append("background--light-primary"), x => x)
-    )([ "card__item", "soft", "text-center", "soft-double-ends" ]);
-
-
-  // [string] -> string
-  cardClasses = (classes) =>
-    compose(
-      join(" "),
-      flatten,
-      ifElse(() => classes, append(classes), x => x)
-    )(["card"]);
-
-  // styles = () => {
-  //   const defaultStyles = {
-  //     overflow: "hidden",
-  //     display: "block",
-  //   };
-  //
-  //   if (this.props.linkAll) {
-  //     defaultStyles.color = "inherit";
-  //     defaultStyles.textDecoration = "none";
-  //   }
-  //
-  //   return defaultStyles;
-  // }
-
-  // boolean -> Object
-  createStyles = (linkAll) =>
-    compose(
-      ifElse(() => linkAll, merge({ color: "inherit", textDecoration: "none" }), x => x)
-    )({ overflow: "hidden", display: "block" });
-
-  // imageStyles = () => {
-  //   const defaultStyles = {};
-  //   if (this.props.image && this.props.image.full) {
-  //     defaultStyles.backgroundImage = `url('${this.props.image.url}')`;
-  //   }
-  //
-  //   return defaultStyles;
-  // }
-  // boolean -> string -> Object
-  imageStyles = (full, url) =>
-    ifElse(() => full, () => ({ backgroundImage: `url('${url}')`}), () => ({}))();
-
-
-  // context from ImageLoader
-  preloader() {
-    return (
-      <div className={`${this.imageclasses.join(" ")}`}>
-        <div className="ratio__item" />
-      </div>
-    );
-  }
-
-  createImage = () => {
-    const { image } = this.props;
-
-    if (image) {
-      let imageclasses = [
-        "background--fill",
-        "card__image",
-        "background-light-tertiary",
-      ];
-
-      if (image.ratio) {
-        imageclasses.push(`ratio--${image.ratio}`);
-      } else {
-        imageclasses.push("ratio--landscape");
-      }
-
-      if (this.props.imageclasses) {
-        imageclasses = [...imageclasses, ...this.props.imageclasses];
-      }
-
-      let style;
-      if (image.full !== true) {
-        style = { backgroundImage: `url('${image.url}')` };
-      }
-
-      return (
-        <ImageLoader
-          src={image.url}
-          preloader={this.preloader}
-          renderElement={this.renderElement}
-          imageclasses={imageclasses}
-          style={style}
-          data-spec="card-image-loader"
-        />
-      );
-    }
-    return undefined;
-  }
-
-  // context from ImageLoader
-  renderElement() {
-    return (
-      <div className={this.imageclasses.join(" ")} style={this.style}>
-        <div className="ratio__item" />
-      </div>
-    );
-  }
-
-  render() {
-    const { link, theme, styles, itemTheme, itemStyles, linkAll, classes, image } = this.props;
-    let { wrapperClasses } = this.props;
-
-    wrapperClasses += " plain";
-    if (this.props.mobile === false) {
-      wrapperClasses += " visuallyhidden@handheld";
-    }
-
-    if (this.props.linkAll) {
+    if (linkAll) {
       return (
         <Link
           data-spec="card"
-          className={theme || this.cardClasses(classes)}
-          style={styles || this.createStyles(linkAll)}
+          className={theme || cardClasses(classes)}
+          style={styles || createStyles(linkAll)}
           to={link}
         >
-          <div className={wrapperClasses} style={this.imageStyles(image ? image.full : null, image ? image.url : null)} data-spec="card-image-wrapper">
-            {this.createImage()}
+          <div
+            className={createWrapperClasses(wrapperClasses)}
+            style={imageStyles(image ? full : false, image ? url : "")}
+            data-spec="card-image-wrapper"
+          >
+            {createImage(ratio, imageclasses, url, full, renderElement, preloader)}
           </div>
           <div
             className={itemTheme || createItemClasses(itemClasses, linkAll)}
             style={itemStyles}
             data-spec="card-item"
           >
-            {this.props.children}
+            {children}
           </div>
         </Link>
       );
@@ -186,34 +127,26 @@ export default class Card extends Component {
     return (
       <div
         data-spec="card"
-        className={theme || this.cardClasses(classes)}
-        style={styles || this.createStyles(linkAll)}
+        className={theme || cardClasses(classes)}
+        style={styles || createStyles(linkAll)}
       >
-        {(() => {
-          if (link) {
-            return (
-              <Link className={wrapperClasses} to={link} data-spec="card-image-wrapper">
-                {this.createImage()}
-              </Link>
-            );
-          }
-          return (
-            <Wrapper className={wrapperClasses} data-spec="card-image-wrapper">
-              {this.createImage()}
-            </Wrapper>
-          );
-        })()}
+        {
+          link ?
+          <Link className={createWrapperClasses(wrapperClasses)} to={link} data-spec="card-image-wrapper">
+            {createImage(ratio, imageclasses, url, full, renderElement, preloader)}
+          </Link>
+          :
+          <div className={createWrapperClasses(wrapperClasses)} data-spec="card-image-wrapper">
+            {createImage(ratio, imageclasses, url, full, renderElement, preloader)}
+          </div>
+        }
         <div
           className={itemTheme || createItemClasses(itemClasses, linkAll)}
           style={itemStyles}
           data-spec="card-item"
         >
-          {this.props.children}
+          {children}
         </div>
-
       </div>
-
     );
   }
-
-}
