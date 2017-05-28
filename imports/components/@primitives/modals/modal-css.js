9c8e6c43c2bc1d954f2b2e477f49c7f8983b7cc6
richarddubay <richardtdubayjr@gmail.com>
2017-05-28 18:15:41 -0400

    fix the modal

--- imports/components/@primitives/modals/modal-css.js
+++ imports/components/@primitives/modals/modal-css.js
@@ -19,20 +19,26 @@ export default StyleSheet.create({
   "prompt-panel": {
     position: "fixed",
     zIndex: 100,
-    top: 200,
+    top: "auto",
     bottom: 0,
 
-    width: "375px",
     overflowX: "hidden",
     overflowY: "auto",
     WebkitOverflowScrolling: "touch",
 
-    "@media (max-width: 480px)": {
+    "@media (max-width: 768px)": {
       bottom: 0,
       top: "auto",
       backgroundColor: "transparent",
       width: "calc(100% - 20px)",
       left: "10px",
+      // maxWidth: "380px",
+    },
+
+    "@media (min-width: 481px)": {
+      left: "50%",
+      marginLeft: "-190px",
+      maxWidth: "380px",
     },
   },
   interior: {
