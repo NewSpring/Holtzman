1da22a4fd162fcefb0248847ecf8d8ed94fcff34
James Baxley <james.baxley@newspring.cc>
2017-06-27 15:47:00 -0400

    well that was easy

--- .end-to-end/accounts/login.js
+++ .end-to-end/accounts/login.js
@@ -1,8 +1,13 @@
 module.exports = {
-  'Demo test': function(browser) {
+  "Login In": function(browser) {
     browser
-      .url(browser.launchUrl + "/give/now")
-      // ...
+      .url(browser.launchUrl + "/profile")
+      .waitForElementVisible("#accounts", 3000)
+      .setValue("#email", ["store-sample@gmail.com", browser.Keys.TAB])
+      .setValue("#password", ["sample", browser.Keys.TAB])
+      .click("#accounts button")
+      .waitForElementPresent(".loaded", 5000)
+      .assert.containsText(".btn--dark-tertiary", "Sign Out")
       .end();
   }
 };
