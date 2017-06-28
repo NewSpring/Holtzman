1e5cbedba9d71b0f6a659a0f8d6292d479ab95db
James Baxley <james.baxley@newspring.cc>
2017-06-28 09:35:09 -0400

    setup single cart item for jake

--- .end-to-end/accounts/login.js
+++ .end-to-end/accounts/login.js
@@ -1,11 +1,7 @@
+var login = require("../util/login");
 module.exports = {
   "Login In": function(browser) {
-    browser
-      .url(browser.launchUrl + "/profile")
-      .waitForElementVisible("#accounts", 3000)
-      .setValue("#email", ["store-sample@gmail.com", browser.Keys.TAB])
-      .setValue("#password", ["sample", browser.Keys.TAB])
-      .click("#accounts button")
+    login(browser.url(browser.launchUrl + "/profile"))
       .waitForElementPresent(".loaded", 5000)
       .assert.containsText(".btn--dark-tertiary", "Sign Out")
       .end();
