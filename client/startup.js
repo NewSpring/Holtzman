92afd4b93019c47acf934de6c9d5ad6b771afc77
richarddubay <richardtdubayjr@gmail.com>
2017-07-17 13:41:50 -0400

    move back to a settings based tag manager

--- client/startup.js
+++ client/startup.js
@@ -256,23 +256,23 @@ Meteor.startup(() => {
       ga("require", "ecommerce");
     }
 
-    // if (Meteor.settings.public.gtm) {
-    // Google Tag Manager
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        "gtm.start": new Date().getTime(),
-        event: "gtm.js"
-      });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-WKZ3JLN");
-    // End Google Tag Manager
-    // }
+    if (Meteor.settings.public.gtm) {
+      // Google Tag Manager
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+          "gtm.start": new Date().getTime(),
+          event: "gtm.js"
+        });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", Meteor.settings.public.gtm);
+      // End Google Tag Manager
+    }
 
     if (Meteor.settings.public.hotjar) {
       (function(h, o, t, j, a, r) {
