a48dc6252a6b7ff22be05e3f7c6cb0ada291c132
delianides <github@delianides.com>
2017-08-17 13:34:56 -0400

    no need to pass event in checkbox

--- imports/components/@primitives/UI/forms/Checkbox.js
+++ imports/components/@primitives/UI/forms/Checkbox.js
@@ -133,7 +133,7 @@ export default class Checkbox extends React.Component {
               ? "checked"
               : ""
           }
-          onClick={e => this.props.clicked(e)}
+          onClick={this.props.clicked}
           onChange={this.props.onChange}
           style={{ width: 0 }}
           data-spec="input"
