895d8a8aa690a634d4f4887163fc265db8337b0a
delianides <github@delianides.com>
2017-10-03 14:28:47 -0400

    runs but has a couple errors

--- imports/data/store/sections/saga.js
+++ imports/data/store/sections/saga.js
@@ -9,7 +9,7 @@ import { addSaga } from "../utilities";
 import { GraphQL } from "../../graphql";
 
 // XXX abstract action creators to file that isn't index
-const set = (content) => ({ type: "SECTIONS.SET_CONTENT", content });
+const set = content => ({ type: "SECTIONS.SET_CONTENT", content });
 
 function* getSectionsData() {
   if (Meteor.isServer) return;
@@ -71,21 +71,21 @@ function* getSectionsData() {
     if (!images.length) return null;
 
     // prefer 1x1 image
-    const oneByOne = _.find(images, (image) => (
+    const oneByOne = _.find(images, image => (
       image.label === "1:1"
     ));
 
     if (oneByOne) return oneByOne.url;
 
     // then try 2x1, especially for devotions that only have 2x1
-    const twoByOne = _.find(images, (image) => (
+    const twoByOne = _.find(images, image => (
       image.label === "2:1"
     ));
 
     if (twoByOne) return twoByOne.url;
 
     // then try default, for devotions with leather times
-    const defaultImage = _.find(images, (image) => (
+    const defaultImage = _.find(images, image => (
       image.label === "default"
     ));
 
@@ -98,14 +98,15 @@ function* getSectionsData() {
   // go ahead and make the query on load (this will be cached on heighliner)
   const { data } = yield GraphQL.query({ query, variables });
   const navigation = data.navigation;
-  delete data.navigation;
   const filteredItems = {};
 
   // parse the results and only get a single usable image
   // eslint-disable-next-line
   for (const item in data) {
-    const image = extractImage(data[item][0]);
-    filteredItems[item] = image;
+    if (item !== "navigation") {
+      const image = extractImage(data[item][0]);
+      filteredItems[item] = image;
+    }
   }
 
   function bindForeignImages(sections) {
