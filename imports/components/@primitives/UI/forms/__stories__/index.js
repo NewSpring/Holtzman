ecd33fadb99a621cbd862e618366406ed2a48933
delianides <github@delianides.com>
2017-10-09 18:41:49 -0400

    updated storybook still need webpack working

--- imports/components/@primitives/UI/forms/__stories__/index.js
+++ imports/components/@primitives/UI/forms/__stories__/index.js
@@ -1,11 +1,11 @@
 
 /* eslint-disable */
-import { storiesOf } from "@kadira/storybook";
+import { storiesOf } from '@storybook/react';
 import withReadme from "storybook-readme/with-readme";
 import backgrounds from "react-storybook-addon-backgrounds";
 import centered from "/.storybook/decorators/centered";
 import defaultColors from "/.storybook/defaults";
-import { withKnobs } from "@kadira/storybook-addon-knobs";
+import { withKnobs } from '@storybook/addon-knobs';
 import Provider from "/.storybook/mocks/Provider";
 
 export default storiesOf("Forms", module)
